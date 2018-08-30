1a1c66f5136bd5bfe4194d7f9b31d8a83d5d1b78
sejungkim <smilesejung@gmail.com>
2018-08-30 16:25:05 +0900

    문자열을 토큰화하는 기능 (tokenizer) 구현 완료

--- array-parser.js
+++ array-parser.js
@@ -1,7 +1,41 @@
+'use strict';
+
 class ArrayParser {
   constructor(targetStr) {
     this.targetStr = targetStr;
     this.result;
+  }
+
+  tokenizer() {
+    const tokens = [];
+    let token = '';
+
+    for (const char of this.targetStr) {
+      switch (char) {
+        case '[':
+          tokens.push(char);
+          break;
+        case ']':
+          this.pushToken(token, tokens);
+          token = '';
+          tokens.push(char);
+          break;
+        case ',':
+          this.pushToken(token, tokens);
+          token = '';
+          break;
+        default:
+          token += char;
+      }
+    }
+    return tokens;
+  }
+
+  pushToken(token, tokens) {
+    if (token) {
+      token = token.trim();
+      tokens.push(token);
+    }
   }
 }
 
@@ -10,3 +44,5 @@ const str2 = '[123,[22],33, [1,2,3,4,5]]';
 const str3 = '[123,[22,23,[11,[112233],112],55],33]';
 
 const arrayParser = new ArrayParser(str3);
+const tokens = arrayParser.tokenizer();
+console.log(tokens);
