8712d63ef2725537f774387d76ed08799be8b5db
cohily12 <k96079123@gmail.com>
2018-10-26 17:15:35 +0900

    covert tokenizer func to class

--- arrayparser.js
+++ arrayparser.js
@@ -15,23 +15,32 @@ function arrayParser(str) {
   const tokens = tokenizer(str);
   const lexemes = lexer(tokens);
 
+  let tempData = '';
   let parsedData;
+
 
   for (let lexeme of lexemes) {
     const type = lexeme.type;
     const value = lexeme.value;
 
-    if (type === 'array') {
+    if (type === 'array' || type === 'object') {
       stack.push(new Data(type, value));
     }
-    else if (type === 'arrayClose') {
+    else if (type === 'arrayClose' || type === 'objectClose') {
+      const top = stack.peek();
+      if (tempData) top.child.push(tempData);
+      tempData = '';
       parsedData = stack.pop();
 
       stack.top ? stack.peek().child.push(parsedData) : '';
     }
-    else {
+    else if (type === 'comma') {
       const top = stack.peek();
-      top.child.push(new Data(type, value, ''));
+      if (tempData) top.child.push(tempData);
+      tempData = '';
+    }
+    else {
+      tempData = new Data(type, value, '');
     }
   }
   return parsedData;
@@ -40,10 +49,6 @@ function arrayParser(str) {
 /*
 Test Case
 */
-const str = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+const str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]"
 const result = arrayParser(str);
 console.log(JSON.stringify(result, null, 2));
-
-const str1 = "['1a'3',[null,false,['11',[112233],112],55, '99'],33, true]";
-const result1 = arrayParser(str1);
-console.log(JSON.stringify(result1, null, 2));
--- lexer.js
+++ lexer.js
@@ -1,3 +1,7 @@
+// const str = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]"
+// const tokenizer = require("./tokenizer.js");
+// const tokens = tokenizer(str);
+
 module.exports = function lexer(tokens) {
     const lexemes = [];
 
@@ -18,29 +22,49 @@ class Lexeme {
         const typeCheck = new TypeCheck;
         const error = new Error;
 
-        if (typeCheck.isNull(token)) return 'null';
         if (typeCheck.isArray(token)) return 'array';
+        if (typeCheck.isComma(token)) return 'comma';
         if (typeCheck.isNumber(token)) return 'number';
         if (typeCheck.isString(token)) return 'string';
-        if (typeCheck.isBoolean(token)) return 'boolean';
         if (typeCheck.isArrayClose(token)) return 'arrayClose';
+        if (typeCheck.isObject(token)) return 'object';
+        if (typeCheck.isObjectClose(token)) return 'objectClose';
+        if (typeCheck.isKey(token)) return 'keyString';
+        if (typeCheck.isBoolean(token)) return 'boolean';
+        if (typeCheck.isNull(token)) return 'null';
 
         return error.throw(token);
     }
 
     getValue(type, token) {
-        if (type === 'null') return null;
-        if (type === 'string') return token;
-        if (type === 'arrayClose') return 'close';
         if (type === 'array') return 'ArrayObject';
         if (type === 'number') return Number(token);
+        if (type === 'string') return token.valueOf();
+        if (type === 'arrayClose') return 'close';
+        if (type === 'object') return 'Object';
+        if (type === 'objectClose') return 'close';
+        if (type === 'keyString') return token.split(':')[0].trim();
         if (type === 'boolean') return token === 'true' ? true : false;
+        if (type === 'null') return null;
     }
 }
 
 class TypeCheck {
     isArray(token) {
         return token === '[';
+    }
+
+    isComma(token) {
+        return token === ',';
+    }
+
+    isNumber(token) {
+        return !token.match(/[^0-9|^.]/);
+    }
+
+    isString(token) {
+        const subStr = token.match(/'.+?'/);
+        return subStr ? subStr[0] === token : false;
     }
 
     isNull(token) {
@@ -55,13 +79,16 @@ class TypeCheck {
         return token === ']';
     }
 
-    isNumber(token) {
-        return !token.match(/[^0-9|^.]/);
+    isObject(token) {
+        return token === '{';
     }
 
-    isString(token) {
-        const subStr = token.match(/'.+?'/);
-        return subStr ? subStr[0] === token : false;
+    isObjectClose(token) {
+        return token === '}';
+    }
+
+    isKey(token) {
+        if (token.match(/:/)) return true;
     }
 }
 
@@ -70,3 +97,5 @@ class Error {
         throw `${token}은 올바른 타입이 아닙니다.`;
     }
 }
+
+// console.log(lexer(tokens));
