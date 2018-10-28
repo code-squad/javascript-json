a120246d12f280ba062d3604617d9a202952b5de
Head <mrdoo309@gmail.com>
2018-10-28 16:55:02 +0900

    예시 수정

--- jsonParser.js
+++ jsonParser.js
@@ -5,8 +5,8 @@ class JSONData {
         this.child = child
     }
 }
- const array = '[123,"hallo",33]'
- function arrayParser(array) {
+const array = '[123,[11, 22],33]'
+function arrayParser(array) {
     str = array.replace(/ /gi, "");
     let type = getType(str)
     let value = getValue(str)
@@ -14,7 +14,7 @@ class JSONData {
     let result = new JSONData(type, value, child)
     printResult(result)
 }
- function getType(str) {
+function getType(str) {
     if (str.indexOf('[') === -1) {
         if (!isNaN(str)) {
             return 'number';
@@ -25,14 +25,14 @@ class JSONData {
         return 'array '
     }
 };
- function getValue(str) {
+function getValue(str) {
     if (str.indexOf('[') !== -1) {
         return str.slice(str.indexOf('[') + 1, str.lastIndexOf(']'))
     } else {
         return str.slice(0, str.length)
     }
 }
- function getChild(str) {
+function getChild(str) {
     child = []
     while (str.indexOf(',') !== -1) {
         var value = str.slice(0, str.indexOf(','))
@@ -45,7 +45,7 @@ class JSONData {
     }
     return child
 }
- function printResult(result) {
+function printResult(result) {
     console.log(result)
 }
- arrayParser(array)
+arrayParser(array)
