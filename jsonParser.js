86ce58ef18cd2cee3c634461498674dd71570058
Head <mrdoo309@gmail.com>
2018-10-30 19:13:34 +0900

    md 설계 완성

--- jsonParser.js
+++ jsonParser.js
@@ -9,16 +9,22 @@ const array = '[123,[11, 22],33]'
 
 //토큰을 구별해내는 함수, 토큰을 분석하는 함수
 function arrayParser(array) {
-    
+    while(array.length === 0) {
+
+    }
 }
 
 function judgeToken(value) {
     if(value.indexOf(',') === -1) {
         return value
     }else if(value[0] === '[') {
-        return value.slice(0, value.indexOf(']'))
+        return value.slice(1, value.lastIndexOf(']'))
     }
     return value.slice(0, value.indexOf(','))
+}
+
+function analyzeToken(token) {
+    const value = judgeToken(token)
 }
 
 function getValue(str) {
