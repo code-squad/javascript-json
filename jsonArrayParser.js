26bca641d8120cdd56e1ce71cc75bd4b90611de8
cocoa <>
2018-11-12 21:45:54 +0900

    Lec4 Array Parser 커밋 로그 step5
    -mainBranch, sideBranch를  rootBranch, lowerBranch로 변경하였습니다.
    
    -새로운 브랜치 추가시 key 값의 유무(객체 또는 배열)에 따라 브랜치의 형태를 다르게 구성하는 메소드를 정의하였습니다.
    
    -문자열 해체시 발생하는 다양한 처리 유형을 별도의 메소드로 분리하였습니다.
    
    -문자열의 오류를 검사하는 메소드를 유형별로 분리하였습니다.

--- jsonArrayParser.js
+++ jsonArrayParser.js
@@ -1,122 +1,226 @@
-// arrayParser step4(여러 가지 type 분석) 
+// arrayParser step5(객체 type 분석) step4 수정 사항 반영, 클래스 작성
 
-const arrayParser = {
-
-    stack: [],
+const ArrayParser = class {
+    constructor() {
+        this.accumulatedObjStack = [];
+    }
 
     // 문자열 해체
     separateStringByLexer(targetStr) {
-        let mainBranch = {}; // 분석 결과
-        let extractedString = "";
+        this.key = "";
+        let rootBranch = {}; // 최종 분석결과
+        this.extractedString = ""; // 추출한 문자열 
         for (let ch of targetStr) {
-            if (ch === "[") {
-                mainBranch = this.checkBrace(mainBranch);
-
-            } else if ((ch === "," || ch === " ") && extractedString !== "") {
-                if (this.analyzeTypeByParser(extractedString)) return;
-                extractedString = "";
-
-            } else if (ch === "]" && extractedString !== "") {
-                if (this.analyzeTypeByParser(extractedString)) return;
-                extractedString = "";
-                this.accumulatedObj = this.stack.pop();
-
-            } else if (ch === "]" && extractedString === "") {
-                this.accumulatedObj = this.stack.pop();
-
-            } else if ((ch === "," || ch === " ") && extractedString === "") {
-                undefined;
-
-            } else extractedString += ch; 
+            if (ch === "[" || ch === "{") {
+                rootBranch = this.checkBrace(rootBranch, ch); // 새로운 브랜치 생성(괄호)
+            }
+            else if (this.passByCharacter(this.extractedString, ch)) continue;
+            else if (this.tossToParser(this.extractedString, ch)) continue;
+            else if (this.identifyKey(this.extractedString, ch)) continue;
+            else this.extractedString += ch;
         }
-        return mainBranch;
-    },
+        return rootBranch;
+    }
 
-    // 대괄호 처리
-    checkBrace(mainBranch) {
-        if (!mainBranch.hasOwnProperty("child")) {
-            this.accumulatedObj = this.generateBranch(mainBranch);
-            return mainBranch;
-        } else {
-            const sideBranch = {};
-            this.generateBranch(sideBranch);
-            this.stack.push(this.accumulatedObj);
-            let accumulatedObj = this.accumulatedObj.child;
-            accumulatedObj.push(sideBranch);
+    // 괄호 체크, 브랜치 생성(중괄호, 대괄호를 만날 때 마다 호출)
+    checkBrace(rootBranch, ch) {
+        if (!rootBranch.hasOwnProperty("child"))
+            return this.generateRootBranch(rootBranch, ch);
+        else return this.generateLowerBranch(rootBranch, ch);
+    }
+
+    // 루트 브랜치 생성
+    generateRootBranch(rootBranch, ch) {
+        this.accumulatedObj = this.composeBranchForm(rootBranch, ch);
+        return rootBranch;
+    }
+
+    // 하위 브랜치 생성
+    generateLowerBranch(branchType, ch) {
+        const lowerBranch = {};
+        this.composeBranchForm(lowerBranch, ch); // lowerBranch를 리턴한다. 
+        this.accumulatedObjStack.push(this.accumulatedObj);
+        let accumulatedObj = this.accumulatedObj.child;
+        accumulatedObj.push(lowerBranch);
+        if (!this.key) {
             this.accumulatedObj = accumulatedObj[accumulatedObj.length - 1];
-            return mainBranch;
         }
-    },
+        if (this.key) {
+            this.accumulatedObj = accumulatedObj[accumulatedObj.length - 1].value;
+            this.key = undefined;
+        }
+        return branchType;
+    }
 
-    // 브랜치 생성
-    generateBranch(branch) {
-        branch.type = 'array';
-        branch.value = 'ArrayObject';
-        branch.child = [];
-        return branch;
-    },
+    // 브랜치 요소 구성하기
+    composeBranchForm(branchType, ch) {
+        if (ch === '[' && !this.key)
+            return this.formObjectInfoBranch(branchType, "array", "object Array");
+        if (ch === '{' && !this.key)
+            return this.formObjectInfoBranch(branchType, "object", "object Object");
+        if (ch === '[' && this.key)
+            return this.formKeyValueBranch(branchType, this.key, "array", "object Array");
+        if (ch === '{' && this.key)
+            return this.formKeyValueBranch(branchType, this.key, "object", "object Object");
+    }
+
+    // 오브젝트 정보 브랜치 
+    formObjectInfoBranch(branchType, type, value) {
+        branchType.type = type;
+        branchType.value = value;
+        branchType.child = [];
+        return branchType;
+    }
+
+    // 키값 정보 브랜치
+    formKeyValueBranch(branchType, key, type, value) {
+        branchType.key = key;
+        branchType.value = {
+            type: type,
+            value: value,
+            child: []
+        }
+        return branchType;
+    }
+
+    // 해당 문자열 통과, 스택 제거
+    passByCharacter(extractedString, ch) {
+        if ((ch === "," && extractedString === "") || ch === " ") return true;
+        if ((ch === "]" || ch === "}") && extractedString === "") {
+            this.accumulatedObj = this.accumulatedObjStack.pop();
+            this.key = undefined;
+            return true;
+        }
+    }
+
+    // 문자열 파서로 전달
+    tossToParser(extractedString, ch) {
+        if (ch === "," && extractedString !== "") {
+            if (this.parseStringByType(extractedString)) {
+                this.extractedString = "";
+                this.key = undefined;
+                return true;
+            }
+        }
+        if ((ch === "]" || ch === "}") && extractedString !== "") {
+            if (this.parseStringByType(extractedString)) {
+                this.extractedString = "";
+                this.accumulatedObj = this.accumulatedObjStack.pop();
+                this.key = undefined;
+                return true;
+            }
+        }
+        return false;
+    }
 
     // 문자열 분석
-    analyzeTypeByParser(extractedString) {
-        if (this.identifyNumber(extractedString)) return;
-        if (this.identifyBoolean(extractedString)) return;
+    parseStringByType(extractedString) {
+        if (this.identifyNumber(extractedString)) return true;
+        if (this.identifyBoolean(extractedString)) return true;
         if (this.identifyString(extractedString)) return true;
-    },
+    }
 
     // 숫자 데이터 식별 
     identifyNumber(extractedString) {
-        if (isNaN(Number(extractedString))) return;
-        this.addDataInfo(extractedString, "number"); // 숫자면 true; 
-        return true;
-    },
+        if (isNaN(Number(extractedString))) return false;
+        return this.accumulatedObj.child.push(this.addChildInfo(extractedString, "number")); // 숫자면 true; 
+    }
 
     // Boolean, null 데이터 식별
     identifyBoolean(extractedString) {
-        if (extractedString === "true") {
-            return this.addDataInfo(true, "Boolean");
-        }
-        if (extractedString === "false") {
-            return this.addDataInfo(false, "Boolean");
-        }
-        if (extractedString === "null") {
-            return this.addDataInfo(null, "null");
-        }
-        return;
-    },
+        let childObj = {};
+        if (extractedString !== "true" && extractedString !== "false" && extractedString !== "null")
+            return false;
+        if (extractedString === "true") childObj = this.addChildInfo(true, "Boolean");
+        if (extractedString === "false") childObj = this.addChildInfo(false, "Boolean");
+        if (extractedString === "null") childObj = this.addChildInfo(null, "null");
+        return this.accumulatedObj.child.push(childObj);
+    }
 
     // 문자열 식별
     identifyString(extractedString) {
-        if (this.checkErrorString(extractedString)) return true;
-        this.addDataInfo(extractedString, "string");
-    },
+        if (this.checkErrorString(extractedString)) {
+            this.accumulatedObj.child.push(this.addChildInfo(extractedString, "string"));
+            return true;
+        }
+        return true;
+    }
 
     // 문자열 오류 체크
     checkErrorString(extractedString) {
-        if (extractedString.startsWith("\'") && extractedString.endsWith("\'") &&
+        if (this.checkIsCorrectString(extractedString)) return true;
+        if (this.checkIsUnknownType(extractedString)) return false;
+        if (this.checkIsInCorrectString(extractedString)) return false;
+    }
+
+    // 올바른 문자열 체크
+    checkIsCorrectString(extractedString) {
+        if (extractedString.startsWith("\'") &&
+            extractedString.endsWith("\'") &&
             extractedString.match(/'/g).length == 2)
-            return;
-        if (!extractedString.startsWith("\'") && !extractedString.endsWith("\'")) {
-            console.log(`오류를 탐지하였습니다. ${extractedString}는 알 수 없는 타입입니다. 분석을 종료합니다.`);
             return true;
-        } if (extractedString.match(/'/g).length > 2 || extractedString.match(/'/g).length === 1) {
-            console.log(`오류를 탐지하였습니다. ${extractedString}는 올바른 문자열이 아닙니다. 분석을 종료합니다.`);
+    }
+
+    // 알 수 없는 타입 체크
+    checkIsUnknownType(extractedString) {
+        if (!extractedString.startsWith("\'") && !extractedString.endsWith("\'")) {
+            console.log(`오류를 탐지하였습니다. ${extractedString}는 알 수 없는 타입입니다.`);
             return true;
         }
-    },
+    }
 
-    // 각 타입에 해당하는 child 객체 생성
-    addDataInfo(extractedString, type) {
+    // 올바르지 않은 문자열 체크
+    checkIsInCorrectString(extractedString) {
+        if (extractedString.match(/'/g).length > 2 ||
+            (!extractedString.startsWith("\'") && extractedString.endsWith("\'")) ||
+            (extractedString.startsWith("\'") && !extractedString.endsWith("\'"))) {
+            console.log(`오류를 탐지하였습니다. ${extractedString}는 올바른 문자열이 아닙니다.`);
+            return true;
+        }
+    }
+
+    // 각 타입에 해당하는 child 객체 생성 
+    addChildInfo(extractedString, type) {
         const childObj = {};
-        childObj.type = type
+        if (this.key) childObj.key = this.key;
+        childObj.type = type;
         childObj.value = extractedString;
-        childObj.child = [];
-        this.accumulatedObj.child.push(childObj);
-        return "added";
-    },
+        if (!this.key) childObj.child = [];
+        return childObj;
+    }
 
-} // end arrayParser 
-console.log(JSON.stringify(arrayParser.separateStringByLexer("[10, 'apple' ,[ 20 , null,[ 30 , false , '32'], '23*6*1'], 11, 'hell'oween', 13, [22, ['33', [true] , 'iOS12.1'],23 , 'false'],14, 'theEnd' ]"), null, 4));
-console.log(JSON.stringify(arrayParser.separateStringByLexer("[10, 'apple' ,[ 20 , null,[ 30 , false , '32'], '23*6*1'], 11, helloween, 13, [22, ['33', [true] , 'iOS12.1'],23 , 'false'],14, 'theEnd' ]"), null, 4));
+    // 키 식별
+    identifyKey(extractedString, ch) {
+        if (ch === ":") {
+            this.key = extractedString;
+            this.extractedString = "";
+            return true;
+        } else return false;
+    }
+
+} // end class
+
+const arrayParser = new ArrayParser();
+
+
+console.log(JSON.stringify(arrayParser.separateStringByLexer("{ name : 'lee', age : 33, hobby : 'photo' }"), null, 2));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("{info:[{ name : 'lee', age : 34, addr : 'Seoul'}, {hobby : 'photo'}, 'endArr' ], study : 'codeSquad', place : 'Gangnam-gu'}"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("{x:[10,20,[30,{y:'apple'},50]]}"), null, 4)); // 
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{x:'apple', y:'samsung'},10,[[[[[[true]]]]]],30]]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("{ tree1 : { tree2 : { tree3 : { tree4 : { tree5 : { tree6 : { tree7 : { tree8 : { tree9 : { tree10 : { top : [true]}}}}}}}}}}}"), null, 2));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("{member1:'crong',member2:'honux',member3:'jk',member4:'pobi',a:{b:1,c:2,d:3},favorite:'milkTea',music:{genre:'rock',artist:'lifeHouse',year:2014,country:'USA'}}"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{a:{b:1,c:2,d:3},favorite:'milkTea'},{name:'lee',hobby:'photo'}]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{x:'a', y:'b', z:'c'},{a:'x', b:'y', c:'z'}]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{x:'a', y:12, z:44}, {y:'b'}, {a:1, b:2, c:3}]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{x:'a', y:'b', z:'c'}, {x:'a', y:'b', z:'c'}]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[ { x : [1,2,3], y : [1,2,3], z : 'apple' }, 10  ]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[ { x : 10,  y : 20 }, 10, [20, [30], 200], 100 ]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[ 40, 50, 60, { name : 'lee', age : false}, 70, 80, 90, { x : [10,20,30]} ]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{ x : { y : { z : { hobby : 'photo' } } } }, [10,20,30]]"), null, 4));
+console.log(JSON.stringify(arrayParser.separateStringByLexer("[{ x : [10,20,{ hobby : 'photo'} ], name : 'lee' } ]"), null, 4));
+
+
 console.log(JSON.stringify(arrayParser.separateStringByLexer("[10, 'apple' ,[ 20 , null,[ 30 , false , '32'], '23*6*1'], 11, 'helloween', 13, [22, ['33', [true] , 'iOS12.1'],23 , 'false'],14, 'theEnd' ]"), null, 4));
 console.log(JSON.stringify(arrayParser.separateStringByLexer("[ '1a3',[ null, false,['11', [112233] , 112], 55   ,99 ], 33, true ]  "), null, 4));
 console.log(JSON.stringify(arrayParser.separateStringByLexer('[1,false,\'apple\',null,5]'), null, 4));
