c6e43a17d20959f4a51a86e0bf9e8f785f884af1
Head <mrdoo309@gmail.com>
2018-11-13 13:18:54 +0900

    주석제거

--- jsonParser.js
+++ jsonParser.js
@@ -33,29 +33,6 @@ class Tokenize {
         }
     }
 }
-// function arrayParser(arr) {
-//     const wholeDataQueue = [];
-//     while (arr.length !== 0) {
-//         //토큰 자르고 토큰wholeDataqueue 에 넣고
-//         const token = getToken(arr)
-//         wholeDataQueue.push(token)
-//         arr = arr.replace(token, '')
-//     }
-//     const analyzedQueue = analyzeQueue(wholeDataQueue)
-//     return analyzedQueue
-// }
-// function getToken(str) {
-//     //, [ or ]가 나오면 따로뽑아냄
-//     if (str[0] === '[' || str[0] === ',' || str[0] === ']') {
-//         return str.slice(0, 1)
-//     } else if (str.indexOf(']') < str.indexOf(',')) {
-//         return str.slice(0, str.indexOf(']'))
-//     } else if (str.indexOf(',') === -1) {
-//         return str.slice(0, str.indexOf(']'))
-//     } else {
-//         return str.slice(0, str.indexOf(','))
-//     }
-// }
 
 class Analyze {
     constructor(queue) {
@@ -89,32 +66,6 @@ class Analyze {
         return child
     }
 }
-// function analyzeQueue(queueArr) {
-//     while (queueArr.length !== 0) {
-//         const value = queueArr.shift()
-//         if (value === '[') {
-//             const child = getChild(queueArr, value)
-//             return new JSONData('array', 'array Object', child)
-//         }
-//     }
-// }
-
-// function getChild(queueArr, valueIn) {
-//     let child = []
-//     while (valueIn !== ']') {
-//         let valueIn = queueArr.shift()
-//         if (valueIn === '[') {
-//             child.push(new JSONData('Array', 'object Array', getChild(queueArr, valueIn)))
-//             continue;
-//         } else if (valueIn === ',') {
-//             continue;
-//         } else if (valueIn === ']') {
-//             break;
-//         }
-//         child.push(new JSONData('Number', valueIn, []))
-//     }
-//     return child
-// }
 
 function printJSONData(JSONData) {
     console.log(JSON.stringify(JSONData, null, 2))
