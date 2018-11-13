58f5f3983003c236a3f3086bb01d18e9bb10ee2c
Head <mrdoo309@gmail.com>
2018-11-13 11:12:11 +0900

    피드백 받은 내용 수정(이름변경)

--- jsonParser.js
+++ jsonParser.js
@@ -6,54 +6,47 @@ class JSONData {
     }
 }
 
-const array = "[123,[22, 33],44,[1,2,3], 11]".replace(/ /gi, '')
+const sentence = "[123,[22,[33]],44,[1,2,3],11]".replace(/ /gi, '')
 
-function ArrayParser(array) {
-    const WholeDataQueue = [];
-    while (array.length !== 0) {
-        debugger;
+function ArrayParser(arr) {
+    const wholeDataQueue = [];
+    while (arr.length !== 0) {
         //토큰 자르고 토큰wholeDataqueue 에 넣고
-        const token = getToken(array)
-        queuePusher(token, WholeDataQueue)
-        array = array.replace(token, '')
+        const token = getToken(arr)
+        wholeDataQueue.push(token)
+        arr = arr.replace(token, '')
     }
-    return analyzeQueue(WholeDataQueue)
+    return analyzeQueue(wholeDataQueue)
 }
-function getToken(string) {
+function getToken(str) {
     //, [ or ]가 나오면 따로뽑아냄
-    if (string[0] === '[' || string[0] === ',' || string[0] === ']') {
-        return string.slice(0, 1)
-    } else if (string.indexOf(']') < string.indexOf(',')) {
-        return string.slice(0, string.indexOf(']'))
-    } else if (string.indexOf(',') === -1) {
-        return string.slice(0, string.indexOf(']'))
+    if (str[0] === '[' || str[0] === ',' || str[0] === ']') {
+        return str.slice(0, 1)
+    } else if (str.indexOf(']') < str.indexOf(',')) {
+        return str.slice(0, str.indexOf(']'))
+    } else if (str.indexOf(',') === -1) {
+        return str.slice(0, str.indexOf(']'))
     } else {
-        return string.slice(0, string.indexOf(','))
+        return str.slice(0, str.indexOf(','))
     }
 }
-function queuePusher(token, queue) {
-    queue.push(token)
-    return queue
-}
-function queueShifter(queue) {
-    return queue.shift()
-}
-function analyzeQueue(queue) {
-    debugger;
-    while (queue.length !== 0) {
-        const value = queueShifter(queue)
+
+function analyzeQueue(queueArr) {
+    while (queueArr.length !== 0) {
+        const value = queueArr.shift()
         if (value === '[') {
-            const child = getChild(queue, value)
+            const child = getChild(queueArr, value)
             return new JSONData('array', 'array Object', child)
         }
     }
 }
-function getChild(queue, valueIn) {
+
+function getChild(queueArr, valueIn) {
     let child = []
     while (valueIn !== ']') {
-        let valueIn = queueShifter(queue)
+        let valueIn = queueArr.shift()
         if (valueIn === '[') {
-            child.push(new JSONData('Array', 'object Array', getChild(queue, valueIn)))
+            child.push(new JSONData('Array', 'object Array', getChild(queueArr, valueIn)))
             continue;
         } else if (valueIn === ',') {
             continue;
@@ -68,4 +61,4 @@ function printJSONData(JSONData) {
     console.log(JSON.stringify(JSONData, null, 2))
 }
 
-printJSONData(ArrayParser(array))
+printJSONData(ArrayParser(sentence))
