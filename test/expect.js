e8585fcbc6be577ba8b42df40aaa26c40190407b
cohily12 <k96079123@gmail.com>
2018-11-14 15:48:34 +0900

    [expect 수정] Object.is 제거 및 type check 수정

--- test/expect.js
+++ test/expect.js
@@ -6,14 +6,11 @@ const expect = function (data) {
 
     return expectation = {
         toBe(value) {
-            const bSame = this.toEqual(value);
-            if (bSame) return console.log(`OK`);
-            console.log(`FAIL (targetValue is ${JSON.stringify(value)}, expectValue is ${JSON.stringify(data)})`)
+            if (this.toEqual(value)) return console.log(`OK`);
+            console.log(`FAIL (targetValue is ${JSON.stringify(value)}, expectValue is ${JSON.stringify(data)})`);
         },
 
         toEqual(value, expectValue = data) {
-            if (Object.is(value, expectValue)) return true;
-
             if (isObject(value, expectValue)) {
                 for (let key in value) {
                     if (!expectValue.hasOwnProperty(key)) return false;
@@ -21,6 +18,7 @@ const expect = function (data) {
                 }
                 return true;
             }
+            return value === expectValue;
         }
     }
 }
