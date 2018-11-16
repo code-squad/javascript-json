e67431dd83797eee3cad1028c079dc785e2daea9
Head <mrdoo309@gmail.com>
2018-11-16 16:10:46 +0900

    마지막 함수를 변수는 짧게 함수 이름은 길게 만듬

--- jsonParser.js
+++ jsonParser.js
@@ -5,7 +5,10 @@ class JSONData {
         this.child = child
     }
 }
-
+// 복잡한 세부로직은 반드시 함수로 분리해본다.
+// 최대한 작은 단위의 함수로 만든다.
+// 중복된 코드역시 함수로 분리해서 일반화한다.
+// 객체형태의 class로 만든다. v
 const sentence = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]".replace(/ /gi, '')
 // const sentence = "'1a3',[null,false,['11',[112233],112],55, '99'],33, true]".replace(/ /gi, '')
 class Tokenize {
@@ -123,7 +126,7 @@ class ErrorCheck {
     }
 }
 
-function printJSONData(JSONData) {
+const print = function printJSONData(JSONData) {
     console.log(JSON.stringify(JSONData, null, 2))
 }
 
@@ -133,5 +136,5 @@ const errorCheck = new ErrorCheck
 const analyze = new Analyze(tokenizedDataArr, errorCheck)
 const jsonData = analyze.queue()
 
-printJSONData(jsonData)
+print(jsonData)
 
