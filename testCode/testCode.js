d5ac860c1ad013e15803e87da19892c9f8080a2e
Head <mrdoo309@gmail.com>
2018-11-23 16:29:18 +0900

    test메서드 완성, checkObj의 toBe메서드에 맞을때와 아닐 때 반환하는 값 변경

--- testCode/testCode.js
+++ testCode/testCode.js
@@ -7,16 +7,23 @@ const checkObj = {
     value: null,
 
     toBe(value) {
-        if(value === this.value) {
-            return true
+        if (value === this.value) {
+            return 'OK'
+        } else {
+            return `FAIL (나온 값 : ${value}, 나와야하는 값 : ${this.value})`
         }
     }
 }
 
 function expect(value) {
-    obj.value = value
-    return obj
+    checkObj.value = value
+    return checkObj
 }
+
+function test(message, method) {
+    console.log(`${message} : ${method()}`)
+}
+
 
 
 
