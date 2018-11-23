9a39aeba592c964eff301f92e5666652d9b55584
Head <mrdoo309@gmail.com>
2018-11-23 22:20:52 +0900

    analyze객체에 필요한 errorCheck인자추가, analyze객체의 getChild메서드 테스트

--- testCode/testCode.js
+++ testCode/testCode.js
@@ -2,8 +2,8 @@ const Analyze = require('../script/analyze')
 const ErrorCheck = require('../script/errorCheck.js')
 const Tokenize = require('../script/tokenize')
 const JSONData = require('../script/jsonData')
-const analyze = new Analyze
 const errorCheck = new ErrorCheck
+const analyze = new Analyze([], errorCheck)
 const tokenize = new Tokenize
 
 const checkObj = {
@@ -45,3 +45,51 @@ test('데이터가 문자열인지 확인한다.', function() {
     return expect(true).toBe(result)
 })
 
+test('주어진 데이터queue배열이 끝날때까지 체크하며 배열이라면 배열 타입의child를 만들어 준다.', function() {
+    const data = ['[']
+    const result = analyze.getChild(data)[0].type
+    return expect('Array').toBe(result)
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 객체라면 객체 타입의 child를 만들어 준다.', function() {
+    const data = ['{']
+    const result = analyze.getChild(data)[0].type
+    return expect('Object').toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 값이 객체의 키값인지 확인한후 키값을 value로 만들어 준다.', function() {
+    const data = ['a:']
+    const result = analyze.getChild(data)[0].value
+    return expect('a').toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 배열이나 객체가 끝나는 괄호가 나온다면 반복문을 끝내고 배열을 반환해준다.', function() {
+    const data = ['}']
+    const result = analyze.getChild(data)[0]
+    return expect(undefined).toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 boolean이라면 boolean타입의 child를 만들어 준다', function() {
+    const data = ['true']
+    const result = analyze.getChild(data)[0].type
+    return expect('Boolean').toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 null 이라면 null타입의 child를 만들어 준다.', function() {
+    const data = ['null']
+    const result = analyze.getChild(data)[0].type
+    return expect('Null').toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 String이라면 string타입의 child를 만들어 준다', function() {
+    const data = ["'string'"]
+    const result = analyze.getChild(data)[0].type
+    
+    return expect('String').toBe(result) 
+})
+
+test('주어진 데이터queue배열이 끝날때까지 체크하며 Number라면 Number타입의 child를 반환해 준다.', function() {
+    const data = ['11']
+    const result = analyze.getChild(data)[0].type
+    return expect('Number').toBe(result) 
+})
