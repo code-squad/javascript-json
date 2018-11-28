c6a6bd2e96355eb44af3219f94d9866ad468d91a
kakafe <sk2504@naver.com>
2018-11-28 22:39:32 +0900

    createArr 함수,createLiteral함수,arrReducer함수 추가
    객체타입이 들어왔을때 객체 리터럴 형태로 반환하도록 일단 구현함.

--- scripts.js
+++ scripts.js
@@ -22,7 +22,6 @@ function checkArray(acc, cur) {
   stack.endCount++;
   acc = cur;
   return acc;
-
 };
 function checkEnd(acc, cur) {
   if (cur === ']' && stack.endCount !== 0) {
@@ -43,6 +42,7 @@ function lexer(fn, str) {
   let tokenArray = fn(str);
   if (checkSyntax(tokenArray)) return false;
   let lexerResult = tokenArray.map(tokenMapper);
+  console.log(lexerResult);
   return lexerResult;
 };
 function checkSyntax(tokenArray) {
@@ -61,7 +61,7 @@ function checkQuote(val) {
 };
 function checkNaN(val) {
   if (getBoolean(val)) return false;
-  if (val.match(/'|"/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
+  if (val.match(/'|"|\{/g) === null && val !== '[' && val !== ']' && isNaN(val * 1)) {
     console.log(`${val}는 알수없는 타입임니다.`);
     return true;
   }
@@ -69,9 +69,7 @@ function checkNaN(val) {
 };
 function getBoolean(val) {
   val = val.match(/\S\w*/g)
-  return type['bool'].some(function (bool) {
-    return bool === val[0];
-  });
+  return type['bool'].some(function (bool) { return bool === val[0]; });
 };
 const type = {
   '[': { type: 'array', value: 'ArrayObject', child: [] },
@@ -100,12 +98,41 @@ function checkType(value, obj = 0) {
   return false;
 };
 function returnObj(value) {
-  let result = { type: 'object', value: 'object', child: [] };
-  result.child.push(JSON.parse(value));
+  let result = { type: 'object', value: null };
+  result.value = createObj(value);
   return result;
 };
+function createObj(value) {
+  let result = {}
+  let key = ''
+  let val = ''
+  let keyCount = 0;
+  let nestedCount = 0;
+  let arrayCount = 0;
+  for (indx in value) {
+    let token = value[indx]
+    if (token === '{' && arrayCount === 0) continue;
+    if (token === ' ') continue;
+    if (token === '[') arrayCount++;
+    if (token === '}'&& arrayCount === 0|| token === ',' && arrayCount === 0) {
+      result[key] = createLiteral(val);
+      key = '';
+      val = '';
+      keyCount = 0;
+    }
+    if (arrayCount !== 0) {
+      val += token;
+    }
+    if (keyCount === 0 && token !== ':' && token !== ',') key += token;
+    if (keyCount !== 0 && token !== '}' && arrayCount === 0) val += token;
+    if (token === ']' && arrayCount !== 0) arrayCount--;
+    if (token === ':') keyCount++;
+  }
+  return result;
+}
 function tokenize(str) {
   let result = each(str, checkToken);
+  console.log(result)
   return result;
 };
 function each(str, iter) {
@@ -118,7 +145,7 @@ function each(str, iter) {
 };
 function checkObjToken(str, token, i) {
   if (str[i] === '{' || stack.objCount !== 0) {
-    if (str[i] === '{') stack.objCount++; 
+    if (str[i] === '{') stack.objCount++;
     token += str[i];
     if (str[i] === '}') stack.objCount--;
     return token;
@@ -135,8 +162,77 @@ function checkToken(str, result, token, i) {
   if (str[i] === ',' && str[i - 1] !== ']' && stack.objCount === 0) { result.push(token); token = ''; }
   return token;
 };
+function createLiteral(val) {
+  if (isNaN(val) && val.match(/\{|\[/g) === null && val !== 'false' && val !== 'true' && val !== 'null') return String(val);
+  if (!isNaN(val)) return Number(val);
+  if (val === 'true' || val === 'false' || val === 'null') {
+    return val === 'true' ? true : val === 'false' ? false : val === 'null' ? null : undefined;
+  }
+  if (val.match(/^\[/g)) return createArr(val)
+  if (val.match(/\{/g)) return createObj(val);
+};
+function createArr(str) {
+  let value = '';
+  let result = [];
+  let stackCount = -1;
+  let endCount = 0;
+  let stack = {};
+  for (indx in str) {
+    let token = str[indx]
+    if (token === ' ') continue;
+    if (indx === `${str.length - 1}`) {
+      if (value.length !== 0) result.push(createLiteral(value))
+      continue;
+    }
+    if (token === '[' && indx === '0') continue;
+    if (token === '[' && indx !== '0') {
+      stack[stackCount] = [];
+      stackCount++;
+      endCount++;
+      continue;
+    }
+    if (token !== '[' && token !== ',' && token !== ']' && !value.match(/\{/g)) value += token;
+    if (token === ',' && stackCount === -1 && !value.match(/\{/g)) {
+      result.push(createLiteral(value));
+      value = '';
+    }
+    if (value.match(/\{/g) && token !== '{') value += token;
+    if (token === '}' && stackCount !== -1) {
+      stack[stackCount - 1].push(createLiteral(value));
+      value = '';
+    }
+    if (stackCount !== -1 && token === ',' && !value.match(/\{/g) && value.length !== 0 || token === ']' && value.length !== 0 && !value.match(/\{/g) && endCount !== 0) {
+      stack[stackCount - 1].push(createLiteral(value));
+      value = '';
+      continue;
+    }
+    if (token === ',' && stackCount !== -1) {
+      stackCount = arrReducer(stack, stackCount, result);
+      continue;
+    }
+    if (token === ']' && stackCount !== 0) {
+      stackCount = arrReducer(stack, stackCount);
+      endCount++;
+    }
+    if (stackCount === 0 && token === ',') {
+      result.push(stack[-1]);
+    }
+  }
+  return result;
+}
+function arrReducer(stack, stackCount, result) {
+  if (stackCount === 0) {
+    result.push(stack[-1])
+    stackCount--;
+    return stackCount;
+  }
+  stack[stackCount - 2].push(stack[stackCount - 1]);
+  delete stack[stackCount - 1]
+  stackCount--;
+  return stackCount;
+}
 
-let str = '["1a3",[{ "ke": "innervalue", "newkeys": { "a": ["1",{ "name": "arr", "arr": ["1", "2"] },"5"]}}],{"name" :1}, true]';
+let str = '[{name : [1,{name : 123}], dsod : "asd"},[1,[2,[3],4],5],6]';
 let stack = new Stack;
 let result = arrayParser(str);
 console.log(JSON.stringify(result, null, 2));
