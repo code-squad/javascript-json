dc1ad9cec0feaa818c2e00b3b6c65600cd9406b1
allen <revenoir7@gmail.com>
2019-05-01 00:32:38 +0900

    feat: 문자열을 토큰화 하기위한 Tokenizer 메서드 작성

--- arrayParser.js
+++ arrayParser.js
@@ -1,5 +1,22 @@
 const str = "[123, 22, 33]";
 class Tokenizer {
+    seperateBrackets(str) {
+        str = str.replace(/\[/g, '[,');
+        str = str.replace(/\]/g, ',]');
+        return str;
+    }
+    removeSpace(str) {
+        let arr = str.split(',');
+        arr = arr.map(el => el.trim());
+        str = arr.join();
+        return str;
+    }
+    tokenizeByChar(str, char) {
+        str = this.seperateBrackets(str);
+        str = this.removeSpace(str);
+        const arr = str.split(char)
+        return arr;
+    }
 }
 
 class Lexer {
