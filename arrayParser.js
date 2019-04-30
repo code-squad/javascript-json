950ba1b63a198be5d2fe70bf67941d077a240e59
allen <revenoir7@gmail.com>
2019-05-01 00:31:38 +0900

    feat: Tokenizer, Lexer, ArrayParser 3개의 클래스로 구조 설계

--- arrayParser.js
+++ arrayParser.js
@@ -1,4 +1,21 @@
 const str = "[123, 22, 33]";
+class Tokenizer {
+}
 
-const result = ArrayParser(str);
+class Lexer {
+}
+
+class ArrayParser {
+    constructor(tokenizer, lexer) {
+        this.tokenizer = tokenizer;
+        this.lexer = lexer;
+    }
+
+}
+
+const tokenizer = new Tokenizer();
+const lexer = new Lexer();
+const arrayParser = new ArrayParser(tokenizer, lexer);
+
+const result = arrayParser.parseToken(str);
 console.log(result);
