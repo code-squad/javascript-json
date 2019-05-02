0a7216ce922d37b1928ae857dc547625cdbef326
fed-gren <deveb22@gmail.com>
2019-05-02 21:19:06 +0900

    add: step 6-2

---
+++ Parser.js
@@ -0,0 +1,164 @@
+const separators = require("./separators");
+const literals = require("./literals");
+const errorMessages = require("./errorMessages");
+const Stack = require("./Stack");
+const parentObjStack = new Stack();
+
+const { log } = console;
+
+const parserUtils = {
+  isSeparator: function(letter) {
+    for (let separator of Object.values(separators)) {
+      if (letter === separator) return true;
+    }
+    return false;
+  },
+
+  isEndofLiteral: function(idx, decomposedDataArr) {
+    return (
+      decomposedDataArr[idx + 1] === separators.rest ||
+      decomposedDataArr[idx + 1] === separators.endOfArray
+    )
+  },
+
+  getTokenizedWord: function(letter, idx, decomposedDataArr) {
+    if (this.isSeparator(letter)) {
+      data = "";
+      return letter;
+    } else {
+      data += letter;
+      if (this.isEndofLiteral(idx, decomposedDataArr)) {
+        return data.trim();
+      }
+    }
+  },
+
+  joinLiterals: function(decomposedDataArr) {
+    return decomposedDataArr.map((letter, idx, arr) =>
+      this.getTokenizedWord(letter, idx, arr)
+    );
+  },
+
+  makeTokenizedData: function(decomposedDataArr) {
+    const literalsJoinedArr = this.joinLiterals(decomposedDataArr);
+
+    const result = literalsJoinedArr.filter(letter => letter !== undefined);
+    return result;
+  },
+
+  isString(literalStr) {
+    const literalStrLen = literalStr.length;
+    if(literalStr[0] === "'" &&
+      literalStr[literalStrLen-1] === "'") return true;
+  },
+
+  isCorrectStringForm(literalStr) {
+    const literalStrLen = literalStr.length;
+    let cnt = 0;
+    for(let i =0; i<literalStrLen; i++) {
+      if(literalStr[i] === "'") cnt += 1;
+    }
+
+    if(cnt === 2) return true;
+    else {
+      throw(`${literalStr}${errorMessages.INCORRECT_STRING}`);
+    }
+  },
+
+  getLiteralsType(word) {
+    if (Number.isFinite(Number(word))) {
+      return literals.number;
+    } else if (word === "false" || word === "true") {
+      return literals.boolean;
+    } else if (word === "null") {
+      return literals.null;
+    } else if(this.isString(word) && this.isCorrectStringForm(word)){
+      return literals.string;
+    } else {
+      throw(`${word}${errorMessages.UNKNOWN_TYPE}`);
+    }
+  }
+};
+
+class Parser {
+  constructor() {
+    this.tokenizedData = [];
+    this.lexedData = [];
+  }
+
+  tokenizing(unparsedData) {
+    //unparsedData를 모두 분해한 뒤, 토큰화(의미 있는 묶음으로 만듦)한다.
+    //결과를 tokenizedData에 저장
+    if (unparsedData === undefined) {
+      log(errorMessages.NO_PARSING_DATA);
+      return;
+    }
+    const decomposedDataArr = unparsedData.split("");
+    this.tokenizedData = parserUtils.makeTokenizedData(decomposedDataArr);
+  }
+
+  lexing() {
+    //tokenizedData의 요소들을 검사하여 의미를 부여한다.
+    //결과를 lexedData에 저장
+    if (this.tokenizedData === undefined) {
+      log(errorMessages.NO_TOKENIZED_DATA);
+      return;
+    }
+    const lexedObj = {};
+    this.lexedData = this.tokenizedData.map(word => {
+      if (!parserUtils.isSeparator(word)) {
+        lexedObj.type = parserUtils.getLiteralsType(word);
+        lexedObj.value = word;
+        lexedObj.child = []; //array인 경우 추가
+        return { ...lexedObj };
+      } else {
+        return word;
+      }
+    });
+  }
+
+  parsing(parsingDataObj) {
+    //구분자를 확인해서 JSON 객체 데이터 생성
+    let word = this.lexedData[0];
+    if(word === undefined) {
+      return;
+    }
+
+    this.lexedData.shift();
+    if (word === separators.endOfArray) {
+      const parentObj = parentObjStack.pop();
+      this.parsing(parentObj);
+
+    } else if (word === separators.startOfArray) {
+      const childObj = {
+        type: "array",
+        child: []
+      };
+      parsingDataObj.child.push(childObj);
+      parentObjStack.push(parsingDataObj);
+      this.parsing(childObj);
+
+    } else if (typeof word === "object") {
+      parsingDataObj.child.push(word);
+      this.parsing(parsingDataObj);
+
+    } else {
+      this.parsing(parsingDataObj);
+
+    }
+  }
+
+  array(unparsedArray) {
+    const unparsedData = unparsedArray;
+    this.tokenizing(unparsedData);
+    this.lexing();
+    const resultObj = {
+      child: []
+    };
+    this.parsing(resultObj);
+    const resultText = JSON.stringify(resultObj.child[0], null, 2);
+    log(resultText);
+  }
+}
+
+module.exports = Parser;
---
+++ parserTest.js
@@ -0,0 +1,26 @@
+const Parser = require("./Parser");
+const parser = new Parser();
+
+const correctData = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+
+const incorrectData1 = "['1a'3',[22,23,[11,[112233],112],55],33]";
+const incorrectData2 = "['1a3',[22,23,[11,[112233],112],55],3d3]";
+const incorrectData3 = "[1,2,3,4]]";
+
+try {
+  parser.array(correctData);
+  parser.array(incorrectData1); //'1a'3'은 올바른 문자열이 아닙니다.
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  parser.array(incorrectData2);  // 3d3은 알수 없는 타입입니다
+} catch (error) {
+  console.log(error);
+}
+try {
+  parser.array(incorrectData3);  // underflow
+} catch (error) {
+  console.log(error);
+}
