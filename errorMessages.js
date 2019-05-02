715fb0102613a76f420f40e17b749f14a13194cc
fed-gren <deveb22@gmail.com>
2019-05-02 18:02:28 +0900

    add: error message
    - 데이터 타입 검사, 문자열 검사 에러 메시지 추가

--- errorMessages.js
+++ errorMessages.js
@@ -1,4 +1,6 @@
 module.exports = {
   "NO_PARSING_DATA" : "파싱할 데이터가 없습니다.",
-  "NO_TOKENIZED_DATA" : "tokenizedData가 존재하지 않습니다. tokenizing이 먼저 진행되어야 합니다."
+  "NO_TOKENIZED_DATA" : "tokenizedData가 존재하지 않습니다. tokenizing이 먼저 진행되어야 합니다.",
+  "INCORRECT_STRING" : "은(는) 올바른 문자열이 아닙니다.",
+  "UNKNOWN_TYPE" : "은(는) 알 수 없는 타입입니다."
 }
