1ba1c7d86e4b0558442fa4814a1fce4cf9b0128e
aftersnowing <gurcks2@gmail.com>
2019-05-02 23:21:57 +0900

    lexer 메소드 구현

--- arrayParser.js
+++ arrayParser.js
@@ -7,7 +7,25 @@ class ArrayParser {
             isNaN(Number(cur)) !== 'number' ? acc.push(...cur.match(/\[|\]|\d+/g)) : acc.push(cur)
             return acc
         }, []);
-        this.tokenizedData = splitedStr.slice(1,splitedStr.length - 1)
+        this.tokenizedData = splitedStr
         
+    }
+
+    lexer(arr) {
+        const stack = [];
+        let pointer = -1;
+        arr.forEach((val) => {
+            // console.log(stack)
+            if(val === '[') {
+                pointer++;
+                stack.push([]);
+            }else if (!isNaN(Number(val))) {
+                stack[pointer].push(val);
+            }else if(val === ']' && pointer !== 0) {
+                pointer--;
+                stack[pointer].push(stack.pop());
+            }
+        });
+        this.lexedData = stack[0];
     }
 }
