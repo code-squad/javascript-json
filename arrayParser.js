88f9dd93b944a7e019a43e7cfb3ea2ea1e1032f6
nailerHeum <kingman330@gmail.com>
2019-05-02 16:11:52 +0900

    Implement simple arrayParse method
    한개의 배열안에 숫자들에 대한 정보를 돌려주는 간단한 arrayParser구현

--- arrayParser.js
+++ arrayParser.js
@@ -5,22 +5,42 @@ const rl = readline.createInterface({
   output : process.stdout
 });
 
-const main = () => {
-  rl.question('ArrayParser 사용자 입력 : ', (userInput) => {
-   
-    console.log(userInput)
 
-    rl.close();
-  });
-}
 
 class ArrayParser{
   constructor(){
 
   }
-
+  arrayParse(input){
+    input = input.trim()
+    if(input[0] === '[' && input[input.length-1] === ']'){
+      let result = {type: 'array', child: []}
+      let token = input.slice(1,input.length-1)
+      
+      const elements = token.split(',')
+      elements.forEach(element => {
+        result.child.push(this.arrayParse(element))
+      });
+      return result
+    }
+    if(!isNaN((input.trim())*1)){
+      let result = {type: 'number', value: input.trim()}
+      return result
+    }
+    throw new Error('Invalid Token')
+  }
 }
 
-const arrayParser = new ArrayParser()
+const arrayParser = new ArrayParser();
 
-main()
+(() => {
+  rl.question('ArrayParser 사용자 입력 : ', (userInput) => {
+    try{
+      let result = arrayParser.arrayParse(userInput)
+      console.log(result)
+    }catch(e){
+      console.log(e.message)
+    }
+    rl.close();
+  });
+})()
