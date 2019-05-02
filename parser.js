0dfc595ffeb2497a41d663bd4348d3d71e79b78a
Min-92 <okssud1@naver.com>
2019-05-02 20:26:37 +0900

    feat : add parser.js
    - 수도코드 작성

---
+++ parser.js
@@ -0,0 +1,30 @@
+
+class Arraypaser {
+    constructor(){
+        this.bracketStack = [];
+    }
+    tokenizer(inputString){
+        // 스트링을 배열에 하나씩 넣기
+        // 공백은 제외
+        return array
+    }
+
+    lexer(inputArray){
+        //리터럴은 리터럴끼리, operator, seperate 끼리 묶어서 한 index에 넣기
+        //객체로 type, value 나눠서 넣기
+        return array
+    }
+
+    parser(inputArray, inputIndex = 0, resultArray){
+        // [ 만나면 스택에 추가
+        // 배열 생성
+        // ] 만나면 배열 닫기
+        // 숫자 만나면 배열에 추가
+        // 쉼표만나면 인덱스 넘기기
+    
+        return inputArray,inputIndex,resultArray
+
+    }
+
+
+}
