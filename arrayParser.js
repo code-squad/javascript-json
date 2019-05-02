2b76b0c11a36d74675637161e24f2dc1c24009fa
aftersnowing <gurcks2@gmail.com>
2019-05-02 22:23:05 +0900

    tokenizer 메소드 구현

--- arrayParser.js
+++ arrayParser.js
@@ -2,9 +2,12 @@ class ArrayParser {
     constructor() {
     }
     tokenizer(str) {
-        this.tokenizedData = str.split(', ');
+        const splitedStr = str.split(', ')
+        .reduce((acc, cur) => {
+            isNaN(Number(cur)) !== 'number' ? acc.push(...cur.match(/\[|\]|\d+/g)) : acc.push(cur)
+            return acc
+        }, []);
+        this.tokenizedData = splitedStr.slice(1,splitedStr.length - 1)
+        
     }
 }
-
-
-c
