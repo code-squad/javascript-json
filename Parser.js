81ba1d08ae7a382feaa4b556b8091d4cea94fa8f
Gren <39091443+fed-gren@users.noreply.github.com>
2019-05-02 11:50:38 +0900

    Step6-1 array parser 구현 (#137)
    * feat: create class Parser
    
    - parsing을 위한 클래스 Parser 기본 구조 작성
    - tokenizing -> lexing -> parsing 순서로 진행
    - array 메서드에 파싱할 배열 형태 데이터 인자로 넣으면 파싱 진행
    
    * feat: create file to test Parser
    
    - Parser 클래스 동작 확인용 파일 생성
    
    * feat: create method to tokenizing
    
    - 데이터 분해하기 위해 split 사용해서 배열에 저장
    - 배열에 있는 데이터 내 literal들을 parserUtils.joinLiterals 메서드로 합침
    - 구분자 확인을 위해 isSeparator 메서드와 separators 모듈 작성
    
    * feat: create module to error messages
    
    - 에러 메시지를 위한 모듈 작성 및 반영
    
    * feat: create method to lexing
    
    - 구분자를 제외한 literal에 대해 의미 부여
    - 유효한 literal 타입에 대한 모듈 작성 : literals.js
    - lexing에서 사용할 errorMessage 추가
    
    * feat: create method to parsing
    
    - 구분자 확인해서 JSON 객체 형태로 데이터 작성하는 메서드 추가: parsing

---
+++ Parser.js
@@ -0,0 +1,124 @@
+const separators = require("./separators");
+const literals = require("./literals");
+const errorMessages = require("./errorMessages");
+
+const { log } = console;
+
+const parserUtils = {
+  isSeparator: function(letter) {
+    for (let separator of Object.values(separators)) {
+      if (letter === separator) return true;
+    }
+    return false;
+  },
+
+  joinLiterals: function(decomposedDataArr) {
+    let data = "";
+    const literalsJoinedArr = decomposedDataArr
+      .map((letter, idx) => {
+        if (this.isSeparator(letter)) {
+          data = "";
+          return letter;
+        } else {
+          data += letter;
+          if (
+            idx < decomposedDataArr.length - 1 &&
+            (decomposedDataArr[idx + 1] === separators.rest ||
+              decomposedDataArr[idx + 1] === separators.endOfArray)
+          ) {
+            return data.trim();
+          }
+        }
+      })
+      .filter(letter => letter !== undefined);
+    return literalsJoinedArr;
+  },
+
+  getLiteralsType(word) {
+    if (Number.isFinite(Number(word))) {
+      return literals.number;
+    } else if (word === "false" || word === "true") {
+      return literals.boolean;
+    } else if (word === "null") {
+      return literals.null;
+    } else {
+      return literals.string;
+    }
+  }
+};
+
+class Parser {
+  constructor() {
+    this.tokenizedData = [];
+    this.lexedData = [];
+  }
+
+  tokenizing() {
+    //unparsedData를 모두 분해한 뒤, 토큰화(의미 있는 묶음으로 만듦)한다.
+    //결과를 tokenizedData에 저장
+    if (this.unparsedData === undefined) {
+      log(errorMessages.NO_PARSING_DATA);
+      return;
+    }
+    const decomposedDataArr = this.unparsedData.split("");
+    this.tokenizedData = parserUtils.joinLiterals(decomposedDataArr);
+  }
+
+  lexing() {
+    //tokenizedData의 요소들을 검사하여 의미를 부여한다.
+    //결과를 lexedData에 저장
+    if(this.tokenizedData === undefined) {
+      log(errorMessages.NO_TOKENIZED_DATA);
+      return;
+    }
+    const lexedObj = {};
+    this.lexedData = this.tokenizedData.map(word => {
+      if (!parserUtils.isSeparator(word)) {
+        lexedObj.type = parserUtils.getLiteralsType(word);
+        lexedObj.value = word;
+        lexedObj.child = []; //array인 경우 추가
+        return { ...lexedObj };
+      } else {
+        return word;
+      }
+    });
+  }
+
+  parsing(parsingDataObj) {
+    //구분자를 확인해서 JSON 객체 데이터 생성
+    let word = this.lexedData[0];
+    if(word === separators.endOfArray) {
+      this.lexedData.shift();
+      return;
+    } else if(word === separators.startOfArray) {
+      const newArrObj = {
+        type:"array",
+        child:[]
+      }
+      parsingDataObj.child.push(newArrObj);
+      this.lexedData.shift();
+      this.parsing(newArrObj);
+    } else if (typeof word === "object") {
+      parsingDataObj.child.push(word);
+      this.lexedData.shift();
+      this.parsing(parsingDataObj);
+    } else {
+      this.lexedData.shift();
+      this.parsing(parsingDataObj);
+    }
+  }
+
+  array(unparsedArray) {
+    this.unparsedData = unparsedArray;
+    this.tokenizing();
+    this.lexing();
+    const resultObj = {
+      child: []
+    }
+    this.parsing(resultObj);
+    const resultText = JSON.stringify(resultObj.child[0], null, 2);
+    log(resultText);
+  }
+}
+
+module.exports = Parser;
