33400453dfb7b1c0aeafbc4528b96e22fdfb7bb6
MCC <cocahack@naver.com>
2019-05-02 23:04:37 +0900

    feat: 프로그램 진입점 추가

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const ArrayParser = require('./arrayParser');
+const rl = readline.createInterface(
+  { input: fs.createReadStream(path.join(__dirname, './testcase/test1.in')),
+  }
+  );
+
+rl.on('line', line => {
+  try{
+    const arrayParser = new ArrayParser(line);
+    const result = arrayParser.analysis();
+    console.log(JSON.stringify(result, null, 2));
+  } catch(error){
+    console.error(`Error: ${error.message}`);
+  }
+});
