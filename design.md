aeaa03a6154ad0ecb700ea4d334d3ca8bcbe0f29
park78951 <park78951@gmail.com>
2019-05-02 19:07:13 +0900

    create lexer, parser methods and design.md

---
+++ design.md
@@ -0,0 +1,99 @@
+# parser 만들기 design
+
+## 1차 design
+1. input readline 설정하여 input 받도록.
+2. 실행함수 `init()` 생성
+3. 입력받은 문자열 token 단위로 쪼개는 함수 `tokenizer` 생성
+4. 같은 type끼리 묶는 lexer 함수 `lexer()` 생성
+5. 전체 type 설정 및 최종 value를 반환하는 함수 `parser()`생성
+6. 결과값 type, value 등을 출력하는 함수 `print()` 생성
+
+## 2차 design
+1. 실행함수 `init()` 생성
+   - parser 함수 호출
+   - input받은 문자열 전달
+2. 같은 type끼리 묶는 lexer 함수 `lexer()` 생성
+   - 입력 받은 문자열 split하여 배열, 객체 구분 기호를 나누고, space와 comma 등의 불필요한 기호는 filtering 하는 작업.
+   - 배열에 각 구분 기호, 문자열들을 차례대로 요소로 push.
+   - 연속된 문자를 판단하고 묶어 하나의 문자열로 반환하는 작업.
+3. 전체 type 설정 및 최종 value를 반환하는 함수 `parser()`생성
+   - 입력받은 문자열을 판독하여 구분 기호인지, 문자인지, 숫자인지를 판단 후 나누는 작업.
+4. `print()`
+   - 배출 타입, 각 value의 타입, value 를 출력하도록 console.log 메소드 사용
+
+## 3차 design
+
+1. node input 가능 코드
+   
+```js
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+```
+
+2. `init()` sudo
+
+```js
+const init =  함수() {
+readline.question('문구', (input) => {
+    parser(input)
+})
+parser(input);
+}
+```
+
+3. `lexer()` sudo
+
+```js
+const lexer = 함수(input) {
+    첫번째변수명 = input 쪼개기 (split('') 사용);
+    두번째변수명 = 구분기호 배열
+    세번째변수명 = 숫자 배열
+    네번째변수명 = 알파벳 배열
+    다섯번째변수명 = 첫번째변수명.reduce() -> '[', ']', '{', '}'는 요소의 가장 앞과 끝에 위치하도록 배치
+    '[', ']', '{', '}' 내부의 요소들 중 연속된 요소들을 묶는 작업 진행 (string과 number 구분)
+    return 다섯번째변수명
+}
+```
+
+4. `parser()` sudo
+
+```js
+const parser = 함수(input) {
+    첫번째변수명 = lexer(input);
+    두번째변수명;
+    세번째변수명 = 숫자배열
+    네번째변수명 = 첫번째변수명.map( letter => {
+        if(letter === '[' or '{') 두번째변수명 배열선언
+        if(letter === ']'or '}') return
+        if(letter === 세번째배열.includes()) parseInt();
+    })
+    print함수(네번재변수명)
+}
+```
+
+```js
+const print(배열또는객체) {
+    첫번째변수 = isArray() || typeof
+    두번째변수 = {type: 첫번재변수의 타입
+        child: []
+    }
+
+    if(첫번째변수 === array) {
+        배열또는객체.forEach(n => {
+            _첫번째변수 = typeof(n)
+            _두번째변수 = n
+            두번째변수.child.push({type: _첫번째변수, value: _두번째변수, child: []})
+        })
+    }
+
+    if(첫번째변수 === object) {
+        for(key in 첫번째변수)
+            _첫번째변수 = typeof(첫번째변수[key])
+            두번째변수.child.push({type: _첫번째변수, key: key, value: 첫번재변수[key]})
+    }
+    console.log(두번째변수)
+}
+```
--- scripts.js
+++ scripts.js
@@ -1,64 +1,62 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+import { strict } from "assert";
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
-const init = () => {
-  rl.question('분석할 JSON 데이터를 입력하세요. ', (answer) => {
-    let arr = answer.split(' ');
-    let res = [];
-    arrays(arr, res);
-    console.log(result(res));
-    rl.close();
-  });
-}
+// const init = () => {
+//   rl.question('분석할 JSON 데이터를 입력하세요. ', (answer) => {
+//     rl.close();
+//   });
+// }
 
-
-const arrays = (arr, res) => {
-  if (arr[0] === '[' && arr[arr.length - 1] === ']') {
-    for (let i = 1; i < arr.length - 1; i++) {
-      arr[i].match(',') ? res.push(arr[i].replace(',', "")) : res.push(arr[i]);
+const lexer = (inputString) => {
+  const splitLetters = inputString.split('');
+  const parsedLetters = [];
+  const arraySetters = ["[", "]"];
+  const seperators = [",", " "]
+  // const numberSetter = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  // const stringSetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  
+  splitLetters.reduce( (acc, cur) => {
+    if(arraySetters.includes(cur)) {
+      return "";
     }
-  }
-  return res;
-}
-
-
-const types = {
-  string: function (res) {
-    let strs = [];
-    for (let val of res) {
-      if (val.match('"')) {
-        strs.push(val.replace('"', "").replace('"', ""));
+    if(seperators.includes(acc)) {
+      return cur;
+    } else {
+      if(seperators.includes(cur)) {
+        parsedLetters.push(acc)
+        return cur;
+      } else {
+        return acc+cur;
       }
     }
-    return strs.length;
-  },
-  numbers: function (res) {
-    let nums = [];
-    for (let val of res) {
-      if (!isNaN(Number(val))) {
-        nums.push(Number(val));
-      }
-    }
-    return nums.length;
-  },
-  bools: function (res) {
-    let bools = [];
-    for (let val of res) {
-      if (val === "false" || val === "true") {
-        bools.push(val);
-      }
-    }
-    return bools.length;
-  }
+  },"");
+  return parsedLetters;
 }
 
-const result = (res) => {
-  return "총 " + res.length + "개의 데이터 중에 문자열 " + types.string(res) + "개, 숫자 " + types.numbers(res) + "개, 부울 " + types.bools(res) + "개가 포함되어 있습니다.";
+const parser = (inputString) => {
+  const lexedItem = lexer(inputString);
+  const numberSetter = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const result = lexedItem.map( letter => {
+    for(let i = 0; i < letter.length; i++) {
+      if(!numberSetter.includes(letter)) {
+        return letter;
+      } else {
+        return Number(letter);
+      }
+    }
+  })
+  print(result);
 }
 
+const print = () => {
+  
+}
 
-init();
+console.log(lexer("[123, 123, 123, 123]"))
+//"[123, 123, 123, 123]"
