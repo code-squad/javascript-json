628e22cf961ccb82b9de7dfaeab881eec5cf2a60
mukeunzi <eunji057@gmail.com>
2019-05-03 12:21:38 +0900

    feat : parser 메소드 생성
    - '[', ']', ',' 특수문자 제거
    - childObj에 결과값 생성
    - module로 exports

--- arrayParser.js
+++ arrayParser.js
@@ -9,11 +9,38 @@ class ArrayParser {
 		return strToken;
 	}
 
+	parser(strToken) {
+		const delSignToken = [];
+
+		if (strToken[0].includes('[') && strToken[strToken.length - 1].includes(']')) {
+			const signRegExp = /^\[|\]$|\,/g;
+
+			for (let str of strToken) {
+				delSignToken.push(str.replace(signRegExp, ''));
+			}
+
+			this.result.type = 'array';
+		}
+
+		for (let token of delSignToken) {
+			let childObj = {};
+			if (!isNaN(token)) {
+				token = Number(token);
+			}
+			childObj['type'] = typeof token;
+			childObj['value'] = token;
+			childObj['child'] = [];
+
+			this.result.child.push(childObj);
+		}
+	}
+
 	printResult() {
 		console.log(this.result);
 	}
 }
 
-const str = '[123, 22, 33]';
-const myArrayParser = new ArrayParser();
-myArrayParser.printResult();
+module.exports = ArrayParser;
+// const str = '[123, 22, 33]';
+// const myArrayParser = new ArrayParser();
+// myArrayParser.arrayParser(str);
