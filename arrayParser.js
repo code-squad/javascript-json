6f74033a34aab2ea4cc6af805bd93ea8feaac6df
aftersnowing <gurcks2@gmail.com>
2019-05-03 12:11:32 +0900

    lexer 구현

--- arrayParser.js
+++ arrayParser.js
@@ -10,6 +10,23 @@ class ArrayParser {
         }, []);
         this.tokenizedData = splitedStr;
     }
+
+    lexer(arr) {
+        let pointer = -1;
+        const stack = arr.reduce((acc, cur) => {
+            if(cur === '[') {
+                pointer++;
+                acc.push([]);
+            }else if (!isNaN(Number(cur))) {
+                acc[pointer].push(cur);
+            }else if(cur === ']' && pointer !== 0) {
+                pointer--;
+                acc[pointer].push(acc.pop());
+            }
+            return acc
+        }, []);
+        this.lexedData = stack[0];
+    }
 }
 
 const arrayParser = new ArrayParser();
