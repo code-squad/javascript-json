65dfb90f4df500d9c8564cfa818b2f0138f39700
Ryujeuk <jeuk817@gmail.com>
2019-05-03 18:31:05 +0900

    feat: tokenizer메소드 구현
    문자로 들어온 배열을 각각의 토큰으로 구분하기위해 tokenizer메소드 구현
    - 공백을 제거하고 대괄호에 쉼표를 추가하여 쉼표로 split함.

--- ArrayParser.js
+++ ArrayParser.js
@@ -1,5 +1,10 @@
 const str = "[123, 12, [22, [55, 66], 4, 33], 44]";
 
 class ArrayParser {
-
+    tokenizer(arr) {
+        arr = arr.split(' ').join(''); // arr = arr.replace(/ /g, '');
+        arr = arr.replace(/\[/g, '[,');
+        arr = arr.replace(/\]/g, ',]');
+        return arr.split(',');
+    }
 }
