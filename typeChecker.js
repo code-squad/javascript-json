a2328a018715e134a3ae573fb975cca1c30b8a52
P-iknow <apricotsoul@gmail.com>
2019-05-03 13:42:57 +0900

    Fix - typeChecker 객체  수정

--- typeChecker.js
+++ typeChecker.js
@@ -1,43 +1,44 @@
-//
-function isQuote (value) {
-    return value === "\'" || value === "\""
+const typeChecker = {
+    isQuote (value) {
+        return value === "\'" || value === "\""
+    },
+    
+    isBraket (value){
+        return value === "[" || value === "]"
+    },
+    
+    isComma (value){
+        return value === ","
+    },
+    
+    isWhiteSpace (value){
+        return /\s/.test(value);
+    },
+    
+    // Returns if a value is really a number
+    isNumber (value) {
+        return /[0-9]/.test(value);
+    },
+    
+    // Returns if a value is a string
+    isString (value) {
+        return /[a-z]/i.test(value)
+    },
+    
+    
+    // Returns if a value is null
+    isNull (value) {
+        return value === 'null';
+    },
+    
+    // Returns if a value is undefined
+    isUndefined (value) {
+        return value === 'undefined';
+    },
+    
+    // Returns if a value is a boolean
+    isBoolean (value) {
+        return value === 'true' || value === "false";
+    },   
 }
-
-function isBraket (value){
-    return value === "[" || value === "]"
-}
-
-function isComma (value){
-    return value === ","
-}
-
-function isWhiteSpace (value){
-    return /\s/.test(value);
-}
-
-// Returns if a value is really a number
-function isNumber (value) {
-    return /[0-9]/.test(value);
-}
-
-// Returns if a value is a string
-function isString (value) {
-    return /[a-z]/i.test(value)
-}
-
-
-// Returns if a value is null
-function isNull (value) {
-    return value === 'null';
-}
-
-// Returns if a value is undefined
-function isUndefined (value) {
-    return value === 'undefined';
-}
-
-// Returns if a value is a boolean
-function isBoolean (value) {
-    return value === 'true' || value === "false";
-}
-
+exports.module = typeChecker;
