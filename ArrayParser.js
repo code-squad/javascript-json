e0c607d116291edff5ba81c5cc139cd28bf0ec83
Ryujeuk <jeuk817@gmail.com>
2019-05-03 13:38:53 +0900

    feat: 무한중첩 구현완료, 하지만 lexer를 구현 안함.
    재귀로 무한중첩을 구현하였지만, 다른사람의 코드를 보니 제가 lexer를
    구현하지 않았다는 것을 깨달음.
    다시 만들 필요가 있음.

--- ArrayParser.js
+++ ArrayParser.js
@@ -1,6 +1,4 @@
 const str = "[123, 12, [22, [3, 5], [55, 66], 4, 33], 44]";
-const result = ArrayParser(str);
-console.log(JSON.stringify(result, null, 2));
 
 //result는 해당 배열을 분석한 형태이다.
 //예를들어, 다음과 같은 결과일 수 있다. (꼭 아래 형태일 필요 없음)
@@ -13,49 +11,57 @@ console.log(JSON.stringify(result, null, 2));
 //     ]
 // }
 
-function ArrayParser(arrString) {
-    function tokenizer(str) {
-        str = str.replace(/\[/g, '[, ');
-        str = str.replace(/\]/g, ', ]');
-        return str.split(', ');
+class ArrayParser {
+    tokenizer(arr) {
+        let newArr = arr.replace(/\[/g, '[, ');
+        let newArr2 = newArr.replace(/\]/g, ', ]');
+        return newArr2.split(', ');
     }
 
-    function findLittleArray(arr) {
+    findLittleArray(arr) {
         const endBracket = arr.indexOf(']')
         const startBracket = arr.lastIndexOf('[', endBracket);
         return [startBracket, endBracket];
     }
 
-    function makeElementObject(element) {
+    makeElementObject(element) {
         return typeof element === 'object' ? element : { type: 'number', value: element, child: [] };
     }
 
-    function makeArrayObject(arr, index) {
-        [startBracket, endBracket] = index;
+    makeArrayObject(arr, index) {
+        const [startBracket, endBracket] = index;
         const arrayObject = {
             type: 'array',
             child: []
         }
         for (let i = startBracket + 1; i < endBracket; i++) {
-            arrayObject.child.push(makeElementObject(arr[i]));
+            arrayObject.child.push(this.makeElementObject(arr[i]));
         }
         return arrayObject;
     }
 
-    function joinElements(arr, arrayObject, index) {
-        [startBracket, endBracket] = index;
+    joinElements(arr, arrayObject, index) {
+        const [startBracket, endBracket] = index;
         const numberOfElements = endBracket - startBracket + 1;
         arr.splice(startBracket, numberOfElements, arrayObject);
         return arr;
     }
 
-    function runArrayParser(arrString) {
-        if (arrString.length === 1) return arrString[0];
-        const arr = tokenizer(arrString);
-        const index = findLittleArray(arr);
-        const arrayObject = makeArrayObject(arr, index);
-        arr = joinElements(arr, arrayObject, index);
-        runArrayParser(arr);
+    start(str) {
+        const arr = this.tokenizer(str);
+        return this.runArrayParser(arr);
+    }
+
+    runArrayParser(arr) {
+        if (arr.length === 1) return arr[0];
+        const index = this.findLittleArray(arr);
+        const arrayObject = this.makeArrayObject(arr, index);
+        arr = this.joinElements(arr, arrayObject, index);
+        return this.runArrayParser(arr);
     }
 
 }
+
+const arrayParser = new ArrayParser();
+// "[123, 12, [22, [3, 5], [55, 66], 4, 33], 44]"
+console.log(arrayParser.start(str));
