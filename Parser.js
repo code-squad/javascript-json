845e36dc7e9f85d55cac3babf8a5c05bf0b15b87
Gren <39091443+fed-gren@users.noreply.github.com>
2019-05-03 16:57:04 +0900

    Step 6-2 Array parser - 무한중첩 (#149)
    * add: step 6-2
    
    * refactor: change loop statement to reduce
    
    - 인자로 들어오는 문자열 배열로 변환 후 reduce 사용
    
    * refactor: remove function keyword in parserUtils object
    
    - 객체 내 메서드 선언 시 function 키워드 선언하지 않아도 되므로 삭제
    
    * refactor: change 'data' variable to 'tokenizedWord' property
    
    - 키워드 없이 선언해서 암묵적으로  전역변수로  사용되던 data 변수 이름 수정 후 tokenizedWord 프로퍼티로 변경
    
    * refactor: remove properties in Parser class
    
    - 데이터 흐름 명확히 하기 위해 프로퍼티 대신 array 메서드에서 변수 선언해서 사용
    
    * refactor: remove needless bracket and log
    
    - 조건문 한 줄로 축소 및 불필요한 log 삭제
    
    * refactor: create isCorrectString method for facade
    
    - 같이 쓰이는 isString(), isCorrectStringForm() 메서드 묶어서 호출하는 isCorrectString() 메서드 작성
    
    * refactor: modify logic of isCorrectStringForm method
    
    - 문자열 배열로 수정 후 reduce 사용 -> 문자열 for of로 순회
    
    * refactor: modify name of method, variable
    
    - array 뿐만 아니라, json 형태의 데이터는 모두 파싱되도록 하는 것이 목표이므로 목적에 맞게 메서드명, 변수명 수정
    
    * add: test example

---
+++ Parser.js
@@ -0,0 +1,150 @@
+const separators = require("./separators");
+const literals = require("./literals");
+const errorMessages = require("./errorMessages");
+const Stack = require("./Stack");
+const parentObjStack = new Stack();
+
+const { log } = console;
+
+const parserUtils = {
+  tokenizedWord : "",
+  isSeparator(letter) {
+    for (let separator of Object.values(separators)) {
+      if (letter === separator) return true;
+    }
+    return false;
+  },
+
+  isEndofLiteral(idx, decomposedDataArr) {
+    return (
+      decomposedDataArr[idx + 1] === separators.rest ||
+      decomposedDataArr[idx + 1] === separators.endOfArray
+    );
+  },
+
+  getTokenizedWord(letter, idx, decomposedDataArr) {
+    if (this.isSeparator(letter)) {
+      this.tokenizedWord = "";
+      return letter;
+    } else {
+      this.tokenizedWord += letter;
+      if (this.isEndofLiteral(idx, decomposedDataArr)) {
+        return this.tokenizedWord.trim();
+      }
+    }
+  },
+
+  joinLiterals(decomposedDataArr) {
+    return decomposedDataArr.map((letter, idx, arr) =>
+      this.getTokenizedWord(letter, idx, arr)
+    );
+  },
+
+  makeTokenizedData(decomposedDataArr) {
+    const literalsJoinedArr = this.joinLiterals(decomposedDataArr);
+    return literalsJoinedArr.filter(letter => letter !== undefined);
+  },
+
+  isString(literalStr) {
+    const literalStrLen = literalStr.length;
+    if (literalStr[0] === "'" && literalStr[literalStrLen - 1] === "'")
+      return true;
+  },
+
+  isCorrectStringForm(literalStr) {
+    let cnt = 0;
+    for(letter of literalStr) {
+      cnt = (letter === "'") ? cnt + 1 : cnt;
+    }
+
+    if (cnt === 2) return true;
+    else throw `${literalStr}${errorMessages.INCORRECT_STRING}`;
+  },
+
+  isCorrectString(word) {
+    return this.isString(word) && this.isCorrectStringForm(word)
+  },
+
+  getLiteralsType(word) {
+    if (Number.isFinite(Number(word))) {
+      return literals.number;
+    } else if (word === "false" || word === "true") {
+      return literals.boolean;
+    } else if (word === "null") {
+      return literals.null;
+    } else if (this.isCorrectString(word)) {
+      return literals.string;
+    } else {
+      throw `${word}${errorMessages.UNKNOWN_TYPE}`;
+    }
+  }
+};
+
+class Parser {
+  tokenizing(unparsedJson) {
+    //unparsedData를 모두 분해한 뒤, 토큰화(의미 있는 묶음으로 만듦)한다.
+    if (unparsedJson === undefined) {
+      log(errorMessages.NO_PARSING_DATA);
+      return;
+    }
+    const decomposedDataArr = unparsedJson.split("");
+    return parserUtils.makeTokenizedData(decomposedDataArr);
+  }
+
+  lexing(tokenizedJson) {
+    //tokenizedData의 요소들을 검사하여 의미를 부여한다.
+    if (tokenizedJson === undefined) {
+      log(errorMessages.NO_TOKENIZED_DATA);
+      return;
+    }
+    const lexedObj = {};
+    return tokenizedJson.map(word => {
+      if (!parserUtils.isSeparator(word)) {
+        lexedObj.type = parserUtils.getLiteralsType(word);
+        lexedObj.value = word;
+        lexedObj.child = []; //array인 경우 추가
+        return { ...lexedObj };
+      } else {
+        return word;
+      }
+    });
+  }
+
+  parsing(lexedJson, parsingDataObj) {
+    //구분자를 확인해서 JSON 객체 데이터 생성
+    let word = lexedJson[0];
+    if (word === undefined) return;
+
+    lexedJson.shift();
+    if (word === separators.endOfArray) {
+      const parentObj = parentObjStack.pop();
+      this.parsing(lexedJson, parentObj);
+    } else if (word === separators.startOfArray) {
+      const childObj = {
+        type: "array",
+        child: []
+      };
+      parsingDataObj.child.push(childObj);
+      parentObjStack.push(parsingDataObj);
+      this.parsing(lexedJson, childObj);
+    } else if (typeof word === "object") {
+      parsingDataObj.child.push(word);
+      this.parsing(lexedJson, parsingDataObj);
+    } else {
+      this.parsing(lexedJson, parsingDataObj);
+    }
+  }
+
+  getJson(unparsedJson) {
+    const tokenizedJson = this.tokenizing(unparsedJson);
+    const lexedJson = this.lexing(tokenizedJson);
+    const resultObj = {
+      child: []
+    };
+    this.parsing(lexedJson, resultObj);
+    const resultText = JSON.stringify(resultObj.child[0], null, 2);
+    log(resultText);
+  }
+}
+
+module.exports = Parser;
