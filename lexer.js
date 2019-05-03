4237ca2c2dc84257f07c764a311e9b93e7aeccb3
allen <revenoir7@gmail.com>
2019-05-03 12:19:59 +0900

    feat: string, boolean, null 타입도 파싱할 수 있도록 Lexer.decideType메서드 수정

--- lexer.js
+++ lexer.js
@@ -5,12 +5,31 @@ class Lexer {
         if (token === '[') {
             return new Node('array');
         }
+        if (this.isString(token)) {
+            return new Node('string', token);
+        }
         if (isFinite(token) && token !== null) {
             return new Node('number', token);
         }
+        if (token === 'true' || token === 'false') {
+            return new Node('boolean', token);
+        }
+        if (token === 'null') {
+            return new Node('null');
+        }
         if (token === ']') {
             return new Node('end');
+        } else {
+            throw Error(`${token}은 알수 없는 타입입니다.`);
         }
     }
+
+    isString(token) {
+        if (token.startsWith("'") && token.endsWith("'")) {
+            return true;
+        }
+        return false;
+    }
+
 }
 module.exports = Lexer;
