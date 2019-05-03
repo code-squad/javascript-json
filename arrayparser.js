b9eadd9f717b6d8d1ea1f51bb9d43c23757596cf
leehwarang <hwrng2@gmail.com>
2019-05-03 13:53:54 +0900

    refactor Arrayparser의 lexer와 tokenizer 함수 구현 수정

--- arrayparser.js
+++ arrayparser.js
@@ -1,48 +1,37 @@
-// 예시
-// const str = "[123, 22, 33]";
-// const result = ArrayParser(str); //str을 토큰 단위로 구분한 형태(객체로 만들면 좋을 듯)
-// console.log(JSON.stringify(result, null, 2)); //토큰 단위로 구분된 것을 분석하는 것
+const Token = require("./token.js");
 
-// //result는 해당 배열을 분석한 형태이다.
-// //예를들어, 다음과 같은 결과일 수 있다. (꼭 아래 형태일 필요 없음)
+class ArrayParser {
+  removeWhiteSpace(inputStr) {
+    inputStr = inputStr.replace(/\s/g, "");
+    return inputStr;
+  }
 
-// { type: 'array',
-//   child:
-//    [ { type: 'number', value: '123', child: [] },
-//      { type: 'number', value: '22', child: [] },
-//      { type: 'number', value: '33', child: [] }
-//     ]
-// }
-
-const Token = require("./token.js/index.js");
-
-const str = "[123, 22, 33]".replace(/\s/g, "");
-const first = str[0];
-
-if (first === "[") {
-  const token = new Token("Array", "", [], 1);
-  parser(str, token);
-}
-
-function parser(str, token) {
-  while (token.cnt !== 0) {
-    word = "";
-    for (let i = 1; i < str.length; i++) {
-      if (str[i] === ",") {
-        token.child.push(new Token(typeof Number(word), word, [], 0));
-        word = "";
-      } else if (str[i] === "]") {
-        token.child.push(new Token(typeof Number(word), word, [], 0));
-        token.cnt -= 1;
-      } else if (str[i] === "[") {
-        const token = new Token("Array", "", [], 0);
-        // token.cnt += 1;
-        // str = str[i+1]
-        return parser(str, token);
-      } else {
-        word += str[i];
+  tokenizer(inputStr) {
+    const splitedStr = inputStr.split(",");
+    console.log(splitedStr);
+    splitedStr.forEach(element => {
+      for (let char of element) {
+        if (char === "[") {
+          this.lexer(char);
+          element = element.replace(char, "");
+        } else if (char === "]") {
+          element = element.replace(char, "");
+        }
       }
+      this.lexer(element);
+    });
+  }
+
+  lexer(token) {
+    if (token === "[") {
+      Token.prototype.tokenArr.push(new Token("array", "", []));
+    } else {
+      Token.prototype.tokenArr.push(new Token("number", token, []));
     }
   }
-  console.log(token);
 }
+
+const parser = new ArrayParser();
+const tergetStr = parser.removeWhiteSpace("[123, 22, 33]");
+parser.tokenizer(tergetStr);
+console.log(Token.prototype.tokenArr);
