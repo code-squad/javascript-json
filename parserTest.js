f3a3bfc97f8c56d47fcc306b1acac7a4ac9e6a97
fed-gren <deveb22@gmail.com>
2019-05-03 16:30:12 +0900

    add: test example

--- parserTest.js
+++ parserTest.js
@@ -2,25 +2,32 @@ const Parser = require("./Parser");
 const parser = new Parser();
 
 const correctData = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+const correctData2 = "[1,2,3,4]";
 
 const incorrectData1 = "['1a'3',[22,23,[11,[112233],112],55],33]";
 const incorrectData2 = "['1a3',[22,23,[11,[112233],112],55],3d3]";
 const incorrectData3 = "[1,2,3,4]]";
 
 try {
-  parser.array(correctData);
-  parser.array(incorrectData1); //'1a'3'은 올바른 문자열이 아닙니다.
+  parser.getJson(correctData);
+  parser.getJson(incorrectData1); //'1a'3'은 올바른 문자열이 아닙니다.
 } catch (error) {
   console.log(error);
 }
 
 try {
-  parser.array(incorrectData2);  // 3d3은 알수 없는 타입입니다
+  parser.getJson(incorrectData2);  // 3d3은 알수 없는 타입입니다
 } catch (error) {
   console.log(error);
 }
 try {
-  parser.array(incorrectData3);  // underflow
+  parser.getJson(incorrectData3);  // underflow
+} catch (error) {
+  console.log(error);
+}
+
+try {
+  parser.getJson(correctData2);
 } catch (error) {
   console.log(error);
 }
