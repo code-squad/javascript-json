efa03e713dbeef94d8766d9a6cfa446a07375721
kkw_10 <kkw_10@naver.com>
2019-05-03 11:08:00 +0900

    mainParser함수 생성 및 정리

--- parsing6-1.js
+++ parsing6-1.js
@@ -5,21 +5,21 @@ class ParsingForm {
         this.child = [];
     }
 } 
+
 class ArrayParser {
     constructor(input) {
         this.rawData = input;
     }
 
     tokenizing() {
-        //문자열 분리
+        //문자열 분리 (좀 더 엄격한 파싱 방법 생각해보기)
         let token = this.rawData.match(/\w+/g);
+        
         return token;
     }
 
-    lexing() {
+    lexing(token) {
         // 데이터의 타입 체크
-        let token = this.tokenizing();
-
         let wrapType;
         if(Array.isArray(token)) {
             wrapType = 'Array';
@@ -37,20 +37,30 @@ class ArrayParser {
         return [wrapType, lexedData]
     }
 
-    parsing() {
-        let [wrapType, lexedData] = this.lexing();
+    // main 메서드임 
+    parsing(lexedData) {
+        let [wrapType, innerData] = lexedData;
         let parsedData = {
             type: `${wrapType}`,
             child: []
         }
-        lexedData.forEach(function(v) {
+        
+        innerData.forEach(function(v) {
             parsedData.child.push(new ParsingForm(v));
         }) 
+        return parsedData;
+    }
+
+    MainParser() {
+        let token = this.tokenizing();
+        let lexedData = this.lexing(token);
+        let parsedData = this.parsing(lexedData);
+
         return parsedData;
     }
 }
 
 const str = "[123, 22, 33]";
 const arrayParser = new ArrayParser(str);
-const result = arrayParser.parsing();
+const result = arrayParser.MainParser();
 console.log(JSON.stringify(result, null, 2)); 
