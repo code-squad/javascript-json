a73e264edee83464f598659e258f588856b7d3b1
allen <revenoir7@gmail.com>
2019-05-04 17:33:07 +0900

    refactor: lexing과정에서 forEach 대신 map메서드를 사용하도록 수정하여 nodeQueue삭제
    - seperator속성은 클래스 내부에서 선언하도록 수정
    - ArrayParser.parseToken메서드에서 불필요한 else 삭제

--- arrayParser.js
+++ arrayParser.js
@@ -2,21 +2,21 @@ const Tokenizer = require('./tokenizer');
 const Lexer = require('./lexer');
 
 class ArrayParser {
-    constructor({ tokenizer, lexer, seperator }) {
-        this.seperator = seperator;
+    constructor({ tokenizer, lexer }) {
+        this.seperator = ',';
         this.tokenizer = tokenizer;
         this.lexer = lexer;
-        this.nodeQueue = [];
     }
 
-    parseToken(parentNode) {
-        const node = this.nodeQueue.shift();
+    parseToken(parentNode, tokens) {
+        const node = tokens.shift();
         if (node.type === 'end') {
             return parentNode;
-        } else if (node.type === 'array') {
+        } 
+        if (node.type === 'array') {
             let childNode;
             while (true) {
-                childNode = this.parseToken(node);
+                childNode = this.parseToken(node, tokens);
                 if (childNode) break;
             }
             parentNode.child.push(childNode);
@@ -26,23 +26,22 @@ class ArrayParser {
     }
 
     getParsedStr(str) {
-        const tokens = this.tokenizer.tokenizeByChar(str, this.seperator);
+        let tokens = this.tokenizer.tokenizeByChar(str, this.seperator);
 
-        tokens.forEach(token => this.nodeQueue.push(this.lexer.decideType(token)));
+        tokens = tokens.map(token => this.lexer.decideType(token));
 
-        const rootNode = this.nodeQueue.shift();
+        const rootNode = tokens.shift();
 
-        while (this.nodeQueue.length) {
-            this.parseToken(rootNode);
+        while (tokens.length) {
+            this.parseToken(rootNode, tokens);
         }
         return rootNode;
     }
 }
 
-const seperator = ',';
 const tokenizer = new Tokenizer();
 const lexer = new Lexer();
-const arrayParser = new ArrayParser({ tokenizer, lexer, seperator });
+const arrayParser = new ArrayParser({ tokenizer, lexer });
 
 const str = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
 const result = arrayParser.getParsedStr(str);
