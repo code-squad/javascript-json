74fa1940c3eb0c7b075e2b3cb32f2ce298c98166
allen <revenoir7@gmail.com>
2019-05-04 17:33:54 +0900

    refactor: 메서드체이닝을 활용한 코드로 수정

--- tokenizer.js
+++ tokenizer.js
@@ -1,21 +1,15 @@
 class Tokenizer {
     insertCommaToBrackets(str) {
-        str = str.replace(/\[/g, '[,');
-        str = str.replace(/\]/g, ',]');
-        return str;
+        return str.replace(/\[/g, '[,').replace(/\]/g, ',]');
     }
 
     removeSpace(str) {
-        let arr = str.split(' ');
-        str = arr.join('');
-        return str;
+        return str.split(' ').join('');
     }
 
     tokenizeByChar(str, char) {
         str = this.insertCommaToBrackets(str);
-        str = this.removeSpace(str);
-        const tokens = str.split(char);
-        return tokens;
+        return this.removeSpace(str).split(char);
     }
 }
 
