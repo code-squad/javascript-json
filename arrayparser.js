36d196b3ef233b2ed23305276868f756818cf6bd
leehwarang <hwrng2@gmail.com>
2019-05-05 01:22:51 +0900

    refactor tokenizer 함수 로직 변경

--- arrayparser.js
+++ arrayparser.js
@@ -5,27 +5,24 @@ class ArrayParser {
     this.tokenArr = [];
   }
 
-  removeWhiteSpace(inputStr) {
-    inputStr = inputStr.replace(/\s/g, "");
-    return inputStr;
-  }
-
   tokenizer(inputStr) {
-    const splitedStr = inputStr.split(",");
-    return splitedStr;
+    inputStr = inputStr.replace(/\[/g, "[,");
+    inputStr = inputStr.replace(/\]/g, ",]");
+    inputStr = inputStr.replace(/\s/g, "");
+    return inputStr.split(",");
   }
 
   lexer(tokenizeredData) {
     tokenizeredData.forEach(element => {
       for (let char of element) {
         if (char === "[") {
-          this.tokenArr.push(new Token("array", "", []));
+          this.tokenArr.push(new Token("array"));
           element = element.replace(char, "");
         } else if (char === "]") {
           element = element.replace(char, "");
         }
       }
-      this.tokenArr.push(new Token("number", element, []));
+      this.tokenArr.push(new Token("number", element));
     });
   }
 
@@ -41,23 +38,25 @@ class ArrayParser {
   }
 
   startParsing(inputStr) {
-    // 1. removeWhiteSpace
-    const spaceRemovedData = this.removeWhiteSpace(inputStr);
+    // 1. tokenizer
+    const tokenizeredData = this.tokenizer(inputStr);
+    console.log(tokenizeredData);
 
-    // 2. tokenizer
-    const tokenizeredData = this.tokenizer(spaceRemovedData);
+    // // 3. lexer
+    // this.lexer(tokenizeredData);
 
-    // 3. lexer
-    this.lexer(tokenizeredData);
+    // // 4. parse
 
-    // 4. parse
-
-    const result = this.parse();
-    return result;
+    // const result = this.parse();
+    // return result;
   }
 }
 
-const targetStr = "[123, 22, 33]";
+// const targetStr = "[123, 22, 33]";
+const targetStr = "[123, 22, [33]]";
+
 const arrayParser = new ArrayParser();
 const result = arrayParser.startParsing(targetStr);
 console.log(result);
+console.log("=======");
+console.log(JSON.stringify(result));
