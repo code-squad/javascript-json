1f54bf06d3ba16c3985f333336cd80268a433efd
janghyoseog <bestdevhyo1225@gmail.com>
2019-05-04 17:39:58 +0900

    문자열을 파싱해서 정보들을 객체로 반환해주는 array_parser.js 모듈
    - ArrayParser(str) : 입력된 문자열을 Lexer, Parser 클래스를 통해 파싱후 객체를 반환하는 함

--- array_parser.js
+++ array_parser.js
@@ -1,9 +1,11 @@
+const Parser = require('./parser');
+const Lexer = require('./lexer');
+
 const ArrayParser = (str) => {
-    const parseArray = JSON.parse(str);
-    return parseArray.reduce( (prevObj, currentValue) => {
-        prevObj['child'].push({ type : 'number', value : currentValue, child : [] } );
-        return prevObj;
-    }, { type : 'array', child : [] } );
+    const lexer = new Lexer();
+    const tokensInArray = lexer.getTokensInArray(str);
+    const parser = new Parser(tokensInArray.length);
+    return parser.createSyntaxTree(tokensInArray, 0);
 }
 
 module.exports = ArrayParser;
