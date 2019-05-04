4e22ad74896ee2262df88f50913a3c83c66685f6
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-04 17:55:21 +0900

    Fix : 예외처리 try-catch로 처리

--- step6-1/parserUtils.js
+++ step6-1/parserUtils.js
@@ -21,15 +21,20 @@ const parserUtils = {
             whitespace : /\s+/g,
         };
 
-        let dataType = null;
 
-        Object.keys(typeRegexp).forEach((key) => {
-            if (typeRegexp[key].test(token)) dataType = this.dataType[key];
-        })
-
-        if (dataType === null) throw new Error('숫자 타입만 지원하는 parser 입니다');
-
-        return dataType;
+        try {
+            let dataType = null;
+            Object.keys(typeRegexp).forEach((key) => {
+                if (typeRegexp[key].test(token)) dataType = this.dataType[key];
+            })
+    
+            if (dataType === null) throw new Error('숫자 타입만 지원하는 parser 입니다');
+            return dataType;
+    
+        } catch (error) {
+            console.error(error.message);
+            return;
+        }
     },
 
     isType(char, type) {
