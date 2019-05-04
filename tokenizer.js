7ff90311daf5539d30d87934d5a2f0faad11bf6b
P-iknow <apricotsoul@gmail.com>
2019-05-04 17:44:02 +0900

    refector: Change tokenizer class to function
    - tokenize 를 class로 만들어야 할 명확한 이유가 없어 function 으로
    재설정

--- tokenizer.js
+++ tokenizer.js
@@ -2,56 +2,45 @@ const tc = require('./typeChecker');
 const Token = require('./token');
 
 let _currentLength = 0;
+let _inputStr= '';
+let _queue = [];
 
-class Tokenizer {
-    constructor(inputstr){
-        this.queue = [];
-        this._inputStr = inputstr;
-    }
-    _readChar(currentLength){
-            return this._inputStr[currentLength]
-    }
-    _getSeriesToken (readChar, char, conditionFunc){
-        if(!conditionFunc(char)) return '' 
-        return char + this._getSeriesToken(readChar, readChar(++_currentLength), conditionFunc)  
-      }
-    _getSeriesStrToken(readChar, char, conditionFunc){
-      if(conditionFunc(char)) {
-        _currentLength++
-        return '';
-      } 
-      return char + this._getSeriesStrToken(readChar, readChar(++_currentLength), conditionFunc)  
-    }
-    _decideTokenType(char){
-        if (tc.isSkipChar(char)) return;
+const _readChar =(currentLength) => _inputStr[currentLength]
 
-        if (tc.isBraket(char)) return new Token('braket', char)
-
-        if (tc.isNumber(char)) {
-            return new Token('number', this._getSeriesToken(this._readChar.bind(this),char, tc.isNumber));
-        }
-        //TODO STPE 6-2  
-        if (tc.isQuote(char)) {
-            return new Token("string", this._getSeriesStrToken(this._readChar.bind(this), this._readChar(++_currentLength), tc.isQuote))
-        }
-        //TODO STPE 6-2    
-        if (tc.isString(char)) {
-            return new Token("otherType", this._getSeriesToken(this._readChar.bind(this), char, tc.isString))
-        }
-
-        throw new TypeError(char + " 는 토근화 할 수 없는 문자 입니다.");
-        }
-    _decideReadNextChar(token){
-        return ( token === undefined || token.type === 'braket')
-    }
-    tokenize(){
-        while(_currentLength < this._inputStr.length) {
-            const token = this._decideTokenType(this._readChar(_currentLength))
-            if(token) this.queue.push(token);
-            if(this._decideReadNextChar(token)) _currentLength++;
-        }
-    }
+const _getSeriesToken = (readChar, char, conditionFunc) => {
+    if (!conditionFunc(char)) return '' 
+    return char + _getSeriesToken(readChar, readChar(++_currentLength), conditionFunc)
 }
 
-test = new Tokenizer("[123,['I', 'Can'], [false, true, 123]]")
-test.tokenize()
+const _getSeriesStrToken = (readChar, char, conditionFunc) => {
+    if (conditionFunc(char)) { _currentLength++; return ''} 
+    return char + _getSeriesStrToken(readChar, readChar(++_currentLength), conditionFunc)  
+}
+const _decideTokenType = (char) => {
+    if (tc.isSkipChar(char)) return
+
+    if (tc.isBraket(char)) return new Token('braket', char)
+
+    if (tc.isNumber(char)) return new Token('number', _getSeriesToken(_readChar, char, tc.isNumber))
+    //TODO STPE 6-2  
+    if (tc.isQuote(char)) return new Token("string", _getSeriesStrToken(_readChar, _readChar(++_currentLength), tc.isQuote))
+    //TODO STPE 6-2    
+    if (tc.isString(char)) {
+        return new Token("otherType", _getSeriesToken(_readChar, char, tc.isString))
+    }
+    throw new TypeError(char + " 는 토근화 할 수 없는 문자 입니다.")
+}
+
+const _isTokentypeSkipOrBraket =(token) => token === undefined || token.type === 'braket'
+
+const tokenize = (input) => {
+    _inputStr = input;
+    while(_currentLength < _inputStr.length) {
+        const token = _decideTokenType(_readChar(_currentLength))
+        if (token) _queue.push(token);
+        if (_isTokentypeSkipOrBraket(token)) _currentLength++;
+    }
+    return _queue;
+}
+
+module.exports = tokenize;
