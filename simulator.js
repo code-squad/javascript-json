6700783624ca09e75430b454cfd9616ad8c9244e
janghyoseog <bestdevhyo1225@gmail.com>
2019-05-04 17:37:45 +0900

    테스트를 하기 위한 simulator.js 모듈

--- simulator.js
+++ simulator.js
@@ -1,7 +1,10 @@
 const ArrayParser = require('./array_parser');
 
 (() => {
-    const str = '[123, 22, 33]';
-    const result = ArrayParser(str);
+    const str1 = "[15, 9, [7, 5], 2, [999, 11, [3, 8]], 25]";
+    const str2 = "[15, 9, 7, 5, 2, 999, 11, 3, 8, 25]";
+    const str3 = "[1, [2, [3], 4], 5]";
+    const str4 = "[123, 22, 33]";
+    const result = ArrayParser(str1);
     console.log(JSON.stringify(result, null, 2));
 })();
