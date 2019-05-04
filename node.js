dd9d72a843cf8cb13c467633f53ff634286b61d5
P-iknow <apricotsoul@gmail.com>
2019-05-04 14:41:07 +0900

    refator: Change tokenizer function to class
    - tokenizer 함수의 크기가 방대하여 class로 변경

--- node.js
+++
@@ -1,7 +0,0 @@
-class Node {
-    constructor(type, value){
-        this.type = type;
-        this.value = value 
-    }
-}
-module.exports = Node;
--- tokenizer.js
+++ tokenizer.js
@@ -1,63 +1,57 @@
-const tc = require('./typeChecker')
+const tc = require('./typeChecker');
+const Token = require('./token');
 
-const _deleteFirstLastBraket = (arrayTypeString) => {
-    return arrayTypeString.substring(1, arrayTypeString.length -1)
+let _currentLength = 0;
+
+class Tokenizer {
+    constructor(inputstr){
+        this.queue = [];
+        this._inputStr = inputstr;
+    }
+    _readChar(currentLength){
+            return this._inputStr[currentLength]
+    }
+    _getSeriesToken (readChar, char, conditionFunc){
+        if(!conditionFunc(char)) return '' 
+        return char + this._getSeriesToken(readChar, readChar(++_currentLength), conditionFunc)  
+      }
+    _getSeriesStrToken(readChar, char, conditionFunc){
+      if(conditionFunc(char)) {
+        _currentLength++
+        return '';
+      } 
+      return char + this._getSeriesStrToken(readChar, readChar(++_currentLength), conditionFunc)  
+    }
+    _decideTokenType(char){
+        if (tc.isSkipChar(char)) return;
+
+        if (tc.isBraket(char)) return new Token('braket', char)
+
+        if (tc.isNumber(char)) {
+            return new Token('number', this._getSeriesToken(this._readChar.bind(this),char, tc.isNumber));
+        }
+        //TODO STPE 6-2  
+        if (tc.isQuote(char)) {
+            return new Token("string", this._getSeriesStrToken(this._readChar.bind(this), this._readChar(++_currentLength), tc.isQuote))
+        }
+        //TODO STPE 6-2    
+        if (tc.isString(char)) {
+            return new Token("otherType", this._getSeriesToken(this._readChar.bind(this), char, tc.isString))
+        }
+
+        throw new TypeError(char + " 는 토근화 할 수 없는 문자 입니다.");
+        }
+    _decideReadNextChar(token){
+        return ( token === undefined || token.type === 'braket')
+    }
+    tokenize(){
+        while(_currentLength < this._inputStr.length) {
+            const token = this._decideTokenType(this._readChar(_currentLength))
+            if(token) this.queue.push(token);
+            if(this._decideReadNextChar(token)) _currentLength++;
+        }
+    }
 }
 
-const tokenizer = (input) => {
-    input = _deleteFirstLastBraket(input);
-    let index = 0;
-    const tokens = [];
-    while (index < input.length) {
-      let char = input[index];
-
-      if (tc.isWhiteSpace(char) || tc.isComma(char)) {
-        index++;
-        continue;
-      }
-      
-      if (tc.isBraket(char)) {
-        tokens.push({type: 'braket', value: char,});
-        index++;
-        continue;
-      }
-
-      if (tc.isNumber(char)) {
-        let value = '';
-        while (tc.isNumber(char)) {
-          value += char;
-          char = input[++index];
-        }
-        tokens.push({ type: 'number', value });
-        continue;
-      }
-
-      //TODO STPE 6-2  
-      if (tc.isQuote(char)) {
-        let value = '';
-        char = input[++index];
-        while (!tc.isQuote(char)) {
-          value += char;
-          char = input[++index];
-        }
-        char = input[++index];
-        tokens.push({ type: 'string', value });
-        continue;
-      }
-      //TODO STPE 6-2    
-      if (tc.isString(char)) {
-        let value = '';
-        while (tc.isString(char)) {
-          value += char;
-          char = input[++index];
-        }
-        tokens.push({ type: 'name', value });
-        continue;
-      }
-
-      throw new TypeError(char + "는 토근화 할 수 없는 문자 입니다.");
-    }
-  
-    return tokens;
-  }
-  module.exports = tokenizer;
+test = new Tokenizer("[123,['I', 'Can'], [false, true, 123]]")
+test.tokenize()
