a3acbb9f699c19251a59db34d549760b0c92f72b
Ryujeuk <jeuk817@gmail.com>
2019-05-05 20:28:20 +0900

    feat: ArrayParser.js 생성 및 수도코드작성
    1. 파일생성후 class ArrayParser을 생성
    2. 디자인에서 작성했던 수도코드를 옮겨옴.

---
+++ ArrayParser.js
@@ -0,0 +1,32 @@
+const str = "['1a3', [null, false, ['11', [112233], 112], 55, '99'], 33, true]";
+
+class ArrayParser {
+    constructor() {
+        this.stack = [];
+    }
+
+    tokenizer(string) {
+        // 문자로 들어온 배열을 각각의 토큰으로 나누어 배열로 반환하자.
+        return array;
+    }
+
+    lexer(token) {
+        // 토큰이 들어오면 type을 구분하여 반환하자.
+        return [type, token];
+    }
+
+    parser(type, token) {
+        // 토큰과 그 타입이 들어오면 자료형태로 바꾸어 반환하자.
+        return { type: type, value: token, child: [] };
+    }
+
+    run(array) {
+        // tokenizer한 배열이 들어오면 하나씩 빼서 lexer와 parser를 이용해 stack에 쌓고 빼는 작업을 해주자. (아마 재귀로 구현될것 같음.)
+        // 그리고 stack의 요소들이 모두 합쳐지면 그것을 반환하도록 하자.
+        if (typeof stack[0] === 'object') return stack[0];
+        const token = array.pop();
+        [type, token] = lexer(token);
+        const _stack = parser(type, token);
+        stack.push(_stack);
+        return array
+    }
