c89aba2824aad89fe5caf1ddf26bc4836f5c11af
leehwarang <hwrng2@gmail.com>
2019-05-05 21:15:51 +0900

    feat parse 함수 이중 중첩 동작하도록 기능 구현

--- arrayparser.js
+++ arrayparser.js
@@ -3,6 +3,16 @@ const Token = require("./token.js");
 class ArrayParser {
   constructor() {
     this.tokenArr = [];
+    this.tokenStack = [];
+    this.parsingData = [];
+  }
+
+  push(parentToken) {
+    this.tokenStack.push(parentToken);
+  }
+
+  pop() {
+    return this.tokenStack[this.tokenStack - 1];
   }
 
   tokenizer(inputStr) {
@@ -17,6 +27,7 @@ class ArrayParser {
       if (element[0] === "[") {
         this.tokenArr.push(new Token("array"));
       } else if (element[0] === "]") {
+        this.tokenArr.push(new Token("array-end"));
       } else {
         this.tokenArr.push(new Token("number", element));
       }
@@ -26,29 +37,54 @@ class ArrayParser {
   }
 
   parse() {
-    const parentToken = this.tokenArr.find(token => token._type === "array");
-
+    //tokenArr들을 순회하면서 돌다가 token_type이 array이면 parentToken으로 저장하고 cnt를 1 만큼 증가
+    //token_type이 end이면 cnt를 1만큼 감소
+    //순회 과정은 cnt가 0이 될 때까지 반복
+    //그 사이에 있던 애들은 parentToken의 child에 push
+    let currentParentToken;
+    let currentChildToken;
     this.tokenArr.forEach(token => {
-      if (token._type === "number") {
-        parentToken._child.push(token);
+      // console.log(token);
+      if (token._type === "array") {
+        this.tokenStack.push(token);
+      } else if (token._type === "number") {
+        currentParentToken = this.tokenStack.pop();
+        currentParentToken._child.push(token);
+        this.tokenStack.push(currentParentToken);
+      } else if (token._type === "array-end") {
+        currentChildToken = this.tokenStack.pop();
+        // console.log(currentChildToken);
+        // this.tokenStack의 길이가 0보다 큰 경우만 아래 실행
+        if (this.tokenStack.length > 0) {
+          currentParentToken = this.tokenStack.pop();
+          // console.log(currentParentToken);
+          currentParentToken._child.push(currentChildToken);
+          this.tokenStack.push(currentParentToken);
+        }
+
+        // currentParentToken._child.push();
+        // this.tokenStack.push(currentParentToken);
+        //pop하고..
+        //pop한 데이터를 다시 한번 pop한 부모의 child에 push
       }
     });
-    return parentToken;
+    return currentParentToken;
   }
 
   startParsing(inputStr) {
     // 1. tokenizer
     const tokenizeredData = this.tokenizer(inputStr);
-    console.log(tokenizeredData);
+    // console.log(tokenizeredData);
 
     // 2. lexer
     const lexeredData = this.lexer(tokenizeredData);
-    console.log(lexeredData);
+    // console.log(lexeredData);
 
     // 3. parse
 
     const result = this.parse();
     return result;
+    console.log(result);
   }
 }
 
