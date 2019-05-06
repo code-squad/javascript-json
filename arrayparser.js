b1b77eaf47289651e85f0d2e4fcc4b661e498512
leehwarang <hwrng2@gmail.com>
2019-05-06 21:21:27 +0900

    refactor 불필요한 코드 삭제

--- arrayparser.js
+++ arrayparser.js
@@ -18,11 +18,9 @@ class ArrayParser {
     inputStr = this.tokenizer.addCommaToBraket(inputStr);
     inputStr = this.tokenizer.removeWhiteSpace(inputStr);
     this.tokenizeredData = this.tokenizer.splitInputStr(inputStr);
-    // console.log(this.tokenizeredData);
 
     // 2. lexer
     this.lexeredData = this.lexer.makeToken(this.tokenizeredData);
-    // console.log(this.lexeredData);
 
     // 3. parse
     this.parseredData = this.parser.parsing(this.lexeredData);
@@ -35,18 +33,12 @@ const lexer = new Lexer();
 const parser = new Parser();
 const arrayParser = new ArrayParser(tokenizer, lexer, parser);
 
-const targetStr = "[123, 22, 33]";
-// const targetStr = "[123, 22, [33]]";
-// arrayParser.startParsing(targetStr);
+const targetStr = "[123, 22, [33]]";
 const result = arrayParser.startParsing(targetStr);
 
 console.log(result);
 console.log("=======");
 console.log(JSON.stringify(result));
 
-// const targetStr = "[123, 22, [33]]";
-// console.log(result);
-// console.log("=======");
-// console.log(JSON.stringify(result));
-
+// const targetStr = "[123, 22, 33]";
 // const targetStr = "['123',[ '456', '789',['11',['22']],'55', '99'], '33']";
