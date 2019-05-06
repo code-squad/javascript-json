c6c23b5a6b426e42afffc9c091849c4c0df5ef24
eugene94 <eugenekang94@gmail.com>
2019-05-06 12:47:31 +0900

    [수정] parser의 다양한 타입 지원
    - 기존 : number 타입만 지원
    - 수정 : number, string, undefined, null, boolean 지원

--- app.js
+++ app.js
@@ -10,24 +10,56 @@ const tokenizer = {
 };
 
 const lexer = {
-  makeNode(element) {
-    element = Number(element);
+  makeNode(type, value) {
     return new Node({
-      type: 'number',
-      value: element
+      type: type,
+      value: value
     });
+  },
+
+  isString(element) {
+    return (
+      element.startsWith("'") ||
+      element.endsWith("'") ||
+      element.startsWith('"') ||
+      element.endsWith('"')
+    );
   },
 
   lex(word) {
     if (word === '[') {
-      return { context: 'ArrayOpen', newNode: new Node({ type: 'Array' }) };
+      return { context: 'ArrayOpen', newNode: this.makeNode('Array') };
     }
 
     if (word === ']') {
       return { context: 'ArrayClose', newNode: undefined };
     }
 
-    return { context: 'Element', newNode: this.makeNode(word) };
+    if (word === 'null') {
+      return { context: 'Element', newNode: this.makeNode('Null', null) };
+    }
+
+    if (word === 'true') {
+      return { context: 'Element', newNode: this.makeNode('Boolean', true) };
+    }
+
+    if (word === 'false') {
+      return { context: 'Element', newNode: this.makeNode('Boolean', false) };
+    }
+
+    if (word === 'undefined') {
+      return { context: 'Element', newNode: this.makeNode('Undefined', undefined) };
+    }
+
+    if (this.isString(word)) {
+      return {
+        context: 'Element',
+        newNode: this.makeNode('String', word.substring(1, word.length - 1))
+      };
+    }
+
+    // if (Number.isNaN(Number(word))) 에러
+    return { context: 'Element', newNode: this.makeNode('Number', Number(word)) };
   }
 };
 
@@ -68,14 +100,18 @@ class Parser {
   }
 }
 
-const str = '[123, [22, 33], 444]';
-const str2 = '[123, 22, 33, 444]';
+// const str = '[123, [22, 33], 444]';
+// const str2 = '[123, 22, 33, 444]';
 
 const parser = new Parser({ lexer, tokenizer });
-const result = parser.parse(str);
-console.log(JSON.stringify(result, null, 2));
+// const result = parser.parse(str);
+// console.log(JSON.stringify(result, null, 2));
 
-console.log('======================================');
+// console.log('======================================');
 
-const result2 = parser.parse(str2);
-console.log(JSON.stringify(result2, null, 2));
+// const result2 = parser.parse(str2);
+// console.log(JSON.stringify(result2, null, 2));
+
+const str3 = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+const result3 = parser.parse(str3);
+console.log(JSON.stringify(result3, null, 2));
--- node.js
+++ node.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor({ type, value = '' }) {
+  constructor({ type, value }) {
     this.type = type;
     this.value = value;
     this.child = [];
