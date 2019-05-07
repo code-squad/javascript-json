b790b3719276452bd82418a8bf5f60cbcc785357
Ryujeuk <jeuk817@gmail.com>
2019-05-07 15:28:57 +0900

    feat: lexer 구현
    처음에는 typeBox라는 객체를 만들어 각각의 타입별로 값을 주어 includes로
    찾는 방법을 썻지만 그것보다 작은 단위의 값도 포함되는 오류가 발생하였다.
    예를 들면, { boolean: 'true, false'} 로 typeBox를 만들고 includes를 쓰면
    'tru' 혹은 'fa'도 포함되는 오류가 발생한다.
    그래서 각각을 if문으로 분류하여type을 반환하도록 구현하였다.
    추후 더 쉽고 간단한 방법이 있는지 고민할 소지가 있다.

--- ArrayParser.js
+++ ArrayParser.js
@@ -29,4 +29,14 @@ class ArrayParser {
 
         return array.map(x => x.trim());
     }
+
+    lexer(token) {
+        // 더 쉽고 간단한 방법이 있을지 고민해보자.
+        if ("[]".includes(token)) return 'array';
+        if (token === 'true' || token === 'false') return 'boolean';
+        if (token === 'null') return 'null';
+        if (`'"`.includes(token[0]) && `'"`.includes(token[token.length - 1])) return 'string';
+
+        return 'number';
+    }
 }
