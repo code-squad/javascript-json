2a321748f8ff42751d40aac857ab8dd3e28f47e5
sonypark <sonypark0204@gmail.com>
2019-05-07 15:04:19 +0900

    test: Execute test

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const ArrayParser = require('./ArrayParser');
-const str = '[123, 22, 33, 123, 45, 6, 78, 6]';
 
-result= ArrayParser(str);
+const str = "[123, [22, 33], 234, [-45, 0.6], '78', '6', [[null, false], true]]";
+result = ArrayParser.parse(str);
+
+console.log(result);
 console.log(JSON.stringify(result, null, 2));
