e6fc220ed4344d183909eed6c20587293f18504d
ymink16 <yminkdev@gmail.com>
2019-05-07 10:53:50 +0900

    initial commit

---
+++ step6-1_arrayParser.js
@@ -0,0 +1,15 @@
+const arrayParser = () => {
+
+}
+
+const tokenizer = () => {
+
+}
+
+const lexer = () => {
+
+}
+
+const str = "[123, 22, 33]";
+const result = arrayParser();
+console.log(JSON.stringify(result, null, 2)); 
