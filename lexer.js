8aa5b8bc937b6c14b233a3dd5cb8a256ce4555dc
sonypark <sonypark0204@gmail.com>
2019-05-07 14:55:23 +0900

    style: Make if syntax to a line for readability

--- lexer.js
+++ lexer.js
@@ -2,27 +2,16 @@ const Node = require('./Node');
 
 class Lexer{
     lex(token){
-        if(token === '['){
-            return new Node('arrStart', '[');
-        }
-        if(token === ']'){
-            return new Node('arrEnd', ']');
-        }
-        if(isFinite(Number(token))){
-            return new Node('number', token);
-        }
-        if(token.startsWith("'") && token.endsWith("'")){
-            return new Node('string', token);
-        }
-        if(token === 'null') {
-            return new Node('null', token);
-        }
-        if(token === 'true' || token ==='false'){
-            return new Node('boolean', token);
-        }
-        else {
-            throw Error(`${token} undefined token type error`);
-        }
+        if(token === '[') return new Node('arrStart', '[');
+        if(token === ']') return new Node('arrEnd', ']');
+
+        if(isFinite(Number(token))) return new Node('number', token);
+        if(token.startsWith("'") && token.endsWith("'")) return new Node('string', token);
+        
+        if(token === 'null') return new Node('null', token);
+        if(token === 'true' || token ==='false') return new Node('boolean', token);
+        
+        else throw Error(`${token} undefined token type error`);
     }
 }
 
