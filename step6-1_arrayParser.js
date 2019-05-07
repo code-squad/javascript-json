5dfae43e326ee89f5b3412a9b3d9b164aac930c6
ymink16 <yminkdev@gmail.com>
2019-05-07 10:54:42 +0900

    tokenizer 함수 구현

--- step6-1_arrayParser.js
+++ step6-1_arrayParser.js
@@ -2,8 +2,24 @@ const arrayParser = () => {
 
 }
 
-const tokenizer = () => {
+const tokenizer = str => {
+    const separator = ', ';
+    const temp = str.split(separator);
+    const tokens = [];
 
+    for (let token of temp) {
+        if (token.indexOf('[') !== -1) {
+            tokens.push('[');
+            tokens.push(token.substr(1));
+        } else if (token.indexOf(']') !== -1) {
+            tokens.push(token.substr(0, token.length-1));
+            tokens.push(']');
+        } else {
+            tokens.push(token);
+        }
+    }
+
+    return tokens;
 }
 
 const lexer = () => {
