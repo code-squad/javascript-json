80954ed257df2f1402113f8555b2b97c1b8c75fc
ymink16 <yminkdev@gmail.com>
2019-05-07 18:13:45 +0900

    checkType 함수 구현

--- step6-2_arrayParser.js
+++ step6-2_arrayParser.js
@@ -22,14 +22,34 @@ const tokenizer = str => {
     return tokens;
 }
 
-const lexer = () => {  
+const lexer = (str) => {  
+    const temp = tokenizer(str);
+    const tokens = [];
 
+}
+
+const checkType = (token) => {
+    if (typeof(Number(token)) === 'number') {
+        return 'number';
+    } else if (Boolean(token) === true || Boolean(token) === false) {
+        return 'boolean';
+    } else if (token === 'null') {
+        return 'null';
+    } else if (token.substr(1, token.length-2).indexOf('\'') !== -1) {
+        console.error(`${token}은 올바른 문자열이 아닙니다.`);
+    } else if (token.substr(1, token.length-2).indexOf('\"') !== -1) {
+        console.error(`${token}은 올바른 문자열이 아닙니다.`);
+    } else if (typeof(token) === "string") {
+        return 'string';
+    } else {
+        console.error(`${token}은 알 수 없는 타입입니다.`);
+    }
 }
 
 const arrayParser = () => {
 
 }
 
-var s = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
-
-console.log(tokenizer(s));
+var s1 = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+var s2 = "['1a'3',[22,23,[11,[112233],112],55],33]";
+console.log(tokenizer(s2));
