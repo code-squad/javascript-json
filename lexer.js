8cc5fd89b9aec2133abaa4ec31f05b53fe928eca
sonypark <sonypark0204@gmail.com>
2019-05-07 12:07:37 +0900

    feat: Create lex function in lexer class

--- lexer.js
+++ lexer.js
@@ -1,5 +1,28 @@
 const Node = require('./Node');
 
 class Lexer{
+    lex(token){
+        if(token === '['){
+            return new Node('arrStart', '[');
+        }
+        if(token === ']'){
+            return new Node('arrEnd', ']');
+        }
+        if(isFinite(Number(token))){
+            return new Node('number', token);
+        }
+        if(token.startsWith("'") && token.endsWith("'")){
+            return new Node('string', token);
+        }
+        if(token === 'null') {
+            return new Node('null', token);
+        }
+        if(token === 'true' || token ==='false'){
+            return new Node('boolean', token);
+        }
+        else {
+            throw Error(`${token} undefined token type error`);
+        }
+    }
 }
 
