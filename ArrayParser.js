0c5f5ec64f921215ee1075895678190bdfd0ff92
sunwook kim <koownusmik@gmail.com>
2019-05-07 15:49:41 +0900

    refactor: lex와 parse 동작로직 변경

--- ArrayParser.js
+++ ArrayParser.js
@@ -1,68 +1,68 @@
-const {log} = console;
+const { log } = console;
 
 
 const ArrayParser = {
-    tokenQueue:[],
+    tokenQueue: [],
     historyStack: [],
     resultTree: {},
 
 
-    run(input){
+    run(input) {
         this.tokenize(input)
-
-
-
-        while (this.tokenQueue.length) {
-            const currentToken = this.tokenQueue.shift()
-
-            const fetchLexTokenObj = this.lex(currentToken);
-            this.resultTree = this.parse(fetchLexTokenObj);
-        }
+        // 테스트코드 1 -> log(tokenQueue)
+        this.lex();
+        // 테스트코드 2 -> log(tokenQueue)
+        this.parse();
         return this.resultTree
     },
 
 
     tokenize(input) {
 
-        const tokens = input.replace(/\[/g, '[,').replace(/\]/g, ',]').replace(/ /g,'').split(',');
-        const convertedTokens = tokens.map(el=>isNaN(Number(el))?el:Number(el));        
+        const tokens = input.replace(/\[/g, '[,').replace(/\]/g, ',]').replace(/ /g, '').split(',');
+        const convertedTokens = tokens.map(el => isNaN(Number(el)) ? el : Number(el));
         this.tokenQueue = convertedTokens
     },
-    lex(token) {
-        ////// -> number , array 를 하드코딩하지 않는 방법이있을까? array를 모르겠다.. 
-        if (!isNaN(token)) return { type: typeof (token), value: token, child: [] }
-        if (['[', ']'].includes(token)) return { type: 'array', value: token, child: [] }
 
-
-
+    lex() {
+        this.tokenQueue = this.tokenQueue.map(token => {
+            if (!isNaN(token)) return { type: typeof (token), value: token, child: [] }
+            if (['[', ']'].includes(token)) return { type: 'array', value: token, child: [] }
+        })
     },
 
 
-    parse(obj) {
+    parse() {
 
-        if (obj.value === '[') {
-            delete obj.value
-            this.historyStack.push(obj)
-            return
-        }
+        while (this.tokenQueue.length) {
+            const currentLexedObj = this.tokenQueue.shift()
 
-        if (obj.type === 'number') {
-            const last = this.historyStack.length - 1
-            this.historyStack[last].child.push(obj);
-            return
-        }
+            if (currentLexedObj.value === '[') {
+                delete currentLexedObj.value
+                this.historyStack.push(currentLexedObj)
 
-        if (obj.value === ']') {
-            delete obj.value
-            const tempResultTree = this.historyStack.pop();
-            const newLast = this.historyStack.length - 1
-
-            // 혹은 newLast가 0이 아니면 
-            if (this.historyStack.length) {
-                this.historyStack[newLast].child.push(tempResultTree);
-            } else {
-                return tempResultTree
             }
+
+            else if (currentLexedObj.type === 'number') {
+                const last = this.historyStack.length - 1
+                this.historyStack[last].child.push(currentLexedObj);
+
+            }
+            else if (currentLexedObj.value === ']') {
+                delete currentLexedObj.value
+                const tempResultTree = this.historyStack.pop();
+                const newLast = this.historyStack.length - 1
+
+                if (this.historyStack.length) {
+                    this.historyStack[newLast].child.push(tempResultTree);
+                } else {
+                    // 이렇게 넣고 while안에 queue길이 0일때까지 돌리는 방식으로 해도되고
+                    this.resultTree = tempResultTree
+                    // return 으로 전달해줘도 될것같음. 이건 취향에 따라 다른듯
+                }
+            }
+
+
 
 
         }
@@ -76,4 +76,5 @@ const ArrayParser = {
 
 const str = "[1, 2, [ 3, [ 4, 5, [ 6 ], 7, [ 8, 9 ], 100 ]  ]  ]";
 const result = ArrayParser.run(str);
+// 테스트코드3 
 log(JSON.stringify(result, null, 2)); 
