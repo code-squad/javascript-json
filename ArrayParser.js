1396541d67b4759be991aeeebe7cc9fc020c317d
sonypark <sonypark0204@gmail.com>
2019-05-07 12:10:45 +0900

    feat: Create parse function in ArrayParser
    - Get tokens from tokenizer
    - Get node from lexer and push it to the queue
    - Get firstNode from the queue, the firstNode is the rootNode

--- ArrayParser.js
+++ ArrayParser.js
@@ -8,6 +8,12 @@ class ArrayParser {
         this.lexer = lexer;
         this.queue = [];
     }
+
+    parse(input) {
+        const tokens = this.tokenizer.tokenize(input);
+        tokens.forEach(token => this.queue.push(this.lexer.lex(token)));
+        const firstNode = this.queue.shift();
+    }
 }
 
 const str = '[123, 22, [0.0033, 123], -45, 0.6, 7.828, 6]';
