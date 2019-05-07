e43ff13b59672c24c73668ed4cf0a051733d78f6
janghyoseog <bestdevhyo1225@gmail.com>
2019-05-07 21:18:52 +0900

    어휘를 분석하는 Lexer 클래스 수정 및 테스트
    - 전체적인 변수(매개 변수) 및 메소드 이름 변
    - getBracketDepth(strValue) 	     : 문자열의 대괄호 깊이를 반환하는 메소드
    - getValueOfRemovedBracket(strValue) : 문자열의 대괄호를 제거한 값을 반환하는 메소드
    - getTypeOf(value)		     : 값의 Type을 체크한 뒤, 해당 Type의 값을 반환하는 메소드
    - anlayze(strArray)  		     : 문자열 요소를 가진 배열의 어휘를 분석하고 분석한 결과를 반환하는 메소드

--- lexer.js
+++ lexer.js
@@ -1,47 +1,52 @@
 class Lexer {
-    constructor() { 
-        this.emptySpaceRegex = /\s/gi;
+    constructor(validation) {
+        this.validation = validation; 
         this.bracketStack = [];
     }
 
-    checkBracketDepth(str) {
+    getBracketDepth(strValue) {
         let currentBracketDepth = this.bracketStack.length;
-        str.split('').forEach( element => {
+        strValue.split('').forEach( element => {
             if (element === ']') this.bracketStack.pop();
-            else if (element === '[') {
+            else if (element === '['){
                 this.bracketStack.push('[');
                 currentBracketDepth = this.bracketStack.length;
             }
         } );
-        return currentBracketDepth - 1;
+        return currentBracketDepth;
     }
 
-    removeBracketFromStringValue(str) {
-        let frontIndex = 0, rearIndex = str.length - 1;
-        str.split('').forEach( element => {
-            if (element === '[')        frontIndex++
-            else if (element === ']')   rearIndex--;
+    getValueOfRemovedBracket(strValue) {
+        let start = 0, end = strValue.length - 1;
+        strValue.split('').forEach( element => {
+            if (element === '[')        start++;
+            else if (element === ']')   end--;
         } );
-        const elementLength = rearIndex - frontIndex + 1;
-        return str.substr(frontIndex, elementLength);
+        const valueLength = end - start + 1;
+        return strValue.substr(start, valueLength);
     }
 
-    extractTokensInformation(strInArray) {
-        const tokensInArray = [];
-        strInArray.forEach( str => {
-            const depth = this.checkBracketDepth(str);
-            const value = this.removeBracketFromStringValue(str);
-            tokensInArray.push( { 'depth' : depth , 'value' : value } ); 
-        });
-        return tokensInArray;
-    }
+    getTypeOf(value) { return this.validation.checkTypeOf(value); }
 
-    splitStringValue(str) { return str.replace(this.emptySpaceRegex, "").split(','); }
-
-    getTokensInArray(str) {
-        if (!str.startsWith('[') || !str.endsWith(']')) throw Error('Syntax Error!');
-        return this.extractTokensInformation(this.splitStringValue(str));
+    analyze(strArray) {
+        const tokenArray = [];
+        strArray.forEach( strValue => {
+            const depth = this.getBracketDepth(strValue);
+            const value = this.getValueOfRemovedBracket(strValue);
+            const type  = this.getTypeOf(value);
+            tokenArray.push({ 'depth' : depth, 'value' : value, 'type' : type, });
+        } );
+        return tokenArray;
     }
 }
 
 module.exports = Lexer;
+
+// const validation = require('./validation');
+// const lexer = new Lexer(validation);
+// console.log(lexer.getBracketDepth('[[98]]'));  // 2
+// console.log(lexer.getValueOfRemovedBracket('[[[null]]]]')); // null
+// console.log(lexer.getTypeOf('123'));
+// console.log(lexer.getTypeOf([1,2,3]));
+// console.log(lexer.getTypeOf("'aa'"));
+// console.log(lexer.analyze(["[12", "'1a3'", "'2abs3'"]));
