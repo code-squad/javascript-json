ce9c39b331dc9c73b00c722103a85bcbc8648db2
MCC <cocahack@naver.com>
2019-05-07 16:15:35 +0900

    refactor: 프로그램 진입점 수정
    파일에 쓴 testcase를 읽어오지 않고 값을 할당하여 프로그램을 실행하도록
    변경
    추후 유닛 테스트 모듈을 사용할 예정

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
+const Tokenizer = require('./tokenizer');
+const Lexer = require('./lexer');
 const ArrayParser = require('./arrayParser');
-const rl = readline.createInterface(
-  { input: fs.createReadStream(path.join(__dirname, './testcase/test1.in')),
-  }
-  );
 
-rl.on('line', line => {
-  try{
-    const arrayParser = new ArrayParser(line);
-    const result = arrayParser.analysis();
-    console.log(JSON.stringify(result, null, 4));
-  } catch(error){
-    console.error(`Error: ${error.message}`);
-  }
-});
+const text = `['1a3', [null, false, ['11', [ 112233 ] , 112 ], 55 , '99'], 33, true]`
+
+const tokenizer = new Tokenizer(text);
+const lexer = new Lexer(tokenizer);
+const arrayParser = new ArrayParser(lexer);
+
+
+const result = arrayParser.analysis();
+
+console.dir(result, {depth: null, });
