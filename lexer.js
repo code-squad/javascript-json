41f35078fbec7236c3404bdbe97867c8ad566f80
sonypark <sonypark0204@gmail.com>
2019-05-07 16:23:23 +0900

    feat: Create function to check valid string

--- lexer.js
+++ lexer.js
@@ -6,12 +6,20 @@ class Lexer{
         if(token === ']') return new Node('arrEnd', ']');
 
         if(isFinite(Number(token))) return new Node('number', token);
-        if(token.startsWith("'") && token.endsWith("'")) return new Node('string', token);
+        if (this.isString(token)) return new Node('string', token);
         
         if(token === 'null') return new Node('null', token);
         if(token === 'true' || token ==='false') return new Node('boolean', token);
         
         else throw Error(`${token} undefined token type error`);
+    }
+
+    isString(token){
+        if (token.startsWith("'") || token.endsWith("'")) {
+            const splitedTokenByQuote = token.split("'").filter(Boolean);
+            if (splitedTokenByQuote.length === 1) return true;
+            else throw Error (`${token}은 올바른 문자열이 아닙니다.`);
+        }else return false;
     }
 }
 
