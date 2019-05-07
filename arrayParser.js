025a5a08e35d6470e009aa9574929cbfdabc39bc
DESKTOP-6B6U099\eunjin <audar8@gmail.com>
2019-05-07 10:41:13 +0900

    클래스및메서드생성
    - ArrayParser 클래스 생성
    - tokenizer 메서드 생성
    - lexer 메서드 생성
    - parser 메서드 생성
    - showParsedResult 메서드 생성

---
+++ arrayParser.js
@@ -0,0 +1,24 @@
+const jsonStr = "[123, 22, 33]";
+
+function ArrayParser() {
+}
+
+//토큰화 기능
+ArrayParser.prototype.tokenizer = function(str) {
+
+}
+
+// lexer 토큰 의미부여하는 기능(type check)
+ArrayParser.prototype.lexer = function() {
+
+}
+
+//렉서한 내용을 분석하여 보여줌
+ArrayParser.prototype.parser = function() {
+    
+}
+
+//결과값 보여주는 함수
+ArrayParser.prototype.showParsedResult = function() {
+   
+}
