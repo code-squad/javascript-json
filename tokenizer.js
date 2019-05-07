884caf0822641dd49eaa8d25147b40263658f29c
P-iknow <apricotsoul@gmail.com>
2019-05-07 16:36:54 +0900

    refector: parser 함수 밖에서 rootnode를 주입하는로직 변경
    - Array Parser
    기존 arrayParser 함수에서rootnode =  lexedTokens.shift()을 통해
    루트 노드를 미리 선정하는 로직이 있었으나 의도가 분명하지 않고
    해당 로직이 parser 함수 밖에서 이루어져야 할 필요가 없어서
    모든 로직이 parser 함수 내부에서 실행되도록 변경
    
    - Tokenizer
    위 로직을 변경을 위해서는 처음과 끝의 [, ] 가 불필요하여 삭제하는 로직
    추가
    
    - Parser
    위 로직 변경을 위해 default parameter를 이용하여 rootnode 를 지정
    lexedTokens 과 rootnode parameter 순서 변경

--- tokenizer.js
+++ tokenizer.js
@@ -1,8 +1,9 @@
+const util = require("./util");
 const tokenize = (string) => {
+    string = util.deleteFirstLastChar(string);
     return string.replace(/\[/g, "\[,")
                  .replace(/\]/g, ",]")
                  .split(",")
                  .map(v => v.trim());
 }
-
 module.exports = tokenize;
