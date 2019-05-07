e462aa87ed25c93c9b7e8565de343dd4851c11f7
sonypark <sonypark0204@gmail.com>
2019-05-07 16:49:21 +0900

    test: Add test case

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const ArrayParser = require('./ArrayParser');
 
-const str = "[123, [22, 33], 234, [-45, 0.6], '78', '6', [[null, false], true]]";
-result = ArrayParser.parse(str);
+const str1 = "[123, [22, 33], 234, [-45, 0.6], '78', '6', [[null, false], true]]";
+const str2 = "['12'3', [22, 33], 234, [-45, 0.6], '78', '6', [[null, false], true]]"; // invalid string error
+const str3 = "[12d3, [22, 33], 234, [-45, 0.6], '78', '6', [[null, false], true]]"; // invalid type error
+result = ArrayParser.parse(str1);
 
 console.log(result);
 console.log(JSON.stringify(result, null, 2));
