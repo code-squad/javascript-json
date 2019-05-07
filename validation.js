97b35d2dae1f2f71d0a2a7ba1eb518e8bd74a482
janghyoseog <bestdevhyo1225@gmail.com>
2019-05-07 21:28:34 +0900

    값의 Type을 확인하는 Validation 객체 리터럴 구현 및 테스트
    - checkTypeOf(value) : Type을 확인하면서 일치하는 Type이 있으면, 해당 Type을 string으로 반환하는 메소드

---
+++ validation.js
@@ -0,0 +1,60 @@
+const validation = {
+    isNull      : function (value) { return value === 'null'; },
+
+    isArray     : function (value) { return Array.isArray(value); },
+
+    isNumber    : function (value) { return !isNaN(Number(value)); },
+
+    isBoolean   : function (value) { return value === 'true' || value === 'false'; },
+
+    isString    : function (value) { 
+        if (!value.startsWith("'") || !value.endsWith("'"))         return false;
+        if (value.substr(1, value.length-2).indexOf("'") !== -1)    return false;
+        return true;
+    },
+
+    checkTypeOf : function (value) {
+        if (this.isNull(value))     return 'null';
+        if (this.isArray(value))    return 'array';
+        if (this.isNumber(value))   return 'number';
+        if (this.isBoolean(value))  return 'boolean';
+        if (this.isString(value))   return 'string';
+        throw Error('Type Error!!');
+    },
+}
+
+module.exports = validation;
+
+// console.log(`isArray : `, validation.isArray([1, 2, 3]));
+// console.log(`isArray : `, validation.isArray([ {key : 'a', value : 0}, {key : 'b', value : 1}, {key : 'c', value : 2}]));
+// console.log(`isArray : `, validation.isArray(['1', '2', '3']));
+// console.log(`isArray : `, validation.isArray('123'));
+// console.log(`isArray : `, validation.isArray('null'));
+// console.log(`isArray : `, validation.isArray("'string'"));
+
+// console.log(`isNull : `, validation.isNull('null'));
+// console.log(`isNull : `, validation.isNull([1,2,3]));
+// console.log(`isNull : `, validation.isNull([ {key : 'a', value : 0}, {key : 'b', value : 1}, {key : 'c', value : 2}]));
+// console.log(`isNull : `, validation.isNull('123'));
+// console.log(`isNull : `, validation.isNull('true'));
+
+// console.log(`isNumber : `, validation.isNumber('123'));
+// console.log(`isNumber : `, validation.isNumber('1d3'));
+// console.log(`isNumber : `, validation.isNumber('d123'));
+// console.log(`isNumber : `, validation.isNumber('123d'));
+// console.log(`isNumber : `, validation.isNumber('d123d'));
+
+// console.log(`isBoolean : `, validation.isBoolean([ {key : 'a', value : 0}, {key : 'b', value : 1}, {key : 'c', value : 2}]));
+// console.log(`isBoolean : `, validation.isBoolean('true'));
+// console.log(`isBoolean : `, validation.isBoolean('false'));
+// console.log(`isBoolean : `, validation.isBoolean('123'));
+// console.log(`isBoolean : `, validation.isBoolean('123d'));
+// console.log(`isBoolean : `, validation.isBoolean('ddd123'));
+
+// console.log(`isString : `, validation.isString("'abc'"));
+// console.log(`isString : `, validation.isString("'abc"));
+// console.log(`isString : `, validation.isString("abc'"));
+// console.log(`isString : `, validation.isString("''abc''"));
+// console.log(`isString : `, validation.isString("'ab'c'"));
+// console.log(`isString : `, validation.isString("'''ab'c'"));
+// console.log(`isString : `, validation.isString("ab'''c'"));
