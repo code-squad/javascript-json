53fdf72beaf1ad7acc48e4ea7b5f6770d8980118
nailerHeum <kingman330@gmail.com>
2019-05-07 13:39:05 +0900

    Implement textTokenize()
      단순 사용자 입력을 token들로 만들어서 처리하기 용이하도록 만든다.

--- arrayParser.js
+++ arrayParser.js
@@ -1,28 +1,67 @@
 class ArrayParser{
   constructor () {
-
+    this.stack = []
   }
-  arrayParse(input){
-    input = input.trim()
+  textTokenize(input){
+    let chars = input.split('');
+    let tokens = []
+    let startIndex = 0
+    let stringIndex = -2
+    console.log('tokenizer run')
+    chars.forEach((char, index) => {
+      if(stringIndex > -1){
+        stringIndex -=1
+        return
+      } 
+      if(char === "'"){
+        startIndex = index+1
+        stringIndex = chars.slice(index+1).findIndex(e => e==="'")
+        if(stringIndex < 0) throw new Error('잘못된 문자열이 포함되어있습니다.')
+        tokens.push("'" + input.slice(startIndex, startIndex + stringIndex) + "'")
+        startIndex +=stringIndex + 1
+        console.log(input)
+      }
+      if(char === '['){
+        startIndex = index + 1
+        tokens.push(char)
+      }
+      if(char === ']'){
+        tokens.push(input.slice(startIndex, index))
+        startIndex = index + 1
+        tokens.push(char)
+      }
+      if(char === ','){
+        tokens.push(input.slice(startIndex, index))
+        startIndex = index+1
+      }
+    });
+    return tokens
+  }
+  parseWithTokens(tokens){
     let result = {}
-    
-    if (input[0] === '[' && input[input.length-1] === ']') {      // array
-      const tokens = input.slice(1,input.length-1).split(',');
-      result = { type: 'array', child: [] };
-      
-      tokens.forEach(element => {
-        result.child.push(this.arrayParse(element))
-      });
-      return result
+    if(tokens[0] === '[' && tokens.pop() === ']'){
+      result = { type: 'array', child: [] }
     }
-    
-    if (!isNaN(input)) {                         //number value 
-      result = { type: 'number', value: input }
-      return result
+    if(tokens[0] === '{' && tokens.pop() === ']'){
+      result = { type: 'object', child: {}}
     }
+    tokens.forEach(token => {
 
-    throw new Error('Invalid Token')
+    });
+    return result
   }
 }
 
 module.exports = ArrayParser
+
+// A tokenizer breaks a stream of text into tokens, 
+// usually by looking for whitespace (tabs, spaces, new lines).
+
+// A lexer is basically a tokenizer, 
+// but it usually attaches extra context to the tokens -- 
+// this token is a number, that token is a string literal, 
+// this other token is an equality operator.
+
+// A parser takes the stream of tokens from the lexer 
+// and turns it into an abstract syntax tree representing 
+// the (usually) program represented by the original text.
--- input.js
+++ input.js
@@ -11,8 +11,13 @@ const arrayParser = new ArrayParser();
 (() => {
   rl.question('ArrayParser 사용자 입력 : ', (userInput) => {
     try {
-      let result = arrayParser.arrayParse(userInput)
-      console.log(JSON.stringify(result, null, 2)); 
+      // arrayParser.checkChars(userInput)
+      let tokens = arrayParser.textTokenize(userInput)
+      let result = arrayParser.parseWithTokens(tokens)
+
+      console.log(result)
+      //console.log(JSON.stringify(result, null, 2)); 
+      
     } catch (e) {
       console.log(e.message)
     }
