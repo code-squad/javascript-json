474f91b3197b7954518dd3d3e1c189f548a55f7d
Jimin Yeon <gmini.y@gmail.com>
2019-05-08 17:05:37 +0900

    Node class 구현
    -lex 한 token을 이후에 node 객체로  리턴하기 위함.

--- parser.js
+++ parser.js
@@ -1,7 +1,16 @@
+class Node {
+    constructor(type, value) {
+        this.type   = type;
+        this.value  = value;
+        this.child  = [];
+    }
+}
+
 function tokenize(string) {
     const noWhiteSpaceString = string.replace(/ /g, '');
     return noWhiteSpaceString.split(/([\[\]])|,/).filter((value) => {return value});
 }
+
 
 const str = "[123, 22, 33]";
 tokenize(str);
