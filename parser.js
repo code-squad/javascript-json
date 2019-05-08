98771bd3ec684912e62b3fc3ade16f2248bdae48
Jimin Yeon <gmini.y@gmail.com>
2019-05-08 18:10:31 +0900

    parser function 구현 중 : type array면 parentNode로 설정하는 기능 구현

--- parser.js
+++ parser.js
@@ -22,6 +22,18 @@ function lex(tokens) {
         }, []);
 }
 
+
+function parse(lexedTokens) {
+    let parentNode = {};
+    lexedTokens.forEach(function (lexedToken) {
+        if(lexedToken.type === 'array') {
+            parentNode = lexedToken;
+        }
+    });
+    return parentNode;
+}
+
 const str = "[1.23, 2.2, 3.3]";
 const tokens = tokenize(str);
 const lexedTokens = lex(tokens);
+console.log(parse(lexedTokens));
