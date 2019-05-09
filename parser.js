21d3c8c10fe924692d1e9595300311c78878e763
Jimin Yeon <gmini.y@gmail.com>
2019-05-09 18:11:31 +0900

    ArrayParser class 생성
    - function들을 ArrayParser method로 변경
    - run method 에서 parse과정을 한번에 수행.

--- parser.js
+++ parser.js
@@ -6,47 +6,53 @@ class Node {
     }
 }
 
-function tokenize(string) {
-    const noWhiteSpaceString = string.replace(/ /g, '');
-    return noWhiteSpaceString.split(/([\[\]])|,/).filter((value) => value);
-}
+class ArrayParser {
+    tokenize(string) {
+        const noWhiteSpaceString = string.replace(/ /g, '');
+        return noWhiteSpaceString.split(/([\[\]])|,/).filter((value) => value);
+    }
 
-function lex(tokens) {
-    let token = {};
-    return tokens.reduce((acc, value) => {
-            if(value === '[') {
-                token = {'name' : 'ArrayOpener', 'value' : value}
-                return acc.concat(token);
-            } else if(!isNaN(value)) {
-                token = {'name' : 'Number', 'value' : value}
-                return acc.concat(token);
-            } else if(value === ']') {
-                token = {'name' : 'ArrayCloser', 'value' : value}
-                return acc.concat(token);
+    lex(tokens) {
+        let token = {};
+        return tokens.reduce((acc, value) => {
+                if(value === '[') {
+                    token = {'name' : 'ArrayOpener', 'value' : value}
+                    return acc.concat(token);
+                } else if(!isNaN(value)) {
+                    token = {'name' : 'Number', 'value' : value}
+                    return acc.concat(token);
+                } else if(value === ']') {
+                    token = {'name' : 'ArrayCloser', 'value' : value}
+                    return acc.concat(token);
+                }
+            }, []);
+    }
+
+    parse(lexedTokens) {
+        let parentNode = {};
+        lexedTokens.forEach((lexedToken) => {
+            if(lexedToken.name === 'ArrayOpener') {
+                const arrayNode = new Node('array');
+                delete arrayNode.value;
+                parentNode = arrayNode;
+            } else if(lexedToken.name === 'Number') {
+                const numberNode = new Node('number', lexedToken.value);
+                parentNode.child.push(numberNode);
+            } else if(lexedToken.name === 'ArrayCloser') {
+                return;
             }
-        }, []);
-}
+        });
+        return parentNode;
+    }
 
-function parse(lexedTokens) {
-    let parentNode = {};
-    lexedTokens.forEach((lexedToken) => {
-        if(lexedToken.name === 'ArrayOpener') {
-            const arrayNode = new Node('array');
-            delete arrayNode.value;
-            parentNode = arrayNode;
-        } else if(lexedToken.name === 'Number') {
-            const numberNode = new Node('number', lexedToken.value);
-            parentNode.child.push(numberNode);
-        } else if(lexedToken.name === 'ArrayCloser') {
-            return;
-        }
-    });
-    return parentNode;
+    run(string) {
+        const tokens = this.tokenize(string);
+        const lexedTokens = this.lex(tokens);
+        return this.parse(lexedTokens);
+    }
 }
 
 const str = "[123, 22, 33]";
-const tokens = tokenize(str);
-const lexedTokens = lex(tokens);
-const result = parse(lexedTokens);
-
-console.log(JSON.stringify(result, null, 2)); 
+const arrayParser = new ArrayParser();
+const result = arrayParser.run(str);
+console.log(JSON.stringify(result, null, 2));
