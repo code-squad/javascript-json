cf40c84b2dcc1d3e5a7268457ac69f90cf139a56
Ryujeuk <jeuk817@gmail.com>
2019-05-09 13:35:10 +0900

    refactor: tokenizer를 다른파일로 분리함

---
+++ ArrayParser6-2.js
@@ -0,0 +1,4 @@
+class ArrayParser {
+
+
+} 
---
+++ tokenizer6-2.js
@@ -0,0 +1,34 @@
+
+class Tokenizer {
+
+    tokenizer(string) {
+        string = string.split('');
+        const array = [];
+        let token = '';
+
+        while (string.length > 0) {
+            const onePiece = string.shift();
+            if (onePiece === '[') {
+                array.push(onePiece);
+                continue;
+            }
+            if (onePiece === ']') {
+                if (token !== '') {
+                    array.push(token);
+                }
+                array.push(onePiece);
+                token = '';
+                continue;
+            }
+            if (onePiece === ',') {
+                if (token !== '') array.push(token)
+                token = '';
+                continue;
+            }
+            token += onePiece;
+        }
+
+        return array.map(x => x.trim());
+    }
+
+}
