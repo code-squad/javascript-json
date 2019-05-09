c3f32ed2df13538e116d8139112347db3c12528f
allen <revenoir7@gmail.com>
2019-05-09 22:37:42 +0900

    feat: 여는괄호 없는 상태에서 닫힌괄호 나올경우 에러처리 추가

--- errorChecker.js
+++ errorChecker.js
@@ -2,21 +2,23 @@ class ErrorChecker {
     checkBrackets(tokens) {
         let openArrCount = 0;
         let openObjCount = 0;
-        for(let token of tokens) {
-            if(token === '[') openArrCount++;
-            if(token === ']') openArrCount--;
-            if(token === '{') openObjCount++;
-            if(token === '}') openObjCount--;
+        for (let token of tokens) {
+            if (token === '[') openArrCount++;
+            if (token === ']') openArrCount--;
+            if (token === '{') openObjCount++;
+            if (token === '}') openObjCount--;
+            if (openArrCount < 0) throw Error('올바르지 않은 배열표현이 포함되어 있습니다.')
+            if (openObjCount < 0) throw Error('올바르지 않은 객체표현이 포함되어 있습니다.')
         }
-        if(openArrCount) throw Error('올바르지 않은 배열표현이 포함되어 있습니다.')
-        if(openObjCount) throw Error('올바르지 않은 객체표현이 포함되어 있습니다.')
+        if (openArrCount) throw Error('올바르지 않은 배열표현이 포함되어 있습니다.');
+        if (openObjCount) throw Error('올바르지 않은 객체표현이 포함되어 있습니다.');
     }
 
     checkNodes(nodes) {
-        for(let node of nodes) {
-            if(node.type === 'withSpace') throw Error(`${node.value} 는 콜론(:)이 누락된 객체표현이거나 공백이 포함된 데이터표현입니다.`)
-            if(node.type === 'wrongString') throw Error(`${node.value} 는 올바른 문자열이 아닙니다.`)
-            if(node.type === 'unknownType') throw Error(`${node.value} 는 알 수 없는 타입입니다.`)
+        for (let node of nodes) {
+            if (node.type === 'withSpace') throw Error(`${node.value} 는 콜론(:)이 누락된 객체표현 또는 공백이 포함된 데이터표현입니다.`);
+            if (node.type === 'wrongString') throw Error(`${node.value} 는 올바른 문자열이 아닙니다.`);
+            if (node.type === 'unknownType') throw Error(`${node.value} 는 알 수 없는 타입입니다.`);
         }
     }
 }
