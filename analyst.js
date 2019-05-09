21d0d1392d65b0138a4859d16baac91b190bfaa1
allen <revenoir7@gmail.com>
2019-05-09 22:59:48 +0900

    feat: type배열을 count해서 출력하도록 countTypes 메소드 및 printCounts메소드 작성

--- analyst.js
+++ analyst.js
@@ -2,6 +2,9 @@ class Analyst {
     constructor() {
         this.typeList = [];
     }
+    initList() {
+        this.typeList = [];
+    }
 
     getAllNodeTypes(parseTree) {
         for (let childNode of parseTree.child) {
@@ -12,10 +15,28 @@ class Analyst {
     }
 
     countTypes(typeList) {
+        const counts = {
+            array: 0,
+            object: 0,
+            number: 0,
+            string: 0,
+            boolean: 0,
+            null: 0
+        }
+        for(let type of typeList) {
+            counts[type] += 1;
+        }
+        return counts;
+    }
 
+    printCounts(counts) {
+        const result = Object.entries(counts).map(([key, value]) => `${key} : ${value}개`).join(',\n');
+        console.log(result);
     }
 
     run(parseTree) {
+        this.initList();
+        const typeList = this.getAllNodeTypes(parseTree);
 
     }
 }
