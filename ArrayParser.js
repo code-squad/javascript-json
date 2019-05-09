51fabaf669084b28a9e6c49c9ce27a8d11868727
sunwook kim <koownusmik@gmail.com>
2019-05-09 15:20:32 +0900

    refactor: str,boolean,null지원, 에러 구현
    1. 올바른 문자열 체크 에러구현 위치와 typeof token === string 으로 한번더 따진부분이 아쉬움
    2. tokenize안에 내부함수를 구현하지않아서 이렇게 된것같음.
    3. null은 typeof null이 object로 나옴.
    4. single quote, double quote 지원을 안했음.
    5. 처음에 90개의 커밋이 왜 같이 넘어왔는지 모르겠음. git reset --hard head^87로 자체삭제함

--- ArrayParser.js
+++ ArrayParser.js
@@ -1,53 +1,52 @@
 const { log } = console;
 
-
 const ArrayParser = {
     historyStack: [],
 
     run(input) {
         const tokenQueue = this.tokenize(input)
         // 테스트코드 1 -> 
-        // log(this.tokenQueue)
+        // log(tokenQueue)
         const fetchedQueue = this.lex(tokenQueue);
         // 테스트코드 2 -> 
-        // log(tokenQueue)
+        // log(fetchedQueue)
         const resultTree = this.parse(fetchedQueue);
         return resultTree
     },
 
-
     tokenize(input) {
-
         const tokens = input.replace(/\[/g, '[,').replace(/\]/g, ',]').replace(/ /g, '').split(',');
-        return tokens.map(el => isNaN(Number(el)) ? el : Number(el));
-    
-    },
-
-    lex(queue) {
-         return queue.map(token => {
-            if (!isNaN(token)) return { type: typeof (token), value: token, child: [] }
-            if (['[', ']'].includes(token)) return { type: 'array', value: token, child: [] }
+        return tokens.map(token => {
+            if (token === 'null') return null;
+            if (['true', 'false'].includes(token)) return (token === 'true');
+            if (!isNaN(token)) return Number(token);
+            if (token[0] === "'" && token[token.length - 1] === "'") return token.slice(1, -1)
+            if (['[', ']'].includes(token)) return token
+            else throw new Error(`${token}은 알수없는 타입입니다.`)
         })
     },
 
+    lex(queue) {
+        return queue.map(token => {
+            if (['[', ']'].includes(token)) return { type: 'array', value: token, child: [] }
+            else {
+                if (typeof token === 'string' && token.includes("'")) throw new Error(`${token}은 올바른 문자열이 아닙니다.`)
+                return { type: typeof (token), value: token, child: [] }
+            }
+        })
+
+    },
 
     parse(queue) {
-
         while (queue.length) {
             const currentLexedObj = queue.shift()
-
             if (currentLexedObj.value === '[') {
                 delete currentLexedObj.value
                 this.historyStack.push(currentLexedObj)
-
             }
 
-            else if (currentLexedObj.type === 'number') {
-                const last = this.historyStack.length - 1
-                this.historyStack[last].child.push(currentLexedObj);
-
-            }
             else if (currentLexedObj.value === ']') {
+
                 delete currentLexedObj.value
                 const tempResultTree = this.historyStack.pop();
                 const newLast = this.historyStack.length - 1
@@ -60,19 +59,18 @@ const ArrayParser = {
                 }
             }
 
-
-
-
+            else {
+                const last = this.historyStack.length - 1
+                this.historyStack[last].child.push(currentLexedObj);
+            }
         }
-
-
-
     }
-
 }
 
-// const str = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
-const str = "[1, 2, [ 3, [ 4, 5, [ 6 ], 7, [ 8, 9 ], 100 ]  ]  ]";
+// var str = "['1a3',[22,23,[11,[112233],112],55],3d3]"
+// var str = "['1a'3',[22,23,[11,[112233],112],55],33]";
+const str = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
+// const str = "[1, 2, [ 3, [ 4, 5, [ 6 ], 7, [ 8, 9 ], 100 ]  ]  ]";
 const result = ArrayParser.run(str);
 // 테스트코드3 
-log(JSON.stringify(result, null, 2)); 
+log(JSON.stringify(result, null, 2));  
