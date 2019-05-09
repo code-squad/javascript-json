758183a892e68d62b94228f4d7b371653ee1f8ae
Ryujeuk <jeuk817@gmail.com>
2019-05-09 14:10:49 +0900

    feat: Lexer, Tokenizer 수입, constructor생성, 인스턴스
    1. 수입한 tokenizer, lexer를 ArrayParser에 인자로 넣고, constructor에
    생성함.
    2. this.stack 생성

--- ArrayParser6-2.js
+++ ArrayParser6-2.js
@@ -1,4 +1,15 @@
+const Tokenizer = require('./tokenizer6-2');
+const Lexer = require('./lexer6-2');
+
 class ArrayParser {
 
+    constructor(obj) {
+        this.tokenizer = obj.tokenizer;
+        this.lexer = obj.lexer;
+        this.stack = [];
+    }
+}
 
-} 
+const tokenizer = new Tokenizer();
+const lexer = new Lexer();
+const arrayParser = new ArrayParser({ tokenizer, lexer });
