599892b0ea3161fac4bfa92a80b62f267da1a95f
LeeJuyeon <ruby413@hanmail.net>
2019-05-09 16:13:26 +0900

    ArrayParser 의 tokenizer method 생성

--- arrayParser2.js
+++ arrayParser2.js
@@ -1,4 +1,40 @@
+// { type: 'array',
+//   child: 
+//    [ { type: 'number', value: '123', child: [] },
+//      { type: 'number', value: '22', child: [] },
+//      { type: 'number', value: '33', child: [] } 
+//     ] 
+// }
+
 const str = "['1a3',[null,false,['11',[11223123],112],55, '99'],33, true]"
+
+const ArrayParser = class {
+    constructor(){
+        this.tokenArr = []
+    }
+
+    tokenizer(str){
+        let word = ""
+        for(let i = 0; i<str.length; i++){
+            if(str[i] === "[" || str[i] === "," || str[i] === "]"){
+                this.tokenArr.push(word)                                        //일반토큰 푸쉬
+                word = ""
+                if(str[i] === "[" || str[i] === "]"){this.tokenArr.push(str[i])} //배열 토큰 푸쉬
+            }else{
+                if(!(str[i] == " " && str[i] == "[")){word = word + str[i]}     //일반 토큰 생성
+            }
+        }
+        this.tokenArr.forEach((token) => this.RemoveWhitespace(token))
+        return this.tokenArr
+    }
+
+    RemoveWhitespace(token){
+        if(token===""){this.tokenArr.splice(this.tokenArr.indexOf(""),1)}
+    }
+}
+
+const arrayParser = new ArrayParser()
+console.log(arrayParser.tokenizer(str).length)
 
 const lexer = (arr) => {
     let i = 0;
