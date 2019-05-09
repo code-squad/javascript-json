39c579a52f019aab74a59e5d4c2f9d6bf77cbcb9
ymink16 <yminkdev@gmail.com>
2019-05-09 16:25:11 +0900

    tokenizer 함수에서 공백과 [] 관련 에러 수정

--- step6-2_arrayParser.js
+++ step6-2_arrayParser.js
@@ -4,15 +4,15 @@ const tokenizer = str => {
     const tokens = [];
 
     for (let token of temp) {
-        if (token.indexOf('[') === 0 && token.indexOf(']') === token.length-1) {
+        if (token.indexOf('[') !== -1 && token.indexOf(']') !== -1) {
             tokens.push('[');
-            tokens.push(token.trim().substr(1, token.length-2));
+            tokens.push(token.trim().substr(token.indexOf('[')+1, token.indexOf(']')-1));
             tokens.push(']');
-        } else if (token.indexOf('[') === 0) {
+        } else if (token.indexOf('[') !== -1) {
             tokens.push('[');
-            tokens.push(token.trim().substr(1));
-        } else if (token.indexOf(']') === token.length-1) {
-            tokens.push(token.trim().substr(0, token.length-1));
+            tokens.push(token.trim().substr(token.indexOf('[')+1));
+        } else if (token.indexOf(']') !== -1) {
+            tokens.push(token.trim().substring(0, token.indexOf(']')));
             tokens.push(']');
         } else {
             tokens.push(token.trim());
@@ -24,23 +24,29 @@ const tokenizer = str => {
 
 const lexer = (str) => {  
     const temp = tokenizer(str);
-    const tokens = [];
-
+    const tokens = temp.map( token => {
+        return getType(token);
+    });
+    return tokens;
 }
 
-const checkType = (token) => {
-    if (typeof(Number(token)) === 'number') {
-        return 'number';
-    } else if (Boolean(token) === true || Boolean(token) === false) {
-        return 'boolean';
+const getType = (token) => {
+    if (!isNaN(Number(token))) {
+        return {type: 'number', value: Number(token)};
+    } else if (token === 'true' || token === 'false') {
+        return {type: 'boolean', value: Boolean(token)};
     } else if (token === 'null') {
-        return 'null';
+        return {type: 'null', value: null};
+    } else if (token ==='[') {
+        return {type: 'array', value: token};
+    } else if (token === ']') {
+        return {type: 'array', value: token};
     } else if (token.substr(1, token.length-2).indexOf('\'') !== -1) {
         console.error(`${token}은 올바른 문자열이 아닙니다.`);
     } else if (token.substr(1, token.length-2).indexOf('\"') !== -1) {
         console.error(`${token}은 올바른 문자열이 아닙니다.`);
     } else if (typeof(token) === "string") {
-        return 'string';
+        return {type: 'string', value: token};
     } else {
         console.error(`${token}은 알 수 없는 타입입니다.`);
     }
@@ -52,4 +58,7 @@ const arrayParser = () => {
 
 var s1 = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
 var s2 = "['1a'3',[22,23,[11,[112233],112],55],33]";
-console.log(tokenizer(s2));
+
+console.log(s1.split(','));
+console.log(tokenizer(s1));
+
