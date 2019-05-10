e4268bf0b6b762f660aa0b82f32b99e066e445ca
aftersnowing <gurcks2@gmail.com>
2019-05-10 23:40:36 +0900

    테스트 케이스 console.log

--- arrayParser.js
+++ arrayParser.js
@@ -130,4 +130,5 @@ class ArrayParser {
 }
 
 const arrayParser = new ArrayParser();
-console.log(arrayParser.executeParser("['1a3',[null,false,['11',[112233],112],55, '99'],33, true]"));
+var test = arrayParser.executeParser("['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]");
+console.log(JSON.stringify(test, null, 2));
