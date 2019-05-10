1ec3b8d415314e55436cbc56af469d3d71167662
mukeunzi <eunji057@gmail.com>
2019-05-10 23:11:44 +0900

    feat : tokenizer 메소드 구현
    - inputStr 을 모든 토큰으로 쪼개기
    - 쪼갠 토큰 중 string 은 다시 조합

---
+++ array_parser.js
@@ -0,0 +1,40 @@
+class ArrayParser {
+	constructor() {
+		this.result = { type: '', child: [] };
+	}
+
+	tokenizer(inputStr) {
+		const stack = [];
+		const inputChar = inputStr.split('');
+		const token = [];
+		let makeStr = '';
+
+		for (let char of inputChar) {
+			if (stack.length === 0) {
+				if (char === "'" || char === '"') {
+					stack.push(char);
+				} else if (char !== ' ') {
+					token.push(char);
+				}
+			} else {
+				if (char === "'" || char === '"') {
+					token.push(makeStr);
+					makeStr = '';
+					stack.pop();
+				} else {
+					makeStr += char;
+				}
+			}
+		}
+
+		return token;
+	}
+
+	arrayParser(inputStr) {
+		console.log(this.tokenizer(inputStr));
+	}
+}
+
+const inputStr = '[1, 2, "a,    b"]';
+const myArrayParser = new ArrayParser();
+myArrayParser.arrayParser(inputStr);
