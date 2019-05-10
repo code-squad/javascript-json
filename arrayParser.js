3b0b696af488e3f83c32829e92bf30de24ffb027
Hoyoung Jung <honux77@gmail.com>
2019-05-10 15:49:44 +0900

    Revert "Ver2 step6 2"

--- arrayParser.js
+++ arrayParser.js
@@ -1,123 +1,17 @@
+function arrayParser(tokens) {
 
-class arrayParser {
-    constructor(tokens) {
-        this.tokens = tokens;
-        this.current = 0;
-        this.quene = [];  
-    }
-    Lexical_Analysis_And_Array_Parsing() {
-        let token = this.tokens[this.current];
-        if(token.type === 'number') {
-            this.current++;
-            return {
-                type: 'number',
-                value: token.value,
-                child: []
-            }
-        }
+    let ast = {
+        type: 'array',
+        child: []
+    };
 
-        if(token.type === 'string') {
-            this.current++;
-            return {
-                type: 'string',
-                value: token.value,
-                child: []
-            }
-        }       
+    tokens.forEach(function(element) {
+      if(element.type === 'number') {
+          ast.child.push(element);
+      }
+    })
 
-        if(token.type === 'boolean') {
-            this.current++;
-            return {
-                type: 'boolean',
-                value: token.value,
-                child: []
-            }
-        }        
-
-        if(token.type === 'null') {
-            this.current++;
-            return {
-                type: 'null',
-                value: token.value,
-                child: []
-            }
-        }    
-
-        if((token.type === 'array' && token.value === '[')) {
-                    this.current++;
-                    token = this.tokens[this.current];  
-                    
-                    let node = {
-                        type: "array",
-                        child: [],
-                    }
-                    
-                    while((token.type !== 'array') || (token.type === 'array' && token.value !== ']')) {
-                        node.child.push(this.Lexical_Analysis_And_Array_Parsing());
-                        token = this.tokens[this.current];
-                    }
-                    
-                    this.quene.push(node);
-                    // node를 갱신시키기위해서, quene에 현재 node를 넣고 return
-                    return node;
-                }                
-            
-        if((token.type === 'array' && token.value === "]")) {
-            
-            this.current++  
-            token = this.tokens[this.current];
-            const minLength = 1;
-
-            if(this.quene.length > minLength) {
-                const first_Quene_Element = 0;
-                let child_Of_ShfitedQuene = this.quene[first_Quene_Element].child;
-
-                this.quene.shift();   
-                
-                let Is_Array_In_CurrentQuene = this.quene[first_Quene_Element].child[this.quene[first_Quene_Element].child.length - 1].type;
-                
-                // 'array'가 child의 property로 있다는 것은 array child안에 nested array 방식으로 다른 값이 있기 때문에, 이전에 갱신된 값을 갱신되기 전 값을 가지고 있는 child의 부모항목의 quene에 = 을 이용해 할당해준다.
-
-                // 그리고 IS_Array_In_CurrentQuene 값을 child 항목의 제일 마지막 index로 준 이유는 [1,1,[2, 와 같이 array가 있다면 무조건 제일 마지막에 있어야 nested array가 발생할 수 있기 때문이다.
-                if(Is_Array_In_CurrentQuene === 'array') {
-                        this.quene[first_Quene_Element].child[this.quene[first_Quene_Element].child.length - 1].child = [child_Of_ShfitedQuene];                       
-                }
-
-                while((token.type !== 'array') || (token.type === 'array' && token.value !== "]")) {
-                        this.quene[first_Quene_Element].child.push(this.Lexical_Analysis_And_Array_Parsing());
-                        token = this.tokens[this.current]; 
-                }
-
-                this.Lexical_Analysis_And_Array_Parsing();
-            }
-            
-            if(this.current < this.tokens.length) {
-            while((token.type !== 'array') || (token.type === 'array' && token.value !== "]")) {
-                const current_Quene_index = 0;
-                this.quene[current_Quene_index].child.push(this.Lexical_Analysis_And_Array_Parsing());
-            }
-        }
-
-            return this.quene
-        }
-                
-    }
-     
-    executor() {
-        let return_In_Sequence = [];
-                                
-        while(this.current < this.tokens.length) {
-            return_In_Sequence.push(this.Lexical_Analysis_And_Array_Parsing());
-        }
-
-        let the_Most_recent_updated_value = return_In_Sequence[return_In_Sequence.length - 1];
-
-        const get_Child_Of_the_Most_recent_updated_Value = 0; 
-
-        return the_Most_recent_updated_value[get_Child_Of_the_Most_recent_updated_Value];
-    }
-
-}
+    return ast;
+  }
 
 module.exports = arrayParser;
-
--- arrayTokenizer.js
+++ arrayTokenizer.js
@@ -1,123 +1,95 @@
-const errorCheck = require('./errorCheck')
+function arraytokenizer(input) {
 
-const errorChecker = new errorCheck();
+  let current = 0;
+  let tokens = [];
 
-class arrayTokenizer {
-    constructor(input) {
-        this.input = input;
-        this.current = 0;
-        this.tokens = [];
+  while (current < input.length) {
+
+    let char = input[current];
+
+    if (char === '[') {
+
+      tokens.push({
+        type: 'array',
+        value: '[',
+      });
+
+      current++;
+
+      continue;
     }
 
-    tokenizer() {
-        while(this.current < this.input.length) {
+    if (char === ']') {
+      tokens.push({
+        type: 'array',
+        value: ']',
+      });
+      current++;
+      continue;
+    }
 
-          let char = this.input[this.current];
+    let WHITESPACE = /\s/;
+    if (WHITESPACE.test(char)) {
+      current++;
+      continue;
+    }
 
-          if(char === '[' || char === ']') {
+    let COMMA = /[,]/g;
+    if(COMMA.test(char)) {
+      current++;
+      continue;
+    }
 
-              if(char === '[') {
-                  this.tokens.push({
-                    type: 'array',
-                    value: '['
-              })} 
-              
-              else {
-                  this.tokens.push({
-                    type: 'array',
-                    value: ']'
-                  })
-              }
+    let NUMBERS = /[0-9]/;
+    if (NUMBERS.test(char)) {
 
-              this.current++;
+      let value = '';
 
-              continue;
-          }      
-
-          let WHITESPACE = /\s/;
-          if(WHITESPACE.test(char)) {
-              this.current++;
-              continue;
-          } 
-
-          let COMMA = /[,]/;
-          if (COMMA.test(char)) {
-            this.current++;
-            continue;
-          }
-
-          let NUMBERS = /[0-9]/ ; 
-          if (NUMBERS.test(char) || char === "-") {
-
-            let value = '';
-
-            while (NUMBERS.test(char) || char === "." || char === "-") {
-              value += char;
-              char = this.input[++this.current];
-            }
-
-            let valid_Num = errorChecker.check_Double_dash_and_dot(value);
-            if(valid_Num) {
-                this.tokens.push({ type: 'number', value})
-                continue;
-            }
-            else {
-                throw new TypeError(`${value} 는 올바른 숫자가 아닙니다.`);
-            }   
-          }
-
-          if(char === "'") {
-            this.current++;
-            let value = '';
-            char = this.input[this.current];
-
-            while(char !== "'") {
-                value += char;
-                if(char === ",") {
-                  let wrongString = errorChecker.check_improper_String(input, current);
-                  throw new TypeError(wrongString + " 은 올바른 문자열이 아닙니다.");
-              }
-                this.current++;
-                char = this.input[this.current];
-            }
-
-            this.current++
-            char = this.input[this.current];
-            
-            
-            this.tokens.push({type: 'string', value});
-
-            continue;
-          }
-
-          let LETTERS = /[a-z]/;
-          if (LETTERS.test(char)) {
-            let value = '';
-
-            while (LETTERS.test(char)) {
-              value += char;
-              char = this.input[++this.current];
-            }
-
-            if(value === 'true' || value === 'false') {
-                this.tokens.push({ type: 'boolean', value });
-                continue;
-            }
-            else if(value === 'null') {
-                this.tokens.push({ type: 'null', value });
-                continue;
-            } else {
-                throw new TypeError(`${value}가 어떤 타입인지 알 수 없습니다.`)
-            }
-            
-
-          }
-
-          throw new TypeError('어떤 타입인지 알 수 없습니다: ' + char);
-        }
-
-          return this.tokens;
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
       }
+
+      tokens.push({ type: 'number', value });
+
+      continue;
+    }
+
+    if (char === '"') {
+      let value = '';
+
+      char = input[++current];
+
+      while (char !== '"') {
+        value += char;
+        char = input[++current];
+      }
+
+      char = input[++current];
+
+      tokens.push({ type: 'string', value });
+
+      continue;
+    }
+
+    let LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)) {
+      let value = '';
+
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ type: 'name', value });
+
+      continue;
+    }
+
+    throw new TypeError('I dont know what this character is: ' + char);
+  }
+
+  return tokens;
 }
 
-module.exports = arrayTokenizer;
+module.exports = arraytokenizer;
