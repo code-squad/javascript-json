fe64ff60ccae53b4020501a737bb877b8e02b3a6
mukeunzi <eunji057@gmail.com>
2019-05-12 16:25:47 +0900

    feat : lexer 메소드 구현
    - 각 토큰의 type, value 를 리턴하는 getTypeAndValue 메소드 구현
    - lexer 메소드에서 getTypeAndValue 메소드 실행

--- array_parser.js
+++ array_parser.js
@@ -5,7 +5,7 @@ class ArrayParser {
 
 	tokenizer(inputStr) {
 		const stack = [];
-		const token = [];
+		const tokens = [];
 		let makeStr = '';
 
 		for (let char of inputStr) {
@@ -13,11 +13,11 @@ class ArrayParser {
 				if (char === "'" || char === '"') {
 					stack.push(char);
 				} else if (char !== ' ') {
-					token.push(char);
+					tokens.push(char);
 				}
 			} else {
 				if (char === "'" || char === '"') {
-					token.push(makeStr);
+					tokens.push(makeStr);
 					makeStr = '';
 					stack.pop();
 				} else {
@@ -26,11 +26,42 @@ class ArrayParser {
 			}
 		}
 
-		return token;
+		return tokens;
+	}
+
+	getTypeAndValue(token) {
+		if (!isNaN(token)) {
+			return { type: 'number', value: Number(token) };
+		} else {
+			const lowerStrToken = token.toLowerCase();
+			if (lowerStrToken === 'true') {
+				return { type: 'boolean', value: token };
+			}
+			if (lowerStrToken === 'false') {
+				return { type: 'boolean', value: token };
+			}
+			if (lowerStrToken === 'null') {
+				return { type: 'null', value: token };
+			}
+			return { type: 'string', value: token };
+		}
+	}
+
+	lexer(tokens) {
+		const lexerArr = [];
+
+		for (let token of tokens) {
+			if (token !== ',') {
+				lexerArr.push(this.getTypeAndValue(token));
+			}
+		}
+
+		return lexerArr;
 	}
 
 	arrayParser(inputStr) {
-		console.log(this.tokenizer(inputStr));
+		const tokens = this.tokenizer(inputStr);
+		console.log(this.lexer(tokens));
 	}
 }
 
