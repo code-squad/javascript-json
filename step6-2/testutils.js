148a6d620b9d785eb38436036ad4479211671a86
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-13 13:06:50 +0900

    Feat : assertObjArrEquals 메서드 추가
    parameter : expectedArr, actualArr, testFunction
    배열 안의 원소가 객체일 경우 객체를 순회하며 값 비교

--- step6-2/testutils.js
+++ step6-2/testutils.js
@@ -21,7 +21,7 @@ const test = {
         }
     },
 
-    // 배열 
+    // 배열 데이터 비교
     // todo : 재귀나 while 이용해 배열의 n depth 까지 검사하도록 개선 필요
     assertArrayEquals(expectedArr, actualArr, testFunction) {
 
@@ -48,6 +48,42 @@ const test = {
             return;
         }
     },
+
+    // obj Arr 데이터 비교
+    // todo : 원래 assertArrayEquals 메서드가 제대로 구현되었다면 필요없는 함수
+    // 나중에 합쳐서 어떤 배열이라도 판별할 수 있도록 refactoring 필요
+    assertObjArrEquals(expectedArr, actualArr, testFunction) {
+
+        let flag = true;
+
+        if (expectedArr.length === 0 && actualArr.length === 0) {
+            this.logSuccess(actualArr, expectedArr, testFunction);
+        } else {
+            expectedArr.forEach((obj, index) => {
+                Object.keys(obj).forEach((key) => {
+                    if (typeof expectedArr[index][key] === 'object') {
+                        this.assertObjArrEquals(expectedArr[index][key], actualArr[index][key], testFunction)
+                    } else {
+                        if(this.assertEquals(expectedArr[index][key], actualArr[index][key], testFunction)) {
+                            flag = false;
+                        };
+                    }
+                })
+            })
+        }
+
+        if(flag) {
+            console.log('========================================================')
+            this.logSuccess(actualArr, expectedArr, testFunction);
+            console.log('========================================================')
+            return;
+        } else {
+            console.log('========================================================')
+            this.logFail(actualArr, expectedArr, testFunction);
+            console.log('========================================================')
+            return;
+        }
+    }
 }
 
 module.exports = test;
