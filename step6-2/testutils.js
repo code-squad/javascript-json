5c6133319b42a80139eeae7cf18d210cf0887ceb
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-13 12:54:24 +0900

    Feat : assertEquals 메서드 추가
    parameter : expected, actual, testFunction
    return type : boolean (logFail, logSuccess 메서드 호출)
    기대값이 실제값과 같을 경우 logSucess,
    그렇지 않을 경우 logFail 메서드 반환

--- step6-2/testutils.js
+++ step6-2/testutils.js
@@ -11,6 +11,15 @@ const test = {
         console.log('\u001b[32m' + `SUCCESS!! ${testFunction.name} 의 [[ ${actual} ]]이 기대값 ${expected}와 일치` + '\u001b[0m')
         return true;
     },
+
+     // 단일 데이터 비교
+     assertEquals(expected, actual, testFunction) {
+        if (expected === actual) {
+            this.logSuccess(actual, expected, testFunction);
+        } else {
+            this.logFail(actual, expected, testFunction);
+        }
+    },
 }
 
 module.exports = test;
