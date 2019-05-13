6678cc788da317121268b082f9897eac3f6237ea
dev-dongwon <dev.dongwonkim@gmail.com>
2019-05-13 12:15:18 +0900

    Feat : get charType private method 추가
    tokenArr - 토큰을 만드는 값의 배열
    bracketArr - 배열을 만드는 값의 배열
    stringArr - 문자열을 만드는 값의 배열

--- step6-2/arrayparser.js
+++ step6-2/arrayparser.js
@@ -5,6 +5,16 @@ const ArrayParser = class {
     constructor(string) {
         this.string = string;
     }  
+
+    get charType() {
+        return {
+            tokenArr : [',','[',']'],
+            bracketArr : ['[', ']'],
+            stringArr : ['\'','\"'],
+        }
+    }
+
+    
 }
 
 module.exports = ArrayParser;
