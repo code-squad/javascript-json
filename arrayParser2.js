cb579a259dd10eaae376de33311a0cda167e48c9
LeeJuyeon <ruby413@hanmail.net>
2019-05-14 12:48:04 +0900

    새롭게 객체 생성한 부분 : openBracketIdxStack, parserArr 기능 : parser 구현 - lexerArr 에서 openBracket을 만나면 idx를 stack 에 쌓아두며, parserArr에 lexing된 배열 obj를 push 한다. - lexerArr 에서 bracket이 아닌 다른 value값을 만나면 stack 정보에 따라 parserArr의 child에 push 한다.

--- arrayParser2.js
+++ arrayParser2.js
@@ -1,24 +1,12 @@
-// { type: 'array',
-//   child: 
-//    [ { type: 'number', value: '123', child: [] },
-//      { type: 'number', value: '22', child: [] },
-//      { type: 'number', value: '33', child: [] } 
-//     ] 
-// }
-
-const str = "['1a3',[null,false,['11',[11223123],112],55, '99'],33, true]"
+const str = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]"
 
 const ArrayParser = class {
     constructor(){
         this.tokenArr = []
         this.lexerArray = [];
         this.openBracketIdxStack = [];
+        this.parserArr = [];
     }
-
-    removeToken(arr,token){
-        arr.forEach((arrParam) => {if(arrParam===token){arr.splice(arr.indexOf(token),1)}})
-    }
-
     tokenizer(str){
         let word = ""
         for(let i = 0; i<str.length; i++){
@@ -27,12 +15,17 @@ const ArrayParser = class {
                 word = ""
                 if(str[i] === "[" || str[i] === "]"){this.tokenArr.push(str[i])} //배열 토큰 푸쉬
             }else{
-                if(!(str[i] ===" " && str[i] === "[")){word = word + str[i]} //일반 토큰 생성
+                if(!(str[i] ===" " && str[i] === "[")){word = word + str[i].trim()} //일반 토큰 생성
             }
         }
         this.removeToken(this.tokenArr,"")
         return this.tokenArr
     }
+
+    removeToken(arr,token){
+        arr.forEach((arrParam) => {if(arrParam===token){arr.splice(arr.indexOf(token),1)}})
+    }
+
 
     lexer(arr){
         arr.forEach((token)=>{
@@ -40,8 +33,12 @@ const ArrayParser = class {
             let lexerToken
             if (token === '[' || token === ']'){
                 lexerObj.type = "array"
+                lexerObj.value = token
             }else{
-                if (!isNaN(token)){
+                if((token === '[' || token === ']')){
+                    lexerObj.type = "array"
+                    lexerToken = token
+                }else if (!isNaN(token)){
                     lexerToken = Number(token)
                 }else if(token[0] === "'"){
                     lexerToken = token.match(/\w+/g)[0]
@@ -59,120 +56,29 @@ const ArrayParser = class {
         return this.lexerArray;
     }
 
-    parser(lexerArray){ 
-        lexerArray.forEach((lexerObj)=>{
-            if(lexerObj.value ==="["){
-                this.openBracketIdxStack.push(lexerArray.indexOf(lexerObj))
+    parser(lexerArr) {
+        lexerArr.forEach((el)=>{
+            let elIndex = lexerArr.indexOf(el)
+            if(el.value ==="["){
+                this.openBracketIdxStack.push(lexerArr.indexOf(el))
+                this.parserArr.push(lexerArr[elIndex])
+            }else if(el.value === ']'){ 
+                this.openBracketIdxStack.pop();
+            }else{
+                this.parserArr[this.openBracketIdxStack.length-1].child.push(el)
             }
         })
-        console.log(this.openBracketIdxStack)
         
-        
-        const openBracketLastIdx = this.openBracketIdxStack[this.openBracketIdxStack.length-1]
-
-        for (let i = openBracketLastIdx; i<this.findFirstTokenIdx(lexerArray,"]"); i++){
-            lexerArray[i].child.push(lexerArray[i+1])
-            lexerArray.splice(i+1,1)
-        }
-        lexerArray.splice(this.findFirstTokenIdx(lexerArray,"]"),1)
-        
-        console.log(lexerArray)
+        return this.parserArr 
     }
-}
+ }
 
 const arrayParser = new ArrayParser()
 const token = arrayParser.tokenizer(str)
-console.log(token.length)
-console.log(arrayParser.lexer(token))
-
-
-const lexer = (arr) => {
-    let i = 0;
-    let Que = [];
-    let word = "";
-    let bracketStack = [];
-
-    // 배열 재배치
-    arr = arr.split("");
-    arr.shift()
-    arr.pop()
-    arr.push(",")
-
-    while(i<= arr.length-1){
-        // 띄어쓰기
-        if (arr[0] ===" "){
-            arr.shift()
-        }
-        // 배열
-        else if (arr[0] === "["){
-            // 배열 스트링 화 시키기
-            word = ""
-            while(i<= arr.length-1){ 
-                if(arr[i] === "["){
-                    bracketStack.push(arr[i]);
-                }
-                word = word+ arr[i];
-                arr.shift()
-                if(arr[i] === "]"){
-                    bracketStack.shift();
-                    word = word+ arr[i];
-                    arr.shift()
-                }
-                if(bracketStack[0] === undefined){
-                    break;
-                }
-            }
-            arr.shift();
-
-            // 배열 재귀
-            let dpAnswer = lexer(word);
-            Que.push(dpAnswer);
-        }
-        // string
-        else if (arr[0] ==="'"){
-            arr.shift() // string 시작 "'" 제거
-            word = ""
-            while(i<= arr.indexOf(',')){
-                if(arr[i] === "'"){
-                    arr.shift() // string 마지막 "'" 제거
-                }else{
-                    word = word+ arr[i];
-                    arr.shift()
-                } 
-                if(arr.indexOf(',') === 0) { // word 단위
-                    break
-                }
-            }
-            Que.push(word)
-            arr.shift()
-        }
-
-        // not string, array => number, boolean, null
-        else{
-            word = ""
-            // word 형성
-            while(i<= arr.indexOf(',')){
-                word = word+ arr[i];
-                arr.shift()
-                if(arr.indexOf(',') === 0) { // word 단위
-                    break
-                }
-            }
-            // boolean or null or Number check
-            if(word === "true" || word === "false"){
-                word = Boolean(word)
-            }else if(word === "null"){
-                word = null;
-            }else if(typeof(Number(word))==="number"){
-                word = Number(word)
-            }
-            // word push
-            arr.shift()
-            Que.push(word);
-        }
-    }
-    return Que
-}
-
-console.log(JSON.stringify(lexer(str), null, 2))
-//console.log(arr)
+const lexer = arrayParser.lexer(token)
+console.log(lexer)
+const parser = arrayParser.parser(lexer)
+// console.log(token)
+// console.log(lexer)
+ console.log(arrayParser.openBracketIdxStack)
+console.log(JSON.stringify(parser, null, 2))
