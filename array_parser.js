7b54ffc43e87a32793615a4668d7dc897a92bea8
mukeunzi <eunji057@gmail.com>
2019-05-17 14:52:21 +0900

    refactor : 함수 위치 변경
    - 추상적인 메소드를 상위로 위치 변경

--- array_parser.js
+++ array_parser.js
@@ -48,6 +48,51 @@ class ArrayParser {
 		return tokens;
 	}
 
+	lexer(tokens) {
+		const lexerArr = [];
+
+		for (let token of tokens) {
+			if (!this.commaAndSpaceArr.includes(token)) {
+				lexerArr.push(this.getTypeAndValue(token));
+			}
+		}
+		return lexerArr;
+	}
+
+	parser(lexerArr) {
+		const parserArr = [];
+
+		while (lexerArr.length) {
+			const lexeredData = lexerArr.shift();
+
+			if (lexeredData.type === 'leftBracket') {
+				this.parserStack.push('[');
+				parserArr.push({ type: 'array', child: this.parser(lexerArr) });
+			} else if (lexeredData.type === 'rightBracket') {
+				if (this.parserStack.length > 0) {
+					this.parserStack.pop();
+					return parserArr;
+				}
+				throw new Error(`${msgObj.INVALID_BRACKET}`);
+			} else {
+				if (this.parserStack.length > 0) {
+					parserArr.push(lexeredData);
+				} else {
+					throw new Error(`${msgObj.INVALID_BRACKET}`);
+				}
+			}
+		}
+		return parserArr;
+	}
+
+	arrayParser(inputStr) {
+		try {
+			console.log(JSON.stringify(this.parser(this.lexer(this.tokenizer(inputStr))), null, 2));
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
+
 	getTypeAndValue(token) {
 		if (!isNaN(token)) {
 			return { type: 'number', value: Number(token) };
@@ -79,44 +124,6 @@ class ArrayParser {
 		throw new Error(`${token}${msgObj.INVALID_TYPE}`);
 	}
 
-	lexer(tokens) {
-		const lexerArr = [];
-
-		for (let token of tokens) {
-			if (!this.commaAndSpaceArr.includes(token)) {
-				lexerArr.push(this.getTypeAndValue(token));
-			}
-		}
-
-		return lexerArr;
-	}
-
-	parser(lexerArr) {
-		const parserArr = [];
-
-		while (lexerArr.length) {
-			const lexeredData = lexerArr.shift();
-
-			if (lexeredData.type === 'leftBracket') {
-				this.parserStack.push('[');
-				parserArr.push({ type: 'array', child: this.parser(lexerArr) });
-			} else if (lexeredData.type === 'rightBracket') {
-				if (this.parserStack.length > 0) {
-					this.parserStack.pop();
-					return parserArr;
-				}
-				throw new Error(`${msgObj.INVALID_BRACKET}`);
-			} else {
-				if (this.parserStack.length > 0) {
-					parserArr.push(lexeredData);
-				} else {
-					throw new Error(`${msgObj.INVALID_BRACKET}`);
-				}
-			}
-		}
-		return parserArr;
-	}
-
 	isValidString(token) {
 		const tokenArr = [...token];
 		const filteredQuotes = tokenArr.filter(char => this.quoteArr.includes(char));
@@ -124,14 +131,6 @@ class ArrayParser {
 		if (filteredQuotes.length !== 2) return false;
 		if (!(this.quoteArr.includes(token[0]) && this.quoteArr.includes(token[token.length - 1]))) return false;
 		return true;
-	}
-
-	arrayParser(inputStr) {
-		try {
-			console.log(JSON.stringify(this.parser(this.lexer(this.tokenizer(inputStr))), null, 2));
-		} catch (error) {
-			console.log(error.message);
-		}
 	}
 }
 
