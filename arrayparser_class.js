f8eabee2ef588a6c20d1b294d4b05f98db37d136
DESKTOP-6B6U099\eunjin <audar8@gmail.com>
2019-05-18 01:20:02 +0900

    ArrayParser 클래스 생성
    - 토큰 생성 : tokenize 메서드
    - 토큰에 의미부여 : lex 메서드
    - 의미부여한 토큰 구조화 : parse 메서드
    - 실행 : run 메서드생성

---
+++ arrayparser_class.js
@@ -0,0 +1,32 @@
+class ArrayParser {
+    constructor(jsonStr) {
+        this.jsonStr = jsonStr;
+    }
+
+    //토큰화
+    tokenize() {
+       
+    }
+
+    //의미 부여
+    lex() {
+        
+    }
+
+    //구조화
+    parse() {
+       
+    }
+
+    //실행
+    run() {
+
+    }
+}
+
+const jsonStr = "['1a3',[null, false, ['11', [112233], 112], 55, '99'], 33, true]";
+const jsonStr2 = "['1a'3', [22, 23, [11, [112233], 112], 55], 33]"; // '1a'3'은 올바른 문자열이 아닙니다.
+const jsonStr2 = "['1a3', [22, 23, [11, [112233], 112], 55], 3d3]"; // 3d3은 알수 없는 타입입니다.
+const arrayParser = new ArrayParser(jsonStr);
+const result = arrayParser.run();
+console.log(JSON.stringify(result, null, 2));
