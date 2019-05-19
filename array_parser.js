5437a822de64dde0006757245903745b0c8a9226
mukeunzi <eunji057@gmail.com>
2019-05-19 20:09:59 +0900

    refactor : 함수별 기능 축소
    - 길이가 긴 함수(여러가지 기능을 가진 함수) 분할

--- array_parser.js
+++ array_parser.js
@@ -5,57 +5,37 @@ class ArrayParser {
 		this.parserStack = [];
 		this.quoteArr = ["'", '"'];
 		this.bracketArr = ['[', ']'];
-		this.commaAndSpaceArr = [',', ' '];
+		this.separatorArr = [',', ' '];
 	}
 
 	tokenizer(inputStr) {
-		const stack = [];
-		const tokens = [];
-		let makeStr = '';
+		const quoteStack = [];
+		const tokenArr = [];
+		let token = '';
 		let doubleQuotes = false;
 
 		for (let char of inputStr) {
-			if (stack.length === 0) {
-				if (this.bracketArr.includes(char) || this.commaAndSpaceArr.includes(char)) {
-					if (makeStr !== '') {
-						tokens.push(makeStr);
-						makeStr = '';
-					}
-					tokens.push(char);
-				} else if (this.quoteArr.includes(char)) {
-					makeStr += char;
-					stack.push(char);
-				} else {
-					makeStr += char;
-				}
+			if (!quoteStack.length) {
+				token = this.makeVariousTypeToken(char, token, tokenArr, quoteStack);
 			} else {
-				if (this.quoteArr.includes(char)) {
-					makeStr += char;
-					doubleQuotes = true;
-				} else if ((this.commaAndSpaceArr.includes(char) || this.bracketArr.includes(char)) && doubleQuotes === true) {
-					tokens.push(makeStr);
-					tokens.push(char);
-					stack.pop();
-					makeStr = '';
-					doubleQuotes = false;
-				} else {
-					makeStr += char;
-					doubleQuotes = false;
-				}
+				let returnObj = this.makeStringTypeToken(char, token, tokenArr, quoteStack, doubleQuotes);
+				token = returnObj.token;
+				doubleQuotes = returnObj.doubleQuotes;
 			}
 		}
 
-		return tokens;
+		return tokenArr;
 	}
 
 	lexer(tokens) {
 		const lexerArr = [];
 
 		for (let token of tokens) {
-			if (!this.commaAndSpaceArr.includes(token)) {
-				lexerArr.push(this.getTypeAndValue(token));
+			if (!this.isSeparator(token)) {
+				this.makeLexerData(lexerArr, token);
 			}
 		}
+
 		return lexerArr;
 	}
 
@@ -65,23 +45,18 @@ class ArrayParser {
 		while (lexerArr.length) {
 			const lexeredData = lexerArr.shift();
 
-			if (lexeredData.type === 'leftBracket') {
-				this.parserStack.push('[');
-				parserArr.push({ type: 'array', child: this.parser(lexerArr) });
-			} else if (lexeredData.type === 'rightBracket') {
-				if (this.parserStack.length > 0) {
-					this.parserStack.pop();
-					return parserArr;
+			if (this.isOpenBracket(lexeredData.type)) {
+				this.startMakeArray(parserArr, lexerArr);
+			} else if (this.isCloseBracket(lexeredData.type)) {
+				if (this.isValidBracket(this.parserStack.length)) {
+					return this.finishMakeArray(parserArr);
 				}
 				throw new Error(`${msgObj.INVALID_BRACKET}`);
 			} else {
-				if (this.parserStack.length > 0) {
-					parserArr.push(lexeredData);
-				} else {
-					throw new Error(`${msgObj.INVALID_BRACKET}`);
-				}
+				this.makeParser(parserArr, lexeredData);
 			}
 		}
+
 		return parserArr;
 	}
 
@@ -98,12 +73,11 @@ class ArrayParser {
 			return { type: 'number', value: Number(token) };
 		}
 		if (token === '[') {
-			return { type: 'leftBracket', value: token };
+			return { type: 'openBracket', value: token };
 		}
 		if (token === ']') {
-			return { type: 'rightBracket', value: token };
+			return { type: 'closeBracket', value: token };
 		}
-
 		if (token.includes('"') || token.includes("'")) {
 			if (this.isValidString(token)) {
 				return { type: 'string', value: token };
@@ -126,11 +100,107 @@ class ArrayParser {
 
 	isValidString(token) {
 		const tokenArr = [...token];
-		const filteredQuotes = tokenArr.filter(char => this.quoteArr.includes(char));
+		const filteredQuotes = tokenArr.filter(char => this.isQuote(char));
 
 		if (filteredQuotes.length !== 2) return false;
-		if (!(this.quoteArr.includes(token[0]) && this.quoteArr.includes(token[token.length - 1]))) return false;
+		if (!(this.isQuote(token[0]) && this.isQuote(token[token.length - 1]))) return false;
 		return true;
+	}
+
+	isBracket(char) {
+		return this.bracketArr.includes(char);
+	}
+
+	isSeparator(char) {
+		return this.separatorArr.includes(char);
+	}
+
+	isQuote(char) {
+		return this.quoteArr.includes(char);
+	}
+
+	pushAndResetToken(tokenArr, token) {
+		if (token !== '') {
+			tokenArr.push(token);
+			token = '';
+		}
+		return token;
+	}
+
+	pushAndPopToken(tokenArr, char, token, quoteStack) {
+		tokenArr.push(token);
+		tokenArr.push(char);
+		token = '';
+		quoteStack.pop();
+
+		return token;
+	}
+
+	isClosingToken(char, doubleQuotes) {
+		return (this.isSeparator(char) || this.isBracket(char)) && doubleQuotes;
+	}
+
+	makeLexerData(lexerArr, token) {
+		lexerArr.push(this.getTypeAndValue(token));
+	}
+
+	isOpenBracket(dataType) {
+		return dataType === 'openBracket';
+	}
+
+	isCloseBracket(dataType) {
+		return dataType === 'closeBracket';
+	}
+
+	startMakeArray(parserArr, lexerArr) {
+		this.parserStack.push('[');
+		parserArr.push({ type: 'array', child: this.parser(lexerArr) });
+	}
+
+	isValidBracket() {
+		return this.parserStack.length;
+	}
+
+	finishMakeArray(parserArr) {
+		this.parserStack.pop();
+		return parserArr;
+	}
+
+	makeParser(parserArr, lexeredData) {
+		if (this.parserStack.length) {
+			parserArr.push(lexeredData);
+		} else {
+			throw new Error(`${msgObj.INVALID_BRACKET}`);
+		}
+	}
+
+	makeVariousTypeToken(char, token, tokenArr, quoteStack) {
+		if (this.isBracket(char) || this.isSeparator(char)) {
+			token = this.pushAndResetToken(tokenArr, token);
+			tokenArr.push(char);
+		} else if (this.isQuote(char)) {
+			token += char;
+			quoteStack.push(char);
+		} else {
+			token += char;
+		}
+
+		return token;
+	}
+
+	makeStringTypeToken(char, token, tokenArr, quoteStack, doubleQuotes) {
+		if (this.isQuote(char)) {
+			token += char;
+			doubleQuotes = true;
+		} else if (this.isClosingToken(char, doubleQuotes)) {
+			token = this.pushAndPopToken(tokenArr, char, token, quoteStack);
+			doubleQuotes = false;
+		} else {
+			token += char;
+			doubleQuotes = false;
+		}
+
+		return { token, doubleQuotes };
 	}
 }
 
