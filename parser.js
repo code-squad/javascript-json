1cb219a012169267e897c85358b1640afcb35de4
P-iknow <apricotsoul@gmail.com>
2019-05-21 18:26:13 +0900

    fix: currentNode 가 key 일때 재귀함수 호출인자 부분 수정
    - parse({lexedQueue, objKeyNode}) key:value 형태가 아님
    - parse({lexedQueue, parentNode: objKeyNode}) 로 수정

--- parser.js
+++ parser.js
@@ -7,22 +7,21 @@ const parse = ({ lexedQueue, parentNode = defaultParentNode }) => {
 
   if (ntc.isEndOfArrOrObj(currentNode)) return parentNode;
 
-  if (ntc.isArrayOrObj(currentNode)) {
-    parentNode.child.push(parse({ lexedQueue, parentNode: currentNode }));
-    return parse({ lexedQueue, parentNode });
-  }
-
-  // 노드가 key인 경우 다음에 나오는 노드와 병합하여 완전한 key: value 객체 를 만듬
-  if (ntc.isKey(currentNode)) {
-    const objKeyNode = currentNode;
-    const objValueNode = parse({ lexedQueue, objKeyNode });
-    currentNode = Object.assign(objKeyNode, objValueNode);
-  }
-
   if (ntc.isKey(parentNode)) {
     return ntc.isArrayOrObj(currentNode)
       ? parse({ lexedQueue, parentNode: currentNode })
       : currentNode;
+  }
+
+  if (ntc.isArrayOrObj(currentNode)) {
+    parentNode.child.push(parse({ lexedQueue, parentNode: currentNode }));
+    return parse({ lexedQueue, parentNode });
+  }
+  // 노드가 key인 경우 다음에 나오는 노드와 병합하여 완전한 key: value 객체 를 만듬
+  if (ntc.isKey(currentNode)) {
+    const objKeyNode = currentNode;
+    const objValueNode = parse({ lexedQueue, parentNode: objKeyNode });
+    currentNode = Object.assign(objKeyNode, objValueNode);
   }
 
   parentNode.child.push(currentNode);
