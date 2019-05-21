8a4a520f9deeb6de21bf7b646c49ee3889adb162
P-iknow <apricotsoul@gmail.com>
2019-05-21 19:31:23 +0900

    chore: 주석 제거, 테스트케이스 변경

--- arrayParser.js
+++ arrayParser.js
@@ -1,16 +1,14 @@
 const tokenize = require('./tokenizer');
-const lex = require('./lexer');
+const makeLexedToken = require('./lexer');
 const parse = require('./parser');
 
 const arrayParser = input => {
-  return parse(tokenize(input).map(v => lex(v)));
+  return parse({ lexedQueue: tokenize(input).reduce(makeLexedToken, []) });
 };
 
 // test case
-const caseRight1 = "['1a3',[null,false,['11',[112233],112],55, '99'],33, true]";
-const caseWrong1 = "['1a'3',[22,23,[11,[112233],112],55],33]";
-const caseWrong2 = "['1a3',[22,23,[11,[112233],112],55],3d3]";
+const caseRight1 =
+  "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:'a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]";
 
-console.dir(arrayParser(caseRight1));
-console.dir(arrayParser(caseWrong1));
-console.dir(arrayParser(caseWrong2));
+const result = arrayParser(caseRight1);
+console.log(JSON.stringify(result, null, 2));
