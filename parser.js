efafb93c67b96635f1c36a131dcf21733f82b9ea
P-iknow <apricotsoul@gmail.com>
2019-05-21 16:13:40 +0900

    refect: obj형태도 처리될 수 있도록 변경
    - current Node 가 key 인 경우에 parrentNode 인자에 current 노드(key node)를 할당하여parse 함수 호출
    - paretnNode 가 key 인 경우 현재 노드를 리턴
    - 리턴된 노드와 key 노드를 합쳐서 완전한 key : value  object 를 만듬

--- parser.js
+++ parser.js
@@ -1,18 +1,32 @@
 const Node = require('./node');
+const ntc = require('./nodeTypeCheker');
 
-const defaultParentNode = new Node('Array');
-const parse = (lexedQueue, parentNode = defaultParentNode) => {
-  const node = lexedQueue.shift();
-  if (node === undefined || node.type === 'End') {
-    return parentNode;
+const defaultParentNode = new Node({ type: 'array', child: [] });
+const parse = ({ lexedQueue, parentNode = defaultParentNode }) => {
+  let currentNode = lexedQueue.shift();
+
+  if (ntc.isEndOfArrOrObj(currentNode)) return parentNode;
+
+  if (ntc.isArrayOrObj(currentNode)) {
+    parentNode.child.push(parse({ lexedQueue, parentNode: currentNode }));
+    return parse({ lexedQueue, parentNode });
   }
-  if (node.type === 'Array') {
-    const childeNode = node;
-    parentNode.child.push(parse(lexedQueue, childeNode));
-    return parse(lexedQueue, parentNode);
+
+  // 노드가 key인 경우 다음에 나오는 노드와 병합하여 완전한 key: value 객체 를 만듬
+  if (ntc.isKey(currentNode)) {
+    const objKeyNode = currentNode;
+    const objValueNode = parse({ lexedQueue, objKeyNode });
+    currentNode = Object.assign(objKeyNode, objValueNode);
   }
-  parentNode.child.push(node);
-  return parse(lexedQueue, parentNode);
+
+  if (ntc.isKey(parentNode)) {
+    return ntc.isArrayOrObj(currentNode)
+      ? parse({ lexedQueue, parentNode: currentNode })
+      : currentNode;
+  }
+
+  parentNode.child.push(currentNode);
+  return parse({ lexedQueue, parentNode });
 };
 
 module.exports = parse;
