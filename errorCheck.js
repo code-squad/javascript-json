6ceba43f69c8ba33d40eaaf004e170e7ba737092
P-iknow <apricotsoul@gmail.com>
2019-05-21 16:35:38 +0900

    fix: "가 처음 열릴때만 실행되도록 함수 변경
    - 기존에는 " 를 만나면 무조건 trippleQuote 함수가 실행됨
    - 수정후에는 "가 열릴때만 trippleQuote 함수가 실행되서 연산횟수 감소
    - 함수 발동 위치가 달라짐에 따라 함수 네이밍 inNotTrippleQuote 로 변경
    - toggleBool 함수에 true 가 담기도록 inNot 으로 바꿔서 ture 리턴

--- errorCheck.js
+++ errorCheck.js
@@ -1,11 +1,11 @@
 const util = require('./util');
 
 const errorCheck = {
-  trippleQuote(str) {
+  isNotTrippleQuote(str) {
     if (util.countQuote(str) === 3) {
       throw new Error(`${str}는 올바른 문자열이 아닙니다`);
     }
-    return false;
+    return true;
   },
 };
 
